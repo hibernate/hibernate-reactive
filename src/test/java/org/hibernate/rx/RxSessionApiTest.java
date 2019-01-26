a9e669978a3cd42e1a9123de16f602f2cd371bb0
Davide D'Alto <davide@hibernate.org>
2019-01-26 19:55:12 +0100

    Test the API using mocks

---
+++ src/test/java/org/hibernate/rx/RxSessionApiTest.java
@@ -0,0 +1,132 @@
+package org.hibernate.rx;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Simple tests for checking that the {@link RxSession} API makes sense.
+ */
+public class RxSessionApiTest {
+
+	private final ConcurrentHashMap<Object, Object> db = new ConcurrentHashMap<>();
+
+	RxSession session = new MockRxSession.Builder<GuineaPig>()
+			.load( (type, id) -> type.cast( db.entrySet().stream()
+													.filter( e -> e.getKey().equals( id ) )
+													.map( Map.Entry::getValue )
+													.findAny()
+													.orElse( null ) ) )
+			.persist( p -> db.put( ( (GuineaPig) p ).getId(), (GuineaPig) p ) )
+			.remove( p -> db.remove( ( (GuineaPig) p ).getId() ) ).build();
+
+	@Test
+	public void testLoadNull() throws Exception {
+		GuineaPig loadedPig = session.load( GuineaPig.class, 1 )
+				.get();
+
+		assertThat( loadedPig ).isNull();
+	}
+
+	@Test
+	public void testLoad() throws Exception {
+		GuineaPig pig = new GuineaPig( 5, "Hamtaro" );
+		db.put( pig.getId(), pig );
+
+		GuineaPig loadedPig = session.load( GuineaPig.class, 5 ).get();
+
+		assertThat( loadedPig ).isEqualTo( pig );
+	}
+
+	@Test
+	public void testPersist() throws Exception {
+		final GuineaPig pig = new GuineaPig( 1, "Buttercup" );
+
+		session.persist( pig ).get();
+
+		assertThat( db.values() ).containsExactly( pig );
+	}
+
+	@Test
+	public void testRemove() throws Exception {
+		GuineaPig pig = new GuineaPig( 5, "McCloud" );
+		db.put( pig.getId(), pig );
+
+		session.remove( pig ).get();
+
+		assertThat( db ).isEmpty();
+	}
+
+	@Test
+	public void testRemoveWithMoreThanOneEntry() throws Exception {
+		GuineaPig highlander = new GuineaPig( 15, "Highlander" );
+		GuineaPig duncan = new GuineaPig( 82, "Duncan" );
+
+		db.put( highlander.getId(), highlander );
+		db.put( duncan.getId(), duncan );
+
+		session.remove( duncan ).get();
+
+		assertThat( db.values() ).containsExactly( highlander );
+	}
+
+	public static class GuineaPig {
+		private Integer id;
+		private String name;
+
+		public GuineaPig() {
+		}
+
+		public GuineaPig(String name) {
+			this.name = name;
+		}
+
+		public GuineaPig(Integer id, String name) {
+			this.id = id;
+			this.name = name;
+		}
+
+		public Integer getId() {
+			return id;
+		}
+
+		public void setId(Integer id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		@Override
+		public String toString() {
+			return name;
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if ( this == o ) {
+				return true;
+			}
+			if ( o == null || getClass() != o.getClass() ) {
+				return false;
+			}
+			GuineaPig guineaPig = (GuineaPig) o;
+			return Objects.equals( id, guineaPig.id ) &&
+					Objects.equals( name, guineaPig.name );
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash( id, name );
+		}
+	}
+}
