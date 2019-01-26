bc0b56bdf386f107aacce28228f500123885d94e
Davide D'Alto <davide@hibernate.org>
2019-01-26 18:46:48 +0100

    The first draft of the reactive session: RxSession

---
+++ src/main/java/org/hibernate/rx/RxSession.java
@@ -0,0 +1,15 @@
+package org.hibernate.rx;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * Right now we are playing around, but this is going to be the core
+ * interface of the project.
+ */
+public interface RxSession {
+	<T> CompletableFuture<T> load(Class<T> entityClass, Object id);
+
+	CompletableFuture<Void> persist(Object entity);
+
+	CompletableFuture<Void> remove(Object entity);
+}
