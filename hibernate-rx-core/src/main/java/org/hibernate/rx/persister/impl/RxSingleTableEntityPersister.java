8eedfcf184b12540a6e8e35ebbe3ef83d03310d3
gavinking <gavin@ceylon-lang.org>
2020-01-14 17:23:14 +0100

    make delete consistent with update and insert

--- hibernate-rx-core/src/main/java/org/hibernate/rx/persister/impl/RxSingleTableEntityPersister.java
+++ hibernate-rx-core/src/main/java/org/hibernate/rx/persister/impl/RxSingleTableEntityPersister.java
@@ -845,16 +845,7 @@ public class RxSingleTableEntityPersister extends SingleTableEntityPersister imp
 
 	@Override
 	protected String generateDeleteString(int j) {
-		final Delete delete = new Delete()
-				.setTableName( getTableName( j ) )
-				.addPrimaryKeyColumns( getKeyColumns( j ) );
-		if ( j == 0 ) {
-			delete.setVersionColumnName( getVersionColumnName() );
-		}
-		if ( getFactory().getSessionFactoryOptions().isCommentsEnabled() ) {
-			delete.setComment( "delete " + getEntityName() );
-		}
-		return delete.toStatementString();
+		return super.generateDeleteString(j).replace("?", "$1");
 	}
 
 	/**
