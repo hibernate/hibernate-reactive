5285e63a99e4f5510b8243404af061ac286cb893
gavinking <gavin@ceylon-lang.org>
2020-01-16 00:48:13 +0100

    Move unsupported operations down to bottom of source file

--- hibernate-rx-core/src/main/java/org/hibernate/rx/persister/impl/RxUnionSubclassEntityPersister.java
+++ hibernate-rx-core/src/main/java/org/hibernate/rx/persister/impl/RxUnionSubclassEntityPersister.java
@@ -164,99 +164,6 @@ public class RxUnionSubclassEntityPersister extends UnionSubclassEntityPersister
 				.buildLoader( this, batchSize, lockOptions, getFactory(), loadQueryInfluencers );
 	}
 
-	@Override
-	protected Serializable insert(
-			Object[] fields, boolean[] notNull, String sql, Object object, SharedSessionContractImplementor session)
-			throws HibernateException {
-		throw new UnsupportedOperationException( "Wrong method calls. Use the reactive equivalent." );
-	}
-
-	@Override
-	protected void insert(
-			Serializable id,
-			Object[] fields,
-			boolean[] notNull,
-			int j,
-			String sql,
-			Object object,
-			SharedSessionContractImplementor session) throws HibernateException {
-		throw new UnsupportedOperationException( "Wrong method calls. Use the reactive equivalent." );
-	}
-
-	@Override
-	public Serializable insert(
-			Object[] fields, Object object, SharedSessionContractImplementor session) throws HibernateException {
-		throw new UnsupportedOperationException( "Wrong method calls. Use the reactive equivalent." );
-	}
-
-	@Override
-	public void insert(Serializable id, Object[] fields, Object object, SharedSessionContractImplementor session) {
-		throw new UnsupportedOperationException( "Wrong method calls. Use the reactive equivalent." );
-	}
-
-	@Override
-	protected void delete(
-			Serializable id,
-			Object version,
-			int j,
-			Object object,
-			String sql,
-			SharedSessionContractImplementor session,
-			Object[] loadedState) throws HibernateException {
-		throw new UnsupportedOperationException( "Wrong method calls. Use the reactive equivalent." );
-	}
-
-	@Override
-	public void delete(
-			Serializable id, Object version, Object object, SharedSessionContractImplementor session)
-			throws HibernateException {
-		throw new UnsupportedOperationException( "Wrong method calls. Use the reactive equivalent." );
-	}
-
-	@Override
-	protected void updateOrInsert(
-			Serializable id,
-			Object[] fields,
-			Object[] oldFields,
-			Object rowId,
-			boolean[] includeProperty,
-			int j,
-			Object oldVersion,
-			Object object,
-			String sql,
-			SharedSessionContractImplementor session) throws HibernateException {
-		throw new UnsupportedOperationException( "Wrong method calls. Use the reactive equivalent." );
-	}
-
-	@Override
-	protected boolean update(
-			Serializable id,
-			Object[] fields,
-			Object[] oldFields,
-			Object rowId,
-			boolean[] includeProperty,
-			int j,
-			Object oldVersion,
-			Object object,
-			String sql,
-			SharedSessionContractImplementor session) throws HibernateException {
-		throw new UnsupportedOperationException( "Wrong method calls. Use the reactive equivalent." );
-	}
-
-	@Override
-	public void update(
-			Serializable id,
-			Object[] fields,
-			int[] dirtyFields,
-			boolean hasDirtyCollection,
-			Object[] oldFields,
-			Object oldVersion,
-			Object object,
-			Object rowId,
-			SharedSessionContractImplementor session) throws HibernateException {
-		throw new UnsupportedOperationException( "Wrong method calls. Use the reactive equivalent." );
-	}
-
 	private void preInsertInMemoryValueGeneration(
 			Object[] fields,
 			Object object,
@@ -874,5 +781,99 @@ public class RxUnionSubclassEntityPersister extends UnionSubclassEntityPersister
 				.replaceFirst("\\?", "\\$" + insertParams[j])
 				.replaceFirst("\\?", "\\$" + (insertParams[j]+1));
 	}
+
+	@Override
+	protected Serializable insert(
+			Object[] fields, boolean[] notNull, String sql, Object object, SharedSessionContractImplementor session)
+			throws HibernateException {
+		throw new UnsupportedOperationException( "Wrong method calls. Use the reactive equivalent." );
+	}
+
+	@Override
+	protected void insert(
+			Serializable id,
+			Object[] fields,
+			boolean[] notNull,
+			int j,
+			String sql,
+			Object object,
+			SharedSessionContractImplementor session) throws HibernateException {
+		throw new UnsupportedOperationException( "Wrong method calls. Use the reactive equivalent." );
+	}
+
+	@Override
+	public Serializable insert(
+			Object[] fields, Object object, SharedSessionContractImplementor session) throws HibernateException {
+		throw new UnsupportedOperationException( "Wrong method calls. Use the reactive equivalent." );
+	}
+
+	@Override
+	public void insert(Serializable id, Object[] fields, Object object, SharedSessionContractImplementor session) {
+		throw new UnsupportedOperationException( "Wrong method calls. Use the reactive equivalent." );
+	}
+
+	@Override
+	protected void delete(
+			Serializable id,
+			Object version,
+			int j,
+			Object object,
+			String sql,
+			SharedSessionContractImplementor session,
+			Object[] loadedState) throws HibernateException {
+		throw new UnsupportedOperationException( "Wrong method calls. Use the reactive equivalent." );
+	}
+
+	@Override
+	public void delete(
+			Serializable id, Object version, Object object, SharedSessionContractImplementor session)
+			throws HibernateException {
+		throw new UnsupportedOperationException( "Wrong method calls. Use the reactive equivalent." );
+	}
+
+	@Override
+	protected void updateOrInsert(
+			Serializable id,
+			Object[] fields,
+			Object[] oldFields,
+			Object rowId,
+			boolean[] includeProperty,
+			int j,
+			Object oldVersion,
+			Object object,
+			String sql,
+			SharedSessionContractImplementor session) throws HibernateException {
+		throw new UnsupportedOperationException( "Wrong method calls. Use the reactive equivalent." );
+	}
+
+	@Override
+	protected boolean update(
+			Serializable id,
+			Object[] fields,
+			Object[] oldFields,
+			Object rowId,
+			boolean[] includeProperty,
+			int j,
+			Object oldVersion,
+			Object object,
+			String sql,
+			SharedSessionContractImplementor session) throws HibernateException {
+		throw new UnsupportedOperationException( "Wrong method calls. Use the reactive equivalent." );
+	}
+
+	@Override
+	public void update(
+			Serializable id,
+			Object[] fields,
+			int[] dirtyFields,
+			boolean hasDirtyCollection,
+			Object[] oldFields,
+			Object oldVersion,
+			Object object,
+			Object rowId,
+			SharedSessionContractImplementor session) throws HibernateException {
+		throw new UnsupportedOperationException( "Wrong method calls. Use the reactive equivalent." );
+	}
+
 }
 
