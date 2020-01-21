c96b896f68c17c8c8504798e260c5af3854f17a3
gavinking <gavin@ceylon-lang.org>
2020-01-21 10:07:47 +0100

    Remove code unnecessarily copy/pasted from core

--- hibernate-rx-core/src/main/java/org/hibernate/rx/boot/impl/RxServiceRegistryInitializer.java
+++ hibernate-rx-core/src/main/java/org/hibernate/rx/boot/impl/RxServiceRegistryInitializer.java
@@ -1,22 +1,21 @@
 package org.hibernate.rx.boot.impl;
 
-import java.util.Map;
-
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.rx.jpa.impl.RxPersisterClassResolverInitiator;
 import org.hibernate.rx.service.initiator.RxConnectionProviderInitiator;
-import org.hibernate.rx.service.initiator.RxSessionFactoryServiceRegistryFactoryInitiator;
 import org.hibernate.rx.service.initiator.RxTransactionCoordinatorBuilderInitiator;
 import org.hibernate.service.spi.ServiceContributor;
 
+/**
+ * Contributes our
+ * {@link org.hibernate.service.spi.ServiceInitiator service initiators}
+ * to Hibernate.
+ */
 public class RxServiceRegistryInitializer implements ServiceContributor {
 
 	@Override
 	public void contribute(StandardServiceRegistryBuilder serviceRegistryBuilder) {
-		Map<Object, Object> settings = serviceRegistryBuilder.getSettings();
-
 //		serviceRegistryBuilder.addInitiator( RxJdbcServicesInitiator.INSTANCE );
-		serviceRegistryBuilder.addInitiator( RxSessionFactoryServiceRegistryFactoryInitiator.INSTANCE );
 		serviceRegistryBuilder.addInitiator( RxConnectionProviderInitiator.INSTANCE );
 		serviceRegistryBuilder.addInitiator( RxTransactionCoordinatorBuilderInitiator.INSTANCE );
 		serviceRegistryBuilder.addInitiator( RxPersisterClassResolverInitiator.INSTANCE );
