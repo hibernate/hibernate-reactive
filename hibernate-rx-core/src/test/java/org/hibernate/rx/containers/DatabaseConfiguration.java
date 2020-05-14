324b1da8ad36106450257c8f8a333dbe92fb5cbe
Davide D'Alto <davide@hibernate.org>
2020-05-14 11:55:05 +0100

    Remove redundant scopes for constants in DatabaseConfiguration

--- hibernate-rx-core/src/test/java/org/hibernate/rx/containers/DatabaseConfiguration.java
+++ hibernate-rx-core/src/test/java/org/hibernate/rx/containers/DatabaseConfiguration.java
@@ -1,11 +1,15 @@
 package org.hibernate.rx.containers;
 
+/**
+ * Contains the common constants that we need for the configuration of databases
+ * during tests.
+ */
 public interface DatabaseConfiguration {
 	
-	public static final boolean USE_DOCKER = Boolean.getBoolean( "docker" );
-	
-	public static final String USERNAME = "hibernate-rx";
-	public static final String PASSWORD = "hibernate-rx";
-	public static final String DB_NAME = "hibernate-rx";
+	boolean USE_DOCKER = Boolean.getBoolean( "docker" );
+
+	String USERNAME = "hibernate-rx";
+	String PASSWORD = "hibernate-rx";
+	String DB_NAME = "hibernate-rx";
 
 }
