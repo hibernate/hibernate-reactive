5846ab233415ae5daa1e372723cf75ed064568af
Andrew Guibert <andy.guibert@gmail.com>
2020-05-15 14:16:45 -0500

    Support for DB2

--- hibernate-reactive-core/src/main/java/org/hibernate/reactive/service/ReactiveConnectionPoolProviderImpl.java
+++ hibernate-reactive-core/src/main/java/org/hibernate/reactive/service/ReactiveConnectionPoolProviderImpl.java
@@ -1,12 +1,8 @@
 package org.hibernate.reactive.service;
 
-import io.vertx.core.Vertx;
-import io.vertx.mysqlclient.MySQLPool;
-import io.vertx.pgclient.PgPool;
-import io.vertx.sqlclient.Pool;
-import io.vertx.mysqlclient.MySQLConnectOptions;
-import io.vertx.pgclient.PgConnectOptions;
-import io.vertx.sqlclient.PoolOptions;
+import java.net.URI;
+import java.util.Map;
+
 import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.internal.util.config.ConfigurationException;
 import org.hibernate.internal.util.config.ConfigurationHelper;
@@ -17,8 +13,15 @@ import org.hibernate.reactive.util.impl.JdbcUrlParser;
 import org.hibernate.service.spi.Configurable;
 import org.hibernate.service.spi.Stoppable;
 
-import java.net.URI;
-import java.util.Map;
+import io.vertx.core.Vertx;
+import io.vertx.db2client.DB2ConnectOptions;
+import io.vertx.db2client.DB2Pool;
+import io.vertx.mysqlclient.MySQLConnectOptions;
+import io.vertx.mysqlclient.MySQLPool;
+import io.vertx.pgclient.PgConnectOptions;
+import io.vertx.pgclient.PgPool;
+import io.vertx.sqlclient.Pool;
+import io.vertx.sqlclient.PoolOptions;
 
 /**
  * A pool of reactive connections backed by a Vert.x {@link Pool}.
@@ -59,10 +62,21 @@ public class ReactiveConnectionPoolProviderImpl implements ReactiveConnectionPoo
 
 		final String url = ConfigurationHelper.getString(AvailableSettings.URL, configurationValues);
 		final URI uri = JdbcUrlParser.parse( url );
-		final String database = uri.getPath().substring( 1 );
+		String database = uri.getPath().substring( 1 );
+		if (uri.getScheme().equals("db2") && database.indexOf( ':' ) > 0) {
+			database = database.substring( 0, database.indexOf( ':' ) );
+		}
 
 		if (username==null || password==null) {
-			String[] params = uri.getQuery().split("&");
+			String[] params = {};
+			if (uri.getScheme().equals("db2")) {
+			    int queryIndex = uri.getPath().indexOf(':') + 1;
+			    if (queryIndex > 0) {
+			      params = uri.getPath().substring(queryIndex).split(";");
+			    }
+			} else {
+			    params = uri.getQuery().split("&");
+			}
 			for (String param : params) {
 				if ( param.startsWith("user=") ) {
 					username = param.substring(5);
@@ -99,6 +113,16 @@ public class ReactiveConnectionPoolProviderImpl implements ReactiveConnectionPoo
 					mysqlOptions.setPassword( password );
 				}
 				return MySQLPool.pool(Vertx.vertx(), mysqlOptions, poolOptions);
+			case "db2":
+			    DB2ConnectOptions db2Options = new DB2ConnectOptions()
+			            .setPort( uri.getPort() )
+			            .setHost( uri.getHost() )
+			            .setDatabase( database )
+			            .setUser( username );
+			    if (password != null) {
+			        db2Options.setPassword( password );
+			    }
+			    return DB2Pool.pool(Vertx.vertx(), db2Options, poolOptions);
 			default:
 				throw new ConfigurationException( "Unrecognized URI scheme: " + uri.getScheme() );	
 		}
