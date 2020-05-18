151174a738cc50c38a31d65a35582e2c2ec631ba
gavinking <gavin@ceylon-lang.org>
2020-05-18 20:25:35 +0200

    fix up the session closing in example

--- example/src/main/java/org/hibernate/example/rx/Main.java
+++ example/src/main/java/org/hibernate/example/rx/Main.java
@@ -35,19 +35,21 @@ public class Main {
 				.toCompletableFuture()
 				.join();
 
-		RxSession session4 = sessionFactory.openRxSession();
+		RxSession session3 = sessionFactory.openRxSession();
 		//query the Book titles
-		session4.createQuery("select title from Book order by title desc").getResultList()
+		session3.createQuery("select title from Book order by title desc")
+				.getResultList()
 				.thenAccept(System.out::println)
+				.thenAccept( $ -> session3.close() )
 				.toCompletableFuture()
 				.join();
 
-		RxSession session3 = sessionFactory.openRxSession();
+		RxSession session4 = sessionFactory.openRxSession();
 		//retrieve the Book and delete it
-		session3.find(Book.class, book.id)
-				.thenCompose( bOOk -> session3.remove(bOOk))
-				.thenCompose( $ -> session3.flush() )
-				.thenAccept( $ -> session2.close() )
+		session4.find(Book.class, book.id)
+				.thenCompose( bOOk -> session4.remove(bOOk) )
+				.thenCompose( $ -> session4.flush() )
+				.thenAccept( $ -> session4.close() )
 				.toCompletableFuture()
 				.join();
 
