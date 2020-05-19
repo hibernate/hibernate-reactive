5fb28b5d554baeb4dfafcb20b4ed760d4afafc79
gavinking <gavin@ceylon-lang.org>
2020-05-19 23:14:35 +0200

    no need to drop the schema after each test

--- hibernate-reactive-core/src/test/java/org/hibernate/reactive/BaseReactiveTest.java
+++ hibernate-reactive-core/src/test/java/org/hibernate/reactive/BaseReactiveTest.java
@@ -46,7 +46,7 @@ public abstract class BaseReactiveTest {
 
 	protected Configuration constructConfiguration() {
 		Configuration configuration = new Configuration();
-		configuration.setProperty( AvailableSettings.HBM2DDL_AUTO, "create-drop" );
+		configuration.setProperty( AvailableSettings.HBM2DDL_AUTO, "create" );
 		configuration.setProperty( AvailableSettings.URL, PostgreSQLDatabase.getJdbcUrl() );
 		configuration.setProperty( AvailableSettings.SHOW_SQL, "true" );
 		return configuration;
