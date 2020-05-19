f44f02c014d12b62bb35bddc7e24a33c113ff7fe
Andrew Guibert <andy.guibert@gmail.com>
2020-05-19 00:50:06 -0500

    Remove the need for any JDBC drivers as test dependencies

---
+++ hibernate-rx-core/src/test/java/org/hibernate/rx/containers/VertxMySqlContainer.java
@@ -0,0 +1,99 @@
+package org.hibernate.rx.containers;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+
+import org.testcontainers.containers.ContainerLaunchException;
+import org.testcontainers.containers.MySQLContainer;
+
+import io.vertx.core.AsyncResult;
+import io.vertx.mysqlclient.MySQLPool;
+import io.vertx.sqlclient.SqlConnection;
+
+/**
+ * Normally the MySQLContainer checks for readiness by attempting to establish a JDBC connection
+ * and seeing if it can run a test query. We need to override this default behavior so that we can
+ * keep all JDBC drivers off of our test classpath.
+ * This is not an issue for other containers (PostgreSQL and DB2) because they use container log scraping
+ * by default.
+ */
+public class VertxMySqlContainer extends MySQLContainer<VertxMySqlContainer> {
+	
+    @Override
+    protected void waitUntilContainerStarted() {
+        logger().info( "Waiting for database connection to become available at {} using query '{}'", getVertxUrl(), getTestQueryString());
+
+        // Repeatedly try and open a connection to the DB and execute a test query
+        long start = System.currentTimeMillis();
+        try {
+            while (System.currentTimeMillis() < start + (1000 * getStartupTimeoutSeconds())) {
+                try {
+                    if (!isRunning()) {
+                        Thread.sleep(100L);
+                        continue; // Don't attempt to connect yet
+                    }
+                    
+                    AtomicReference<AsyncResult<?>> result = new AtomicReference<>();
+                    CountDownLatch countDown = new CountDownLatch( 1 );
+                    SqlConnection connection = createVertxConnection();
+                    connection.query( getTestQueryString() ).execute( ar -> {
+                    	result.set( ar );
+                    	countDown.countDown();
+                    });
+                    await(countDown);
+                    if (result.get().succeeded()) {
+                    	break;
+                    } else {
+                    	throw result.get().cause();
+                    }
+                } catch (Throwable e) {
+                    // ignore so that we can try again
+                    logger().debug("Failure when trying test query", e);
+                    Thread.sleep(100L);
+                }
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new ContainerLaunchException("Container startup wait was interrupted", e);
+        }
+
+        logger().info("Container is started (URL: {})", getVertxUrl());
+    }
+    
+    public String getVertxUrl() {
+    	return MySQLDatabase.buildUrlWithCredentials(getJdbcUrl().replace( "jdbc:", "" ));
+    }
+    
+    public SqlConnection createVertxConnection() {
+    	String url = getVertxUrl();
+    	CountDownLatch countDown = new CountDownLatch( 1 );
+    	AtomicReference<AsyncResult<SqlConnection>> result = new AtomicReference<>();
+    	MySQLPool.pool( url ).getConnection( ar -> {
+    		result.set( ar );
+    		countDown.countDown();
+    	});
+    	await(countDown);
+    	SqlConnection con = result.get().result();
+    	if (con != null)
+    		return con;
+    	else
+    		throw new ContainerLaunchException( "Failed to obtain a connection", result.get().cause() );
+    }
+    
+    @Override
+    public Connection createConnection(String queryString) throws SQLException, NoDriverFoundException {
+    	throw new UnsupportedOperationException();
+    }
+    
+    private void await(CountDownLatch latch) {
+    	try {
+    		latch.await( getStartupTimeoutSeconds(), TimeUnit.SECONDS );
+		} catch (InterruptedException e) {
+			throw new ContainerLaunchException("Container startup wait was interrupted", e);
+		}
+    }
+
+}
