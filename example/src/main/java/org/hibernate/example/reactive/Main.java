ccb76aab271f29d5e4bd84984d0446f3953d0692
gavinking <gavin@ceylon-lang.org>
2020-05-25 15:16:30 +0200

    proper use of generated metamodel in example code

--- example/src/main/java/org/hibernate/example/reactive/Main.java
+++ example/src/main/java/org/hibernate/example/reactive/Main.java
@@ -97,7 +97,7 @@ public class Main {
 					CriteriaQuery<Book> query = sessionFactory.getCriteriaBuilder().createQuery(Book.class);
 					Root<Author> a = query.from(Author.class);
 					Join<Author,Book> b = a.join(Author_.books);
-					query.where( a.get("name").in("Neal Stephenson", "William Gibson") );
+					query.where( a.get(Author_.name).in("Neal Stephenson", "William Gibson") );
 					query.select(b);
 					return session.createQuery(query).getResultList().thenAccept(
 							books -> books.forEach( book -> out.println(book.title) )
