faf93f8aae2811b7d1974c8fcea5e0510657c761
Sanne Grinovero <sanne@hibernate.org>
2020-05-27 23:03:59 +0100

    Fold the custom ServiceContributor into our custom ReactiveServiceInitiators

--- hibernate-reactive-core/src/main/java/org/hibernate/reactive/boot/impl/ReactiveServiceInitiators.java
+++ hibernate-reactive-core/src/main/java/org/hibernate/reactive/boot/impl/ReactiveServiceInitiators.java
@@ -12,23 +12,24 @@ import org.hibernate.bytecode.internal.ProxyFactoryFactoryInitiator;
 import org.hibernate.cache.internal.RegionFactoryInitiator;
 import org.hibernate.engine.config.internal.ConfigurationServiceInitiator;
 import org.hibernate.engine.jdbc.batch.internal.BatchBuilderInitiator;
-import org.hibernate.engine.jdbc.connections.internal.ConnectionProviderInitiator;
 import org.hibernate.engine.jdbc.connections.internal.MultiTenantConnectionProviderInitiator;
 import org.hibernate.engine.jdbc.cursor.internal.RefCursorSupportInitiator;
 import org.hibernate.engine.jdbc.dialect.internal.DialectFactoryInitiator;
 import org.hibernate.engine.jdbc.dialect.internal.DialectResolverInitiator;
-import org.hibernate.engine.jdbc.env.internal.JdbcEnvironmentInitiator;
 import org.hibernate.engine.jdbc.internal.JdbcServicesInitiator;
 import org.hibernate.engine.jndi.internal.JndiServiceInitiator;
-import org.hibernate.engine.transaction.jta.platform.internal.JtaPlatformInitiator;
-import org.hibernate.engine.transaction.jta.platform.internal.JtaPlatformResolverInitiator;
 import org.hibernate.event.internal.EntityCopyObserverFactoryInitiator;
-import org.hibernate.hql.internal.QueryTranslatorFactoryInitiator;
-import org.hibernate.id.factory.internal.MutableIdentifierGeneratorFactoryInitiator;
 import org.hibernate.jmx.internal.JmxServiceInitiator;
-import org.hibernate.persister.internal.PersisterClassResolverInitiator;
 import org.hibernate.persister.internal.PersisterFactoryInitiator;
 import org.hibernate.property.access.internal.PropertyAccessStrategyResolverInitiator;
+import org.hibernate.reactive.boot.service.NoJdbcConnectionProviderInitiator;
+import org.hibernate.reactive.boot.service.NoJdbcEnvironmentInitiator;
+import org.hibernate.reactive.boot.service.NoJtaPlatformInitiator;
+import org.hibernate.reactive.boot.service.ReactiveQueryTranslatorFactoryInitiator;
+import org.hibernate.reactive.id.impl.ReactiveIdentifierGeneratorFactoryInitiator;
+import org.hibernate.reactive.jpa.impl.ReactivePersisterClassResolverInitiator;
+import org.hibernate.reactive.pool.impl.SqlClientPoolInitiator;
+import org.hibernate.reactive.vertx.impl.VertxInstanceInitiator;
 import org.hibernate.resource.beans.spi.ManagedBeanRegistryInitiator;
 import org.hibernate.resource.transaction.internal.TransactionCoordinatorBuilderInitiator;
 import org.hibernate.service.internal.SessionFactoryServiceRegistryFactoryInitiator;
@@ -49,6 +50,13 @@ public final class ReactiveServiceInitiators {
     private static List<StandardServiceInitiator> buildInitialServiceInitiatorList() {
         final ArrayList<StandardServiceInitiator> serviceInitiators = new ArrayList<StandardServiceInitiator>();
 
+        // Exclusive to Hibernate Reactive:
+        serviceInitiators.add( VertxInstanceInitiator.INSTANCE );
+
+        // Exclusive to Hibernate Reactive:
+        serviceInitiators.add( SqlClientPoolInitiator.INSTANCE );
+
+        //Custom for Hibernate Reactive:
         serviceInitiators.add( ReactiveSessionFactoryBuilderInitiator.INSTANCE );
 
         serviceInitiators.add( BytecodeProviderInitiator.INSTANCE );
@@ -61,14 +69,19 @@ public final class ReactiveServiceInitiators {
         serviceInitiators.add( ImportSqlCommandExtractorInitiator.INSTANCE );
         serviceInitiators.add( SchemaManagementToolInitiator.INSTANCE );
 
-        serviceInitiators.add( JdbcEnvironmentInitiator.INSTANCE );
+        //Custom for Hibernate Reactive:
+        serviceInitiators.add( NoJdbcEnvironmentInitiator.INSTANCE );
+
         serviceInitiators.add( JndiServiceInitiator.INSTANCE );
         serviceInitiators.add( JmxServiceInitiator.INSTANCE );
 
-        serviceInitiators.add( PersisterClassResolverInitiator.INSTANCE );
+        //Custom for Hibernate Reactive:
+        serviceInitiators.add( ReactivePersisterClassResolverInitiator.INSTANCE );
         serviceInitiators.add( PersisterFactoryInitiator.INSTANCE );
 
-        serviceInitiators.add( ConnectionProviderInitiator.INSTANCE );
+        //Custom for Hibernate Reactive:
+        serviceInitiators.add( NoJdbcConnectionProviderInitiator.INSTANCE );
+
         serviceInitiators.add( MultiTenantConnectionProviderInitiator.INSTANCE );
         serviceInitiators.add( DialectResolverInitiator.INSTANCE );
         serviceInitiators.add( DialectFactoryInitiator.INSTANCE );
@@ -76,11 +89,15 @@ public final class ReactiveServiceInitiators {
         serviceInitiators.add( JdbcServicesInitiator.INSTANCE );
         serviceInitiators.add( RefCursorSupportInitiator.INSTANCE );
 
-        serviceInitiators.add( QueryTranslatorFactoryInitiator.INSTANCE );
-        serviceInitiators.add( MutableIdentifierGeneratorFactoryInitiator.INSTANCE);
+        //Custom for Hibernate Reactive:
+        serviceInitiators.add( ReactiveQueryTranslatorFactoryInitiator.INSTANCE );
 
-        serviceInitiators.add( JtaPlatformResolverInitiator.INSTANCE );
-        serviceInitiators.add( JtaPlatformInitiator.INSTANCE );
+        //Custom for Hibernate Reactive:
+        serviceInitiators.add( ReactiveIdentifierGeneratorFactoryInitiator.INSTANCE);
+
+
+        //Custom for Hibernate Reactive:
+        serviceInitiators.add( NoJtaPlatformInitiator.INSTANCE );
 
         serviceInitiators.add( SessionFactoryServiceRegistryFactoryInitiator.INSTANCE );
 
