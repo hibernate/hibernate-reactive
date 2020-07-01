aaaa7e67045a8931c30a4247fc506f1a8157886d
Andrew Guibert <andy.guibert@gmail.com>
2020-07-01 17:32:01 -0500

    Enable prepared statement caching for DB2

--- hibernate-reactive-core/src/main/java/org/hibernate/reactive/pool/impl/SqlClientPool.java
+++ hibernate-reactive-core/src/main/java/org/hibernate/reactive/pool/impl/SqlClientPool.java
@@ -176,8 +176,7 @@ public class SqlClientPool implements ReactiveConnectionPool, ServiceRegistryAwa
 			connectOptions.setPassword( password );
 		}
 
-		//enable the prepared statement cache by default (except for DB2)
-		connectOptions.setCachePreparedStatements( !scheme.equals("db2") );
+		connectOptions.setCachePreparedStatements( true );
 
 		final Integer cacheMaxSize = ConfigurationHelper.getInteger( Settings.PREPARED_STATEMENT_CACHE_MAX_SIZE, configurationValues );
 		if (cacheMaxSize!=null) {
