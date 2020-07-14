6858c8c0fa5032dc0f3cd39aeb0912a8300311ff
Davide D'Alto <davide@hibernate.org>
2020-07-14 09:32:34 +0100

    [#280] Test unique constraint violation exception
      We expect a PersitenceException.

---
+++ hibernate-reactive-core/src/test/java/org/hibernate/reactive/MutinyExceptionsTest.java
@@ -0,0 +1,81 @@
+/* Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ * Copyright: Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.reactive;
+
+import java.util.concurrent.CompletionException;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.PersistenceException;
+
+import org.hibernate.cfg.AvailableSettings;
+import org.hibernate.cfg.Configuration;
+
+import org.junit.Test;
+
+import io.vertx.ext.unit.TestContext;
+
+public class MutinyExceptionsTest extends BaseMutinyTest {
+
+	@Override
+	protected Configuration constructConfiguration() {
+		Configuration configuration = super.constructConfiguration();
+		configuration.setProperty( AvailableSettings.NATIVE_EXCEPTION_HANDLING_51_COMPLIANCE, "false" );
+		configuration.addAnnotatedClass( Person.class );
+		return configuration;
+	}
+
+	@Test
+	public void testDuplicateKeyException(TestContext context) {
+		test( context, openSession()
+				.onItem().produceUni( session -> session.persist( new Person( "testFLush1", "unique" ) ) )
+				.onItem().produceUni( session -> session.flush() )
+				.onItem().produceUni( session -> session.persist( new Person( "testFlush2", "unique" ) ) )
+				.onItem().produceUni( session -> session.flush() )
+				.onItem().invoke( ignore -> context.fail( "Expected exception not thrown" ) )
+				.onFailure().recoverWithItem( err -> {
+					context.assertEquals( CompletionException.class, err.getClass() );
+					context.assertTrue( err.getCause() instanceof PersistenceException
+							, "Expected instance of " + PersistenceException.class.getName() + " but was " + err.getCause().getClass() );
+					return null;
+				} )
+		);
+	}
+
+	@Entity
+	public static class Person {
+
+		@Id
+		public String name;
+
+		@Column(unique = true)
+		public String uniqueName;
+
+		public Person() {
+		}
+
+		public Person(String name, String uniqueName) {
+			this.name = name;
+			this.uniqueName = uniqueName;
+		}
+
+		@Override
+		public String toString() {
+			return name + ", " + uniqueName;
+		}
+	}
+
+	// I don't think we need to support this case but at the moment it would require more work to
+	// disable the behaviour.
+	public static class Native51ExceptionHandlingTest extends MutinyExceptionsTest {
+		@Override
+		protected Configuration constructConfiguration() {
+			Configuration configuration = super.constructConfiguration();
+			configuration.setProperty( AvailableSettings.NATIVE_EXCEPTION_HANDLING_51_COMPLIANCE, "true" );
+			return configuration;
+		}
+	}
+}
---
+++ hibernate-reactive-core/src/test/java/org/hibernate/reactive/StageExceptionsTest.java
@@ -0,0 +1,84 @@
+/* Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ * Copyright: Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.reactive;
+
+import java.util.concurrent.CompletionException;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.PersistenceException;
+
+import org.hibernate.cfg.AvailableSettings;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.reactive.provider.Settings;
+import org.hibernate.reactive.util.impl.CompletionStages;
+
+import org.junit.Test;
+
+import io.vertx.ext.unit.TestContext;
+
+public class StageExceptionsTest extends BaseReactiveTest {
+
+	@Override
+	protected Configuration constructConfiguration() {
+		Configuration configuration = super.constructConfiguration();
+		configuration.setProperty( Settings.NATIVE_EXCEPTION_HANDLING_51_COMPLIANCE, "false" );
+		configuration.addAnnotatedClass( MyPerson.class );
+		return configuration;
+	}
+
+	@Test
+	public void testDuplicateKeyException(TestContext context) {
+		final Class<PersistenceException> expectedException = PersistenceException.class;
+
+		test( context, openSession()
+				.thenCompose( session -> session.persist( new MyPerson( "testFLush1", "unique" ) ) )
+				.thenCompose( session -> session.flush() )
+				.thenCompose( session -> session.persist( new MyPerson( "testFlush2", "unique" ) ) )
+				.thenCompose( session -> session.flush() )
+			  	.handle( (res, err) -> {
+			  				context.assertNotNull( err );
+			  				context.assertTrue( err.getClass().isAssignableFrom( CompletionException.class) );
+							context.assertTrue( expectedException.isAssignableFrom( err.getCause().getClass() ), "Expected " + expectedException.getName() + " but was " + err );
+							return CompletionStages.nullFuture();
+			  	} )
+		);
+	}
+
+	@Entity
+	public class MyPerson {
+
+		@Id
+		public String name;
+
+		@Column(unique = true)
+		public String uniqueName;
+
+		public MyPerson() {
+		}
+
+		public MyPerson(String name, String uniqueName) {
+			this.name = name;
+			this.uniqueName = uniqueName;
+		}
+
+		@Override
+		public String toString() {
+			return name + ", " + uniqueName;
+		}
+	}
+
+	// I don't think we need to support this case but at the moment it would require more work to
+	// disable the behaviour.
+	public static class Native51ExceptionHandlingTest extends StageExceptionsTest {
+		@Override
+		protected Configuration constructConfiguration() {
+			Configuration configuration = super.constructConfiguration();
+			configuration.setProperty( AvailableSettings.NATIVE_EXCEPTION_HANDLING_51_COMPLIANCE, "true" );
+			return configuration;
+		}
+	}
+}
