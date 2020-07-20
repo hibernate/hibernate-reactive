fef0d51300e68c43911b970e77fa57c40ca8a63a
Davide D'Alto <davide@hibernate.org>
2020-07-20 09:53:25 +0100

    [#288] Test GenerationType.IDENTITY
      Replace deleted AutoIncrementTest.

---
+++ hibernate-reactive-core/src/test/java/org/hibernate/reactive/IdentityGeneratorTest.java
@@ -0,0 +1,92 @@
+/* Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ * Copyright: Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.reactive;
+
+import java.util.concurrent.CompletionStage;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.cfg.Configuration;
+
+import org.junit.Test;
+
+import io.vertx.ext.unit.TestContext;
+
+public class IdentityGeneratorTest extends BaseReactiveTest {
+
+	// The number of entities we want to create
+	private static final int ENTITY_NUMBER = 100;
+
+	@Override
+	protected org.hibernate.cfg.Configuration constructConfiguration() {
+		Configuration configuration = super.constructConfiguration();
+		configuration.addAnnotatedClass( EntityWithIdentity.class );
+		return configuration;
+	}
+
+	private CompletionStage<?> populateDb() {
+		final Object[] identities = new EntityWithIdentity[ENTITY_NUMBER];
+		for ( int i = 0; i < ENTITY_NUMBER; i++ ) {
+			identities[i] = new EntityWithIdentity( i );
+		}
+		return getSessionFactory().withTransaction( (session, tx) -> session.persist( identities ) );
+	}
+
+	@Test
+	public void testIdentityGenerator(TestContext context) {
+		test( context, populateDb()
+				.thenCompose( $ -> openSession() )
+				.thenCompose( session ->
+					  session.createQuery( "FROM EntityWithIdentity ORDER BY position ASC", EntityWithIdentity.class )
+					  .getResultList() )
+				.thenAccept( list -> {
+					context.assertEquals( ENTITY_NUMBER, list.size() );
+					int i = 0;
+					for ( EntityWithIdentity entity : list ) {
+						context.assertEquals( i, entity.getPosition() );
+						i++;
+					}
+				} ) );
+	}
+
+	@Entity(name = "EntityWithIdentity")
+	private static class EntityWithIdentity {
+		private static final String PREFIX = "Entity: ";
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		Long id;
+
+		@Column(unique = true)
+		String name;
+
+		@Column
+		private int position;
+
+		public EntityWithIdentity() {
+		}
+
+		public EntityWithIdentity(int index) {
+			this.name =  PREFIX + index;
+			this.position = index;
+		}
+
+		public int getPosition() {
+			return position;
+		}
+
+		public void setPosition(int position) {
+			this.position = position;
+		}
+
+		@Override
+		public String toString() {
+			return id + ":" + name + ":" + position;
+		}
+	}
+}
