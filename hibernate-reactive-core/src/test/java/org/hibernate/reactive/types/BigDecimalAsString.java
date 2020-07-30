49bae910d430a462eac30a24081d7b3b0cb89e7a
Gavin King <gavin@hibernate.org>
2020-07-30 11:53:21 +0200

    add test for UserType mappings
    see #279

---
+++ hibernate-reactive-core/src/test/java/org/hibernate/reactive/types/BigDecimalAsString.java
@@ -0,0 +1,82 @@
+/* Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ * Copyright: Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.reactive.types;
+
+import org.hibernate.HibernateException;
+import org.hibernate.engine.spi.SharedSessionContractImplementor;
+import org.hibernate.usertype.UserType;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+import java.util.Objects;
+
+public class BigDecimalAsString implements UserType {
+
+    @Override
+    public int[] sqlTypes() {
+        return new int[] {Types.VARCHAR};
+    }
+
+    @Override
+    public Class returnedClass() {
+        return BigDecimal.class;
+    }
+
+    @Override
+    public boolean equals(Object x, Object y) throws HibernateException {
+        return Objects.equals(x,y);
+    }
+
+    @Override
+    public int hashCode(Object x) throws HibernateException {
+        return Objects.hashCode(x);
+    }
+
+    @Override
+    public Object nullSafeGet(ResultSet rs, String[] names, SharedSessionContractImplementor session, Object owner) throws HibernateException, SQLException {
+        String string = rs.getString(names[0]);
+        return string == null || rs.wasNull() ? null : new BigDecimal(string);
+    }
+
+    @Override
+    public void nullSafeSet(PreparedStatement st, Object value, int index, SharedSessionContractImplementor session) throws HibernateException, SQLException {
+        if (value==null) {
+            st.setNull(index, Types.VARCHAR);
+        }
+        else {
+            st.setString(index, value.toString());
+        }
+    }
+
+    @Override
+    public Object deepCopy(Object value) throws HibernateException {
+        return value;
+    }
+
+    @Override
+    public boolean isMutable() {
+        return false;
+    }
+
+    @Override
+    public Serializable disassemble(Object value) throws HibernateException {
+        return (BigDecimal) value;
+    }
+
+    @Override
+    public Object assemble(Serializable cached, Object owner) throws HibernateException {
+        return cached;
+    }
+
+    @Override
+    public Object replace(Object original, Object target, Object owner) throws HibernateException {
+        return original;
+    }
+}
