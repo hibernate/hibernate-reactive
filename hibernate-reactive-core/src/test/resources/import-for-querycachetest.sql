dd962317178029b5a6ec08c5aeaace8887c2ab1a
Davide D'Alto <davide@hibernate.org>
2020-09-18 14:02:01 +0100

    [#247] Test case

---
+++ hibernate-reactive-core/src/test/resources/import-for-querycachetest.sql
@@ -0,0 +1,4 @@
+-- This import is required for QueryCacheInternalLoadTest
+INSERT INTO known_fruits(id, name) VALUES (1, 'Banana');
+INSERT INTO known_fruits(id, name) VALUES (2, 'Tomato');
+INSERT INTO known_fruits(id, name) VALUES (3, 'Pineapple');
