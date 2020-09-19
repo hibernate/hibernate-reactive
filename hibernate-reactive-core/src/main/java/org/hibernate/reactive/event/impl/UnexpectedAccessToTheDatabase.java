ae1edda74b7e446e7521e92b3f46b9ad88fbedf1
Davide D'Alto <davide@hibernate.org>
2020-09-19 06:53:49 +0100

    [#247] Add UnexpectedAccessToTheDatabase exception
      So far we used AssertionFailure instead.
      The problem with it is that it logs an error every time
      an AssertionFailure is created.
    
      Sometimes we want to deal with the issue quietly.

---
+++ hibernate-reactive-core/src/main/java/org/hibernate/reactive/event/impl/UnexpectedAccessToTheDatabase.java
@@ -0,0 +1,24 @@
+/* Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ * Copyright: Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.reactive.event.impl;
+
+/**
+ * For situation where we don't excpect to access the database,
+ * <p>
+ *     For Reactive, this usually means that we are returning a {@link java.util.concurrent.CompletionStage}
+ *     and we are not ready to handle it.
+ * </p>
+ */
+public class UnexpectedAccessToTheDatabase extends RuntimeException {
+
+	public UnexpectedAccessToTheDatabase() {
+		this( "Unexpected access to the database" );
+	}
+
+	public UnexpectedAccessToTheDatabase(String message) {
+		super( message );
+	}
+}
