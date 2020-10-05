cef82fa4a26745ecdfa9994e5f290059aebb4c67
blafond <blafond@redhat.com>
2020-10-05 09:09:39 -0500

    common orm and reactive persistence tests #367

---
+++ hibernate-reactive-core/src/test/java/org/hibernate/reactive/ORMReactivePersistenceTest.java
@@ -0,0 +1,182 @@
+/* Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ * Copyright: Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.reactive;
+
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.reactive.mutiny.Mutiny;
+import org.hibernate.reactive.provider.Settings;
+import org.hibernate.reactive.stage.Stage;
+import org.hibernate.reactive.testing.DatabaseSelectionRule;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import io.vertx.ext.unit.TestContext;
+
+import static org.hibernate.reactive.containers.DatabaseConfiguration.DBType.POSTGRESQL;
+import static org.hibernate.reactive.util.impl.CompletionStages.completedFuture;
+
+/**
+ * This test class verifies that data can be persisted and queried on the same database
+ * using both JPA/hibernate and reactive session factories.
+ */
+public class ORMReactivePersistenceTest extends BaseReactiveTest {
+
+	@Rule
+	public DatabaseSelectionRule rule = DatabaseSelectionRule.runOnlyFor( POSTGRESQL );
+
+	private SessionFactory ormFactory;
+
+	@Override
+	protected Configuration constructConfiguration() {
+		Configuration configuration = super.constructConfiguration();
+		configuration.addAnnotatedClass( Flour.class );
+		return configuration;
+	}
+
+	@Before
+	public void prepareOrmFactory() {
+		Configuration configuration = constructConfiguration();
+		configuration.setProperty( Settings.DRIVER, "org.postgresql.Driver" );
+		configuration.setProperty( Settings.DIALECT, "org.hibernate.dialect.PostgreSQL95Dialect");
+
+		StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
+				.applySettings( configuration.getProperties() );
+
+		StandardServiceRegistry registry = builder.build();
+		ormFactory = configuration.buildSessionFactory( registry );
+	}
+
+	@After
+	public void cleanDb(TestContext context) {
+		ormFactory.close();
+
+		test( context, completedFuture( openSession() )
+				.thenCompose( s -> s.createQuery( "delete Flour" )
+						.executeUpdate() ) );
+	}
+
+	@Test
+	public void testORMWithStageSession(TestContext context) {
+		final Flour almond = new Flour( 1, "Almond", "made from ground almonds.", "Gluten free" );
+
+		Session session = ormFactory.openSession();
+		session.beginTransaction();
+		session.persist( almond );
+		session.getTransaction().commit();
+		session.close();
+
+		// Check database with Stage session and verify 'almond' flour exists
+		Stage.Session stageSession = openSession();
+		test( context, stageSession.find( Flour.class, almond.id )
+				.thenAccept( entityFound -> context.assertEquals( almond, entityFound ) )
+		);
+	}
+
+	@Test
+	public void testORMWitMutinySession(TestContext context) {
+		final Flour rose = new Flour( 2, "Rose", "made from ground rose pedals.", "Full fragrance" );
+
+		Session ormSession = ormFactory.openSession();
+		ormSession.beginTransaction();
+		ormSession.persist( rose );
+		ormSession.getTransaction().commit();
+		ormSession.close();
+
+		// Check database with Mutiny session and verify 'rose' flour exists
+		Mutiny.Session mutinySession = openMutinySession();
+		test( context, mutinySession.find( Flour.class, rose.id)
+				.invoke( foundRose -> context.assertEquals( rose, foundRose ) )
+		);
+	}
+
+	@Entity(name = "Flour")
+	@Table(name = "Flour")
+	public static class Flour {
+		@Id
+		private Integer id;
+		private String name;
+		private String description;
+		private String type;
+
+		public Flour() {
+		}
+
+		public Flour(Integer id, String name, String description, String type) {
+			this.id = id;
+			this.name = name;
+			this.description = description;
+			this.type = type;
+		}
+
+		public Integer getId() {
+			return id;
+		}
+
+		public void setId(Integer id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getDescription() {
+			return description;
+		}
+
+		public void setDescription(String description) {
+			this.description = description;
+		}
+
+		public String getType() {
+			return type;
+		}
+
+		public void setType(String type) {
+			this.type = type;
+		}
+
+		@Override
+		public String toString() {
+			return name;
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if ( this == o ) {
+				return true;
+			}
+			if ( o == null || getClass() != o.getClass() ) {
+				return false;
+			}
+			Flour flour = (Flour) o;
+			return Objects.equals( name, flour.name ) &&
+					Objects.equals( description, flour.description ) &&
+					Objects.equals( type, flour.type );
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash( name, description, type );
+		}
+	}
+}
