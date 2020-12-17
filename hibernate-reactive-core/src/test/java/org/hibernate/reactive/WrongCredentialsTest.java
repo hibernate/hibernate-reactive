2c9b83cfa0985bf4ca7b920d3ac8113090a5873b
Davide D'Alto <davide@hibernate.org>
2020-12-17 17:02:30 +0000

    [#494] Test behaviour when configuration has wrong credentials

---
+++ hibernate-reactive-core/src/test/java/org/hibernate/reactive/WrongCredentialsTest.java
@@ -0,0 +1,68 @@
+/* Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ * Copyright: Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.reactive;
+
+import javax.persistence.Id;
+
+import org.hibernate.cfg.Configuration;
+import org.hibernate.reactive.provider.Settings;
+import org.hibernate.reactive.testing.DatabaseSelectionRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import io.vertx.ext.unit.TestContext;
+
+import static org.hibernate.reactive.containers.DatabaseConfiguration.DBType.POSTGRESQL;
+
+/**
+ * Check that the right exception is thrown when there is an error with the credentials.
+ * <p>
+ *     Similar to {@link org.hibernate.reactive.configuration.ReactiveConnectionPoolTest} but at the session level.
+ *     Note that the wrong configuration is also used for the schema generation but ORM catch the exception and
+ *     continue anyway it in case of errors. You might just see sine warnings in the log.
+ * </p>
+ */
+public class WrongCredentialsTest extends BaseReactiveTest {
+
+	// I've decided to not test it for all databases because
+	// the error message might be different and it shouldn't be necessary
+	@Rule
+	public DatabaseSelectionRule selectionRule = DatabaseSelectionRule.runOnlyFor( POSTGRESQL );
+
+	@Override
+	protected Configuration constructConfiguration() {
+		Configuration configuration = super.constructConfiguration();
+		configuration.setProperty( Settings.USER, "BogusBogus" );
+		configuration.setProperty( Settings.PASS, "BogusBogus" );
+		return configuration;
+	}
+
+	@Test
+	public void testWithTransaction(TestContext context) {
+		test( context, getSessionFactory()
+				// The error will occur before the find even get executed
+				.withTransaction( (s, t) -> s
+						.find( Artist.class, "Bansky" ) )
+				.handle( (v, e) -> {
+					context.assertNotNull( e );
+					context.assertTrue( e.getMessage().contains( "BogusBogus" ) );
+					return null;
+				} ) );
+	}
+
+	static class Artist {
+		@Id
+		String name;
+
+		public Artist() {
+		}
+
+		public Artist(String name) {
+			this.name = name;
+		}
+	}
+}
