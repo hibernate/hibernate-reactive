d131bb2b38662b3bc64ee88faae42a9c223e5a70
Davide D'Alto <davide@hibernate.org>
2021-01-11 14:55:14 +0000

    Update WrongCredentialsTest
      Test exception for each db and test with Mutiny

--- hibernate-reactive-core/src/test/java/org/hibernate/reactive/WrongCredentialsTest.java
+++ hibernate-reactive-core/src/test/java/org/hibernate/reactive/WrongCredentialsTest.java
@@ -5,16 +5,20 @@
  */
 package org.hibernate.reactive;
 
+import java.util.concurrent.CompletionException;
+import javax.persistence.Entity;
 import javax.persistence.Id;
 
 import org.hibernate.cfg.Configuration;
+import org.hibernate.reactive.containers.DatabaseConfiguration;
 import org.hibernate.reactive.provider.Settings;
-import org.hibernate.reactive.testing.DatabaseSelectionRule;
 
-import org.junit.Rule;
 import org.junit.Test;
 
+import io.vertx.db2client.DB2Exception;
 import io.vertx.ext.unit.TestContext;
+import io.vertx.mysqlclient.MySQLException;
+import io.vertx.pgclient.PgException;
 
 import static org.hibernate.reactive.containers.DatabaseConfiguration.DBType.POSTGRESQL;
 
@@ -23,37 +27,79 @@ import static org.hibernate.reactive.containers.DatabaseConfiguration.DBType.POS
  * <p>
  *     Similar to {@link org.hibernate.reactive.configuration.ReactiveConnectionPoolTest} but at the session level.
  *     Note that the wrong configuration is also used for the schema generation but ORM catch the exception and
- *     continue anyway it in case of errors. You might just see sine warnings in the log.
+ *     continue anyway it in case of errors. You might just see some warnings in the log.
  * </p>
  */
 public class WrongCredentialsTest extends BaseReactiveTest {
 
-	// I've decided to not test it for all databases because
-	// the error message might be different and it shouldn't be necessary
-	@Rule
-	public DatabaseSelectionRule selectionRule = DatabaseSelectionRule.runOnlyFor( POSTGRESQL );
-
 	@Override
 	protected Configuration constructConfiguration() {
 		Configuration configuration = super.constructConfiguration();
+		// We need to set both credentials or they are going to be ignored
+		// See DefaultSqlClientPoolConfiguration
 		configuration.setProperty( Settings.USER, "BogusBogus" );
 		configuration.setProperty( Settings.PASS, "BogusBogus" );
+		configuration.addAnnotatedClass( Artist.class );
 		return configuration;
 	}
 
 	@Test
-	public void testWithTransaction(TestContext context) {
+	public void testStageWithTransaction(TestContext context) {
 		test( context, getSessionFactory()
 				// The error will occur before the find even get executed
-				.withTransaction( (s, t) -> s
-						.find( Artist.class, "Bansky" ) )
+				.withTransaction( (s, t) -> s.find( Artist.class, "Bansky" ) )
 				.handle( (v, e) -> {
 					context.assertNotNull( e );
-					context.assertTrue( e.getMessage().contains( "BogusBogus" ) );
+					context.assertEquals( CompletionException.class, e.getClass() );
+					switch ( DatabaseConfiguration.dbType() ) {
+						case DB2:
+							context.assertEquals( DB2Exception.class, e.getCause().getClass()  );
+							context.assertTrue( e.getMessage().contains( "Invalid credentials" ) );
+							break;
+						case POSTGRESQL:
+							context.assertEquals( PgException.class, e.getCause().getClass()  );
+							context.assertTrue( e.getMessage().contains( "BogusBogus" ) );
+							break;
+						case MYSQL:
+							context.assertEquals( MySQLException.class, e.getCause().getClass()  );
+							context.assertTrue( e.getMessage().contains( "BogusBogus" ) );
+							break;
+						default:
+							context.fail( "Assertions missing for " + DatabaseConfiguration.dbType() );
+					}
 					return null;
 				} ) );
 	}
 
+	@Test
+	public void testMutinyWithTransaction(final TestContext context) {
+		test( context, getMutinySessionFactory()
+				// The error will occur before the find even get executed
+				.withTransaction( (s, t) -> s.find( Artist.class, "Bansky" ) )
+				.onItem().invoke( a -> context.fail( "Expected an exception" ) )
+				.onFailure().recoverWithItem( e -> {
+					switch ( DatabaseConfiguration.dbType() ) {
+						case DB2:
+							context.assertEquals( DB2Exception.class, e.getClass()  );
+							context.assertTrue( e.getMessage().contains( "Invalid credentials" ) );
+							break;
+						case POSTGRESQL:
+							context.assertEquals( PgException.class, e.getClass() );
+							context.assertTrue( e.getMessage().contains( "BogusBogus" ) );
+							break;
+						case MYSQL:
+							context.assertEquals( MySQLException.class, e.getClass() );
+							context.assertTrue( e.getMessage().contains( "BogusBogus" ) );
+							break;
+						default:
+							context.fail( "Assertion missing for " + DatabaseConfiguration.dbType() );
+					}
+					return null;
+				} )
+		);
+	}
+
+	@Entity
 	static class Artist {
 		@Id
 		String name;
