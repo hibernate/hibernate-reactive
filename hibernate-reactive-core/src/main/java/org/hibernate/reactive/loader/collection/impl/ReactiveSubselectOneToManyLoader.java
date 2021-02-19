f4660daff2fa1ebaf3be5114b7121cf7d6c6091b
Gavin King <gavin@hibernate.org>
2021-02-19 10:57:52 +0100

    fix use of raw type + ancient loop syntax

--- hibernate-reactive-core/src/main/java/org/hibernate/reactive/loader/collection/impl/ReactiveSubselectOneToManyLoader.java
+++ hibernate-reactive-core/src/main/java/org/hibernate/reactive/loader/collection/impl/ReactiveSubselectOneToManyLoader.java
@@ -19,7 +19,6 @@ import org.hibernate.type.Type;
 
 import java.io.Serializable;
 import java.util.Collection;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.concurrent.CompletionStage;
 
@@ -42,7 +41,7 @@ public class ReactiveSubselectOneToManyLoader extends ReactiveOneToManyLoader {
 	public ReactiveSubselectOneToManyLoader(
 			QueryableCollection persister,
 			String subquery,
-			Collection entityKeys,
+			Collection<EntityKey> entityKeys,
 			QueryParameters queryParameters,
 			Map<String, int[]> namedParameterLocMap,
 			SessionFactoryImplementor factory,
@@ -50,10 +49,9 @@ public class ReactiveSubselectOneToManyLoader extends ReactiveOneToManyLoader {
 		super( persister, 1, subquery, factory, loadQueryInfluencers );
 
 		keys = new Serializable[ entityKeys.size() ];
-		Iterator iter = entityKeys.iterator();
 		int i=0;
-		while ( iter.hasNext() ) {
-			keys[i++] = ( (EntityKey) iter.next() ).getIdentifier();
+		for ( EntityKey key: entityKeys ) {
+			keys[i++] = key.getIdentifier();
 		}
 
 		this.namedParameters = queryParameters.getNamedParameters();
