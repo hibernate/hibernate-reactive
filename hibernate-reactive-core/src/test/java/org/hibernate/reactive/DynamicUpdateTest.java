42cda443c31ae81cb9c226e29f3c23a426c04885
Gavin King <gavin@hibernate.org>
2021-02-21 21:33:24 +0100

    smoke test for @DynamicInsert @DynamicUpdate

---
+++ hibernate-reactive-core/src/test/java/org/hibernate/reactive/DynamicUpdateTest.java
@@ -0,0 +1,73 @@
+/* Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ * Copyright: Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.reactive;
+
+import io.vertx.ext.unit.TestContext;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.reactive.testing.DatabaseSelectionRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import static org.hibernate.reactive.containers.DatabaseConfiguration.DBType.POSTGRESQL;
+
+public class DynamicUpdateTest extends BaseReactiveTest {
+
+    @Rule
+    public DatabaseSelectionRule rule = DatabaseSelectionRule.runOnlyFor( POSTGRESQL );
+
+    @Override
+    protected Configuration constructConfiguration() {
+        Configuration configuration = super.constructConfiguration();
+        configuration.addAnnotatedClass(Record.class);
+        return configuration;
+    }
+
+    @Test
+    public void test(TestContext context) {
+        Record record = new Record();
+        record.text = "initial text";
+        test(context,
+                getMutinySessionFactory()
+                        .withSession( session -> session.persist(record)
+                                .chain(session::flush)
+                                .invoke( () -> record.text = "edited text" )
+                                .chain(session::flush)
+                        )
+                        .chain( () -> getMutinySessionFactory()
+                                .withSession( session -> session.find(Record.class, record.id)
+                                        .invoke( (result) -> {
+                                            context.assertNotNull(result);
+                                            context.assertEquals("edited text", result.text );
+                                        } )
+                                        .chain(session::remove)
+                                        .chain(session::flush)
+                                ) )
+                        .chain( () -> getMutinySessionFactory()
+                                .withSession( session -> session.find(Record.class, record.id)
+                                        .invoke(context::assertNull)
+                                ) )
+        );
+    }
+
+    @Entity
+    @Table(name="BigRecord")
+    @DynamicInsert @DynamicUpdate
+    static class Record {
+        @GeneratedValue @Id long id;
+        @Basic(optional = false) String text;
+        String moreText;
+        Double number;
+        String evenMoreText;
+    }
+}
