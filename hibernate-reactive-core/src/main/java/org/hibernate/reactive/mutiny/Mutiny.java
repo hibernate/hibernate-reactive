583b310fcfb81ad9abac7da58484a8b39c2d8be8
Gavin King <gavin@hibernate.org>
2021-02-27 22:00:08 +0100

    minor fix to javadoc

--- hibernate-reactive-core/src/main/java/org/hibernate/reactive/mutiny/Mutiny.java
+++ hibernate-reactive-core/src/main/java/org/hibernate/reactive/mutiny/Mutiny.java
@@ -1170,7 +1170,6 @@ public interface Mutiny {
 		 * or SQL update, insert, or delete statement. In the case of an update,
 		 * insert, or delete, the returned {@link Query} must be executed using
 		 * {@link Query#executeUpdate()} which returns an affected row count.
-		 * In the case of a query:
 		 *
 		 * @param queryString The SQL select, update, insert, or delete statement
 		 *
