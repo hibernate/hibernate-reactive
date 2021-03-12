f4c120f61fbd23f79f37ede1ec6284e45a271bf9
Yoann Rodière <yoann@hibernate.org>
2021-03-12 13:55:56 +0100

    Test against JDK17 EA
    Signed-off-by: Yoann Rodière <yoann@hibernate.org>

---
+++ examples/session-example/src/main/generated/org/hibernate/reactive/example/session/Book_.java
@@ -0,0 +1,25 @@
+package org.hibernate.reactive.example.session;
+
+import java.time.LocalDate;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Book.class)
+public abstract class Book_ {
+
+	public static volatile SingularAttribute<Book, Author> author;
+	public static volatile SingularAttribute<Book, String> isbn;
+	public static volatile SingularAttribute<Book, Integer> id;
+	public static volatile SingularAttribute<Book, LocalDate> published;
+	public static volatile SingularAttribute<Book, String> title;
+
+	public static final String AUTHOR = "author";
+	public static final String ISBN = "isbn";
+	public static final String ID = "id";
+	public static final String PUBLISHED = "published";
+	public static final String TITLE = "title";
+
+}
+
