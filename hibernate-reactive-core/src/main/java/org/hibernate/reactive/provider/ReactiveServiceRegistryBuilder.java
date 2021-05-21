001cd8fce494620cde9f6c69f120bf5d80d021c8
Davide D'Alto <davide@hibernate.org>
2021-05-21 10:17:23 +0100

    Remove call to deprecated constructor in ReactiveServiceRegistryBuilder

--- hibernate-reactive-core/src/main/java/org/hibernate/reactive/provider/ReactiveServiceRegistryBuilder.java
+++ hibernate-reactive-core/src/main/java/org/hibernate/reactive/provider/ReactiveServiceRegistryBuilder.java
@@ -66,6 +66,7 @@ public final class ReactiveServiceRegistryBuilder extends StandardServiceRegistr
         return new StandardServiceRegistryBuilder(
                 bootstrapServiceRegistry,
                 new HashMap<>(),
+                new ConfigLoader( bootstrapServiceRegistry ),
                 loadedConfig,
                 defaultReactiveInitiatorList()
         ) {
