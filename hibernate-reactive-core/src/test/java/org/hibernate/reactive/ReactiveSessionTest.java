0bd7e907634644cfb5d7d0872199d71c7ce94926
gavin <gavin@hibernate.org>
2021-05-24 11:53:17 +0200

    tests for Session/Transaction propagation

--- hibernate-reactive-core/src/test/java/org/hibernate/reactive/ReactiveSessionTest.java
+++ hibernate-reactive-core/src/test/java/org/hibernate/reactive/ReactiveSessionTest.java
@@ -751,15 +751,34 @@ public class ReactiveSessionTest extends BaseReactiveTest {
 	}
 
 	@Test
-	public void reactiveThreadBoundTransaction(TestContext context) {
+	public void testTransactionPropagation(TestContext context) {
 		test( context, getSessionFactory().withTransaction(
-				(session, transaction) -> session.createQuery("from GuineaPig").getResultList().thenAccept( list -> {
-					context.assertNotNull( session.currentTransaction() );
-					context.assertFalse( session.currentTransaction().isMarkedForRollback() );
-					session.currentTransaction().markForRollback();
-					context.assertTrue( session.currentTransaction().isMarkedForRollback() );
-					context.assertTrue( transaction.isMarkedForRollback() );
-				} ) ) );
+				(session, transaction) -> session.createQuery("from GuineaPig").getResultList()
+						.thenCompose( list -> {
+							context.assertNotNull( session.currentTransaction() );
+							context.assertFalse( session.currentTransaction().isMarkedForRollback() );
+							session.currentTransaction().markForRollback();
+							context.assertTrue( session.currentTransaction().isMarkedForRollback() );
+							context.assertTrue( transaction.isMarkedForRollback() );
+							return session.withTransaction( t -> {
+								context.assertTrue( t.isMarkedForRollback() );
+								return session.createQuery("from GuineaPig").getResultList();
+							} );
+						} )
+		) );
+	}
+
+	@Test
+	public void testSessionPropagation(TestContext context) {
+		test( context, getSessionFactory().withSession( session -> {
+			context.assertEquals( false, session.isDefaultReadOnly() );
+			session.setDefaultReadOnly(true);
+			return session.createQuery("from GuineaPig").getResultList()
+					.thenCompose( list -> getSessionFactory().withSession(s -> {
+						context.assertEquals( true, s.isDefaultReadOnly() );
+						return s.createQuery("from GuineaPig").getResultList();
+					} ) );
+		} ) );
 	}
 
 	@Entity(name="GuineaPig")
