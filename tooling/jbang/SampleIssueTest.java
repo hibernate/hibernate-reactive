8c3190cc5606c1517a1b1752308eb404a050e7a1
Davide D'Alto <davide@hibernate.org>
2021-05-27 17:03:37 +0100

    [#815] Upgrade to Vert.x 4.1.0.CR2

--- tooling/jbang/SampleIssueTest.java
+++ tooling/jbang/SampleIssueTest.java
@@ -9,11 +9,11 @@
 //DEPS javax.persistence:javax.persistence-api:2.2
 //DEPS org.hibernate.reactive:hibernate-reactive-core:1.0.0.CR5
 //DEPS org.assertj:assertj-core:3.13.2
-//DEPS io.vertx:vertx-pg-client:4.1.0.CR1
-//DEPS io.vertx:vertx-mysql-client:4.1.0.CR1
-//DEPS io.vertx:vertx-db2-client:4.1.0.CR1
-//DEPS io.vertx:vertx-sql-client:4.1.0.CR1
-//DEPS io.vertx:vertx-unit:4.1.0.CR1
+//DEPS io.vertx:vertx-pg-client:4.1.0.CR2
+//DEPS io.vertx:vertx-mysql-client:4.1.0.CR2
+//DEPS io.vertx:vertx-db2-client:4.1.0.CR2
+//DEPS io.vertx:vertx-sql-client:4.1.0.CR2
+//DEPS io.vertx:vertx-unit:4.1.0.CR2
 //
 //
 
