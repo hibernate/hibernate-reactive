26aa9871ab888b40ee459f11b0eb1cc3482fe633
Davide <davide@hibernate.org>
2021-06-15 15:32:35 +0100

    [#255] Handle SqlServer query parameters

--- hibernate-reactive-core/src/main/java/org/hibernate/reactive/pool/impl/Parameters.java
+++ hibernate-reactive-core/src/main/java/org/hibernate/reactive/pool/impl/Parameters.java
@@ -8,18 +8,17 @@ package org.hibernate.reactive.pool.impl;
 import org.hibernate.dialect.CockroachDB192Dialect;
 import org.hibernate.dialect.Dialect;
 import org.hibernate.dialect.PostgreSQL9Dialect;
+import org.hibernate.dialect.SQLServerDialect;
 
 /**
- * PostgreSQL has a "funny" parameter syntax of form {@code $n}, which
+ * Some databases have a different parameter syntax, which
  * the Vert.x {@link io.vertx.sqlclient.SqlClient} does not abstract.
  * This class converts JDBC/ODBC-style {@code ?} parameters generated
- * by Hibernate ORM to this native format.
+ * by Hibernate ORM to the native format.
  */
-public class Parameters {
+public abstract class Parameters {
 
-	private static Parameters INSTANCE = new Parameters();
-
-	private static Parameters NO_PARSING = new Parameters() {
+	private static final Parameters NO_PARSING = new Parameters() {
 		@Override
 		public String process(String sql) {
 			return sql;
@@ -37,124 +36,20 @@ public class Parameters {
 	};
 
 	public static Parameters instance(Dialect dialect) {
-		return dialect instanceof PostgreSQL9Dialect || dialect instanceof CockroachDB192Dialect
-				? INSTANCE
-				: NO_PARSING;
+		if (dialect instanceof PostgreSQL9Dialect || dialect instanceof CockroachDB192Dialect) return PostgresParameters.INSTANCE;
+		if (dialect instanceof SQLServerDialect) return SQLServerParameters.INSTANCE;
+		return NO_PARSING;
 	}
 
-	private Parameters() {
-	}
-
-	public String process(String sql) {
-		if ( isProcessingNotRequired( sql ) ) {
-			return sql;
-		}
-		return new Parser( sql ).result();
-	}
-
-	/**
-	 * Limit and offset gets applied just before the execution of the query but because we know
-	 * how the string looks like for Postgres, it's faster to replace the last bit instead
-	 * of processing the whole query
-	 */
-	public String processLimit(String sql, Object[] parameterArray, boolean hasOffset) {
-		if ( isProcessingNotRequired( sql ) ) {
-			return sql;
-		}
-
-		// Replace 'limit ? offset ?' with the $ style parameters for PostgreSQL
-		int index = hasOffset ? parameterArray.length - 1 : parameterArray.length;
-		int pos = sql.indexOf( " limit ?" );
-		if ( pos > -1 ) {
-			String sqlProcessed = sql.substring( 0, pos ) + " limit $" + index++;
-			if ( hasOffset ) {
-				sqlProcessed += " offset $" + index;
-			}
-			return sqlProcessed;
-		}
-
-		return sql;
-	}
-
-	/**
-	 * Replace all JDBC-style {@code ?} parameters with Postgres-style
-	 * {@code $n} parameters in the given SQL string.
-	 */
-	public String process(String sql, int parameterCount) {
-		if ( isProcessingNotRequired( sql ) ) {
-			return sql;
-		}
-		return new Parser( sql, parameterCount ).result();
-	}
-
-	private static boolean isProcessingNotRequired(String sql) {
+	public static boolean isProcessingNotRequired(String sql) {
 		return sql == null
 				// There aren't any parameters
-				|| sql.indexOf( '?' ) == -1;
+				|| sql.indexOf('?') == -1;
 	}
 
-	private static class Parser {
+	public abstract String process(String sql);
 
-		private boolean inString;
-		private boolean inQuoted;
-		private boolean inSqlComment;
-		private boolean inCComment;
-		private boolean escaped;
-		private int count = 0;
-		private StringBuilder result;
-		private int previous;
+	public abstract String process(String sql, int parameterCount);
 
-		private Parser(String sql) {
-			this( sql, 10 );
-		}
-
-		private Parser(String sql, int parameterCount) {
-			result = new StringBuilder( sql.length() + parameterCount );
-			sql.codePoints().forEach( this::append );
-		}
-
-		private String result() {
-			return result.toString();
-		}
-
-		private void append(int codePoint) {
-			if ( escaped ) {
-				escaped = false;
-			}
-			else {
-				switch ( codePoint ) {
-					case '\\':
-						escaped = true;
-						break;
-					case '"':
-						if ( !inString && !inSqlComment && !inCComment ) inQuoted = !inQuoted;
-						break;
-					case '\'':
-						if ( !inQuoted && !inSqlComment && !inCComment ) inString = !inString;
-						break;
-					case '-':
-						if ( !inQuoted && !inString && !inCComment && previous == '-' ) inSqlComment = true;
-						break;
-					case '\n':
-						inSqlComment = false;
-						break;
-					case '*':
-						if ( !inQuoted && !inString && !inSqlComment && previous == '/' ) inCComment = true;
-						break;
-					case '/':
-						if ( previous == '*' ) inCComment = false;
-						break;
-					//TODO: $$-quoted strings
-					case '?':
-						if ( !inQuoted && !inString ) {
-							result.append( '$' ).append( ++count );
-							previous = '?';
-							return;
-						}
-				}
-			}
-			previous = codePoint;
-			result.appendCodePoint( codePoint );
-		}
-	}
+	public abstract String processLimit(String sql, Object[] parameterArray, boolean hasOffset);
 }
---
+++ hibernate-reactive-core/src/main/java/org/hibernate/reactive/pool/impl/SQLServerParameters.java
@@ -0,0 +1,118 @@
+/* Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ * Copyright: Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.reactive.pool.impl;
+
+public class SQLServerParameters extends Parameters {
+
+    static final SQLServerParameters INSTANCE = new SQLServerParameters();
+
+    private SQLServerParameters() {
+    }
+
+    @Override
+    public String process(String sql) {
+        if (isProcessingNotRequired(sql)) {
+            return sql;
+        }
+        return new Parser(sql).result();
+    }
+
+    @Override
+    public String process(String sql, int parameterCount) {
+        if (isProcessingNotRequired(sql)) {
+            return sql;
+        }
+        return new Parser(sql, parameterCount).result();
+    }
+
+    /* Offset and Fetch gets applied just before the execution of the query but because we know
+     * how the string looks like for Sql Server, it's faster to replace the last bit instead
+     * of processing the whole query
+     */
+    @Override
+    public String processLimit(String sql, Object[] parameterArray, boolean hasOffset) {
+        if (isProcessingNotRequired(sql)) {
+            return sql;
+        }
+
+        // Replace 'offset ? fetch next ? rows only' with the @P style parameters for Sql Server
+        int index = hasOffset ? parameterArray.length - 1 : parameterArray.length;
+        int pos = sql.indexOf( " offset ?" );
+        if ( pos > -1 ) {
+            String sqlProcessed = sql.substring( 0, pos ) + " offset @P" + index++ + " rows";
+            if ( sql.indexOf( " fetch next ?" ) > -1 ) {
+                sqlProcessed += " fetch next @P" + index + " rows only ";
+            }
+            return sqlProcessed;
+        }
+
+        return sql;
+    }
+
+    private static class Parser {
+
+        private boolean inString;
+        private boolean inQuoted;
+        private boolean inSqlComment;
+        private boolean inCComment;
+        private boolean escaped;
+        private int count = 0;
+        private StringBuilder result;
+        private int previous;
+
+        private Parser(String sql) {
+            this(sql, 10);
+        }
+
+        private Parser(String sql, int parameterCount) {
+            result = new StringBuilder(sql.length() + parameterCount);
+            sql.codePoints().forEach(this::append);
+        }
+
+        private String result() {
+            return result.toString();
+        }
+
+        private void append(int codePoint) {
+            if (escaped) {
+                escaped = false;
+            } else {
+                switch (codePoint) {
+                    case '\\':
+                        escaped = true;
+                        break;
+                    case '"':
+                        if (!inString && !inSqlComment && !inCComment) inQuoted = !inQuoted;
+                        break;
+                    case '\'':
+                        if (!inQuoted && !inSqlComment && !inCComment) inString = !inString;
+                        break;
+                    case '-':
+                        if (!inQuoted && !inString && !inCComment && previous == '-') inSqlComment = true;
+                        break;
+                    case '\n':
+                        inSqlComment = false;
+                        break;
+                    case '*':
+                        if (!inQuoted && !inString && !inSqlComment && previous == '/') inCComment = true;
+                        break;
+                    case '/':
+                        if (previous == '*') inCComment = false;
+                        break;
+                    //TODO: $$-quoted strings
+                    case '?':
+                        if (!inQuoted && !inString) {
+                            result.append("@P").append(++count);
+                            previous = '?';
+                            return;
+                        }
+                }
+            }
+            previous = codePoint;
+            result.appendCodePoint(codePoint);
+        }
+    }
+}
