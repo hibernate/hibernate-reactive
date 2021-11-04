fb7dd512b031a7293b6766cdd4fe69731e4effcc
Davide <davide@hibernate.org>
2021-11-04 21:27:37 +0000

    [#1011] Test withTransaction(Function<*Session>, ...)

--- hibernate-reactive-core/src/test/java/org/hibernate/reactive/MutinySessionTest.java
+++ hibernate-reactive-core/src/test/java/org/hibernate/reactive/MutinySessionTest.java
@@ -67,6 +67,28 @@ public class MutinySessionTest extends BaseReactiveTest {
 	}
 
 	@Test
+	public void reactiveWithTransactionStatelessSession(TestContext context) {
+		final GuineaPig guineaPig = new GuineaPig( 61, "Mr. Peanutbutter" );
+		test( context, getMutinySessionFactory()
+				.withStatelessTransaction( s -> s.insert( guineaPig ) )
+				.chain( () -> getMutinySessionFactory()
+						.withSession( s -> s.find( GuineaPig.class, guineaPig.getId() ) ) )
+				.invoke( result -> assertThatPigsAreEqual( context, guineaPig, result ) )
+		);
+	}
+
+	@Test
+	public void reactiveWithTransactionSession(TestContext context) {
+		final GuineaPig guineaPig = new GuineaPig( 61, "Mr. Peanutbutter" );
+		test( context, getMutinySessionFactory()
+				.withTransaction( s -> s.persist( guineaPig ) )
+				.chain( () -> getMutinySessionFactory()
+						.withSession( s -> s.find( GuineaPig.class, guineaPig.getId() ) ) )
+				.invoke( result -> assertThatPigsAreEqual( context, guineaPig, result ) )
+		);
+	}
+
+	@Test
 	public void reactiveFind1(TestContext context) {
 		final GuineaPig expectedPig = new GuineaPig( 5, "Aloi" );
 		test(
