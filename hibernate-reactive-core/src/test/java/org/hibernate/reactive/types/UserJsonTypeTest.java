f3ab4b3ef8c4b5530ba357776d81eafb2e06c920
blafond <blafond@redhat.com>
2021-11-19 11:52:38 -0600

    Disabled Json tests for Oracle dialect

--- hibernate-reactive-core/src/test/java/org/hibernate/reactive/types/UserJsonTypeTest.java
+++ hibernate-reactive-core/src/test/java/org/hibernate/reactive/types/UserJsonTypeTest.java
@@ -27,6 +27,7 @@ import io.vertx.core.json.JsonObject;
 import io.vertx.ext.unit.TestContext;
 
 import static org.hibernate.reactive.containers.DatabaseConfiguration.DBType.DB2;
+import static org.hibernate.reactive.containers.DatabaseConfiguration.DBType.ORACLE;
 import static org.hibernate.reactive.containers.DatabaseConfiguration.DBType.SQLSERVER;
 
 /**
@@ -35,7 +36,7 @@ import static org.hibernate.reactive.containers.DatabaseConfiguration.DBType.SQL
 public class UserJsonTypeTest extends BaseReactiveTest {
 
 	@Rule
-	public DatabaseSelectionRule selectionRule = DatabaseSelectionRule.skipTestsFor( DB2, SQLSERVER );
+	public DatabaseSelectionRule selectionRule = DatabaseSelectionRule.skipTestsFor( DB2, SQLSERVER, ORACLE );
 
 	@Override
 	protected Configuration constructConfiguration() {
