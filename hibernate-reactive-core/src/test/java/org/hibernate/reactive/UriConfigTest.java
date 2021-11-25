1aaba8ec197f9c407201fdeefc73e1a8f3933cd8
Davide <davide@hibernate.org>
2021-11-25 07:52:22 +0000

    Format code in UriConfigTest

--- hibernate-reactive-core/src/test/java/org/hibernate/reactive/UriConfigTest.java
+++ hibernate-reactive-core/src/test/java/org/hibernate/reactive/UriConfigTest.java
@@ -28,50 +28,50 @@ import static org.hibernate.reactive.containers.DatabaseConfiguration.dbType;
 
 public class UriConfigTest extends BaseReactiveTest {
 
-    @Rule // Oracle doesn't have a fromUri method at the moment
-    public DatabaseSelectionRule rule = DatabaseSelectionRule.skipTestsFor( ORACLE );
+	@Rule // Oracle doesn't have a fromUri method at the moment
+	public DatabaseSelectionRule rule = DatabaseSelectionRule.skipTestsFor( ORACLE );
 
-    @Override
-    protected Configuration constructConfiguration() {
-        Class<? extends Dialect> dialect;
-        switch ( dbType() ) {
-            case POSTGRESQL: dialect = PostgreSQL10Dialect.class; break;
-            case COCKROACHDB: dialect = CockroachDB201Dialect.class; break;
-            case MYSQL: dialect = MySQL8Dialect.class; break;
-            case MARIA: dialect = MariaDB103Dialect.class; break;
-            case SQLSERVER: dialect = SQLServer2012Dialect.class; break;
-            case DB2: dialect = DB297Dialect.class; break;
-            default: throw new IllegalArgumentException("Database not recognized: " + dbType().name());
-        }
+	@Override
+	protected Configuration constructConfiguration() {
+		Class<? extends Dialect> dialect;
+		switch ( dbType() ) {
+			case POSTGRESQL: dialect = PostgreSQL10Dialect.class; break;
+			case COCKROACHDB: dialect = CockroachDB201Dialect.class; break;
+			case MYSQL: dialect = MySQL8Dialect.class; break;
+			case MARIA: dialect = MariaDB103Dialect.class; break;
+			case SQLSERVER: dialect = SQLServer2012Dialect.class; break;
+			case DB2: dialect = DB297Dialect.class; break;
+			default: throw new IllegalArgumentException( "Database not recognized: " + dbType().name() );
+		}
 
-        Configuration configuration = super.constructConfiguration();
-        configuration.setProperty( Environment.DIALECT, dialect.getName() );
-        configuration.setProperty( Settings.URL, DatabaseConfiguration.getUri() );
-        configuration.setProperty( Settings.SQL_CLIENT_POOL_CONFIG, UriPoolConfiguration.class.getName() );
-        return configuration;
-    }
+		Configuration configuration = super.constructConfiguration();
+		configuration.setProperty( Environment.DIALECT, dialect.getName() );
+		configuration.setProperty( Settings.URL, DatabaseConfiguration.getUri() );
+		configuration.setProperty( Settings.SQL_CLIENT_POOL_CONFIG, UriPoolConfiguration.class.getName() );
+		return configuration;
+	}
 
-    @Test
-    public void testUriConfig(TestContext context) {
-        test( context, getSessionFactory()
-                        .withTransaction( (s, t) -> s.createNativeQuery( selectQuery(), String.class ).getSingleResult() )
-                        .thenAccept( context::assertNotNull )
-        );
-    }
+	@Test
+	public void testUriConfig(TestContext context) {
+		test( context, getSessionFactory()
+				.withTransaction( (s, t) -> s.createNativeQuery( selectQuery(), String.class ).getSingleResult() )
+				.thenAccept( context::assertNotNull )
+		);
+	}
 
-    private String selectQuery() {
-        switch ( dbType() ) {
-            case POSTGRESQL:
-            case COCKROACHDB:
-            case SQLSERVER:
-                return "select cast(current_timestamp as varchar)";
-            case MARIA:
-            case MYSQL:
-                return "select cast(current_timestamp as char) from dual";
-            case DB2:
+	private String selectQuery() {
+		switch ( dbType() ) {
+			case POSTGRESQL:
+			case COCKROACHDB:
+			case SQLSERVER:
+				return "select cast(current_timestamp as varchar)";
+			case MARIA:
+			case MYSQL:
+				return "select cast(current_timestamp as char) from dual";
+			case DB2:
 				return "select cast(current time AS varchar) from sysibm.sysdummy1;";
-            default:
-                throw new IllegalArgumentException("Database not recognized: " + dbType().name());
-        }
-    }
+			default:
+				throw new IllegalArgumentException( "Database not recognized: " + dbType().name() );
+		}
+	}
 }
