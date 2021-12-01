d4a502f9164a0dbd02d8dca5f91ad6e286fc0509
Davide <davide@hibernate.org>
2021-12-01 16:55:22 +0000

    Fix DynamicUpdateTest
    Escape columns names

--- hibernate-reactive-core/src/test/java/org/hibernate/reactive/DynamicUpdateTest.java
+++ hibernate-reactive-core/src/test/java/org/hibernate/reactive/DynamicUpdateTest.java
@@ -6,13 +6,16 @@
 package org.hibernate.reactive;
 
 import io.vertx.ext.unit.TestContext;
+
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 import org.hibernate.annotations.OptimisticLocking;
 import org.hibernate.cfg.Configuration;
+
 import org.junit.Test;
 
 import javax.persistence.Basic;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -22,49 +25,56 @@ import static org.hibernate.annotations.OptimisticLockType.DIRTY;
 
 public class DynamicUpdateTest extends BaseReactiveTest {
 
-    @Override
-    protected Configuration constructConfiguration() {
-        Configuration configuration = super.constructConfiguration();
-        configuration.addAnnotatedClass(Record.class);
-        return configuration;
-    }
+	@Override
+	protected Configuration constructConfiguration() {
+		Configuration configuration = super.constructConfiguration();
+		configuration.addAnnotatedClass( Record.class );
+		return configuration;
+	}
 
-    @Test
-    public void test(TestContext context) {
-        Record record = new Record();
-        record.text = "initial text";
-        test(context,
-                getMutinySessionFactory()
-                        .withSession( session -> session.persist(record)
-                                .chain(session::flush)
-                                .invoke( () -> record.text = "edited text" )
-                                .chain(session::flush)
-                        )
-                        .chain( () -> getMutinySessionFactory()
-                                .withSession( session -> session.find(Record.class, record.id)
-                                        .invoke( (result) -> {
-                                            context.assertNotNull(result);
-                                            context.assertEquals("edited text", result.text );
-                                        } )
-                                        .chain(session::remove)
-                                        .chain(session::flush)
-                                ) )
-                        .chain( () -> getMutinySessionFactory()
-                                .withSession( session -> session.find(Record.class, record.id)
-                                        .invoke(context::assertNull)
-                                ) )
-        );
-    }
+	@Test
+	public void test(TestContext context) {
+		Record record = new Record();
+		record.text = "initial text";
+		test(
+				context,
+				getMutinySessionFactory()
+						.withSession( session -> session.persist( record )
+								.chain( session::flush )
+								.invoke( () -> record.text = "edited text" )
+								.chain( session::flush )
+						)
+						.chain( () -> getMutinySessionFactory()
+								.withSession( session -> session.find( Record.class, record.id )
+										.invoke( (result) -> {
+											context.assertNotNull( result );
+											context.assertEquals( "edited text", result.text );
+										} )
+										.chain( session::remove )
+										.chain( session::flush )
+								) )
+						.chain( () -> getMutinySessionFactory()
+								.withSession( session -> session.find( Record.class, record.id )
+										.invoke( context::assertNull )
+								) )
+		);
+	}
 
-    @Entity
-    @Table(name="BigRecord")
-    @DynamicInsert @DynamicUpdate
-    @OptimisticLocking(type = DIRTY)
-    static class Record {
-        @GeneratedValue @Id long id;
-        @Basic(optional = false) String text;
-        String moreText;
-        Double number;
-        String evenMoreText;
-    }
+	@Entity
+	@Table(name = "BigRecord")
+	@DynamicInsert
+	@DynamicUpdate
+	@OptimisticLocking(type = DIRTY)
+	static class Record {
+		@GeneratedValue
+		@Id
+		long id;
+		@Basic(optional = false)
+		@Column(name = "`text`")
+		String text;
+		String moreText;
+		@Column(name = "`number`")
+		Double number;
+		String evenMoreText;
+	}
 }
