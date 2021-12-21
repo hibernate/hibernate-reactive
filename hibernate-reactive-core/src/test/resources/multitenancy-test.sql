4fca3544d6ad33687d1408441dab6b0acc7a2610
Davide <davide@hibernate.org>
2021-12-21 09:01:45 +0000

    [#1105] Fix multitenant tests
    At the end of the tests there was an sql for dropping the db that was
    never successful but we didn't notice because the execution  wasn't
    chained correctly.

--- hibernate-reactive-core/src/test/resources/multitenancy-test.sql
+++ hibernate-reactive-core/src/test/resources/multitenancy-test.sql
@@ -1,4 +1,7 @@
 -- For PostgreSQL. We use these additional databases to test multitenancy.
+DROP DATABASE IF EXISTS dbtenant1;
+DROP DATABASE IF EXISTS dbtenant2;
+
 CREATE DATABASE dbtenant1;
 CREATE DATABASE dbtenant2;
 
