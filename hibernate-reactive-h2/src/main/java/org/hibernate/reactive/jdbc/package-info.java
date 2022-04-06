0a4946e2affac83b413bd190f3e390d5af05332c
blafond <blafond@redhat.com>
2022-04-06 09:26:31 -0500

    [#929] hibernate-reactive-h2 module

---
+++ hibernate-reactive-h2/src/main/java/org/hibernate/reactive/jdbc/package-info.java
@@ -0,0 +1,8 @@
+/**
+// * Hibernate Reactive is an adaptation of Hibernate ORM to the
+// * world of reactive programming, and replaces JDBC for database
+// * access with a non-blocking database client.
+// * <p>
+// * <p>
+ */
+package org.hibernate.reactive.jdbc;
