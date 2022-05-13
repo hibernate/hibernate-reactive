da58193782e82d3e3e54ed41cb36d3e28b47eb45
Davide <davide@hibernate.org>
2022-05-13 11:15:04 +0100

    [#696] Fix InternalStateAssertionsTest
    The previous test was tryint to test that our assertions work
    as expected. But it wasn't creating different threads explicitly,
    relying on the fact that, usually, when running in a different context
    a new thread is created. Because this doesn't always happen, the test
    wasn't working on CI.

--- hibernate-reactive-core/src/test/java/org/hibernate/reactive/InternalStateAssertionsTest.java
+++ hibernate-reactive-core/src/test/java/org/hibernate/reactive/InternalStateAssertionsTest.java
@@ -7,9 +7,8 @@ package org.hibernate.reactive;
 
 import java.util.Collection;
 import java.util.List;
-import java.util.Locale;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
+import java.util.concurrent.Executor;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
@@ -18,32 +17,27 @@ import org.hibernate.reactive.stage.Stage;
 import org.hibernate.reactive.testing.DatabaseSelectionRule;
 
 import org.junit.After;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 
 import io.smallrye.mutiny.Uni;
-import io.vertx.core.Context;
-import io.vertx.core.Vertx;
-import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
-import org.assertj.core.api.Assertions;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hibernate.reactive.containers.DatabaseConfiguration.DBType.POSTGRESQL;
 import static org.hibernate.reactive.testing.DatabaseSelectionRule.runOnlyFor;
+import static org.hibernate.reactive.testing.ReactiveAssertions.assertThrown;
 
 /**
- * It's currently considered an error to share a Session between multiple reactive streams,
- * so we should detect that condition and throw an exception.
- * <p>
- * WARNING: Because we are running the code to test inside a function, we must create the {@link Async}
- * in advance. Otherwise the test will end successfully because the async has not been created yet.
- * </p>
+ * Checks that we throw the right exception when a session is shared between threads.
+ * @see org.hibernate.reactive.common.InternalStateAssertions
  */
 public class InternalStateAssertionsTest extends BaseReactiveTest {
 
-	private static final String ERROR_MESSAGE_LOWER_CASED = "Detected use of the reactive Session from a different Thread"
-			.toLowerCase( Locale.ROOT );
+	/**
+	 * @see org.hibernate.reactive.logging.impl.Log#detectedUsedOfTheSessionOnTheWrongThread
+	 */
+	private static final String ERROR_CODE = "HR000069";
 
 	private Object currentSession;
 
@@ -63,108 +57,73 @@ public class InternalStateAssertionsTest extends BaseReactiveTest {
 	}
 
 	@Test
-	@Ignore
-	// I don't know why but this test fails on CI because no exception is thrown
 	public void testPersistWithStage(TestContext testContext) {
-		Async async = testContext.async();
 		CompletionStage<Stage.Session> sessionStage = getSessionFactory().openSession();
 		currentSession = sessionStage;
-		Context testVertxContext = Vertx.currentContext();
 
-		// Create a different new context
-		Context newContext = Vertx.vertx().getOrCreateContext();
-		Assertions.assertThat( testVertxContext ).isNotEqualTo( newContext );
+		ThreadPerCommandExecutor executor = new ThreadPerCommandExecutor();
 
-		test( testContext, sessionStage.thenAccept( session ->
-			// Run test in the new context
-			newContext.runOnContext( event -> test( async, testContext, session
-					.persist( new Competition( "Cheese Rolling" ) )
-					.handle( (v, e) -> assertExceptionThrown( e ).join() ) )
-			)
-		) );
+		test( testContext, assertThrown( IllegalStateException.class, sessionStage
+				.thenComposeAsync( session -> session.persist( new Competition( "Cheese Rolling" ) ), executor ) )
+				.thenAccept( InternalStateAssertionsTest::assertException )
+		);
 	}
 
 	@Test
 	public void testFindWithStage(TestContext testContext) {
-		Async async = testContext.async();
 		CompletionStage<Stage.Session> sessionStage = getSessionFactory().openSession();
 		currentSession = sessionStage;
-		Context testVertxContext = Vertx.currentContext();
 
-		// Create a different new context
-		Context newContext = Vertx.vertx().getOrCreateContext();
-		Assertions.assertThat( testVertxContext ).isNotEqualTo( newContext );
+		ThreadPerCommandExecutor executor = new ThreadPerCommandExecutor();
 
-		test( testContext, sessionStage.thenAccept( session ->
-			 // Run test in the new context
-			 newContext.runOnContext( event -> test( async, testContext, session
-					 .find( Competition.class, "Chess boxing" )
-					 .handle( (v, e) -> assertExceptionThrown( e ).join() ) )
-			 )
-	  	) );
+		test( testContext, assertThrown( IllegalStateException.class, sessionStage
+				.thenComposeAsync( session -> session.find( Competition.class, "Chess boxing" ), executor ) )
+				.thenAccept( InternalStateAssertionsTest::assertException )
+		);
 	}
 
 	@Test
 	public void testOnPersistWithMutiny(TestContext testContext) {
-		final Async async = testContext.async();
 		Uni<Mutiny.Session> sessionUni = getMutinySessionFactory().openSession();
 		currentSession = sessionUni;
-		Context testVertxContext = Vertx.currentContext();
 
-		// Create a different new context
-		Context newContext = Vertx.vertx().getOrCreateContext();
-		Assertions.assertThat( testVertxContext ).isNotEqualTo( newContext );
+		ThreadPerCommandExecutor executor = new ThreadPerCommandExecutor();
 
-		test( testContext, sessionUni.invoke( session ->
-			// Run test in the new context
-			newContext.runOnContext( event -> test( async, testContext, session
-					.persist( new Competition( "Cheese Rolling" ) )
-					.onItemOrFailure()
-					.transformToUni( (unused, e) -> Uni.createFrom().completionStage( assertExceptionThrown( e ) ) ) )
-			)
-		) );
+		test( testContext, assertThrown( IllegalStateException.class, sessionUni
+				.call( session -> session
+						.persist( new Competition( "Cheese Rolling" ) )
+						.runSubscriptionOn( executor ) ) )
+				.invoke( InternalStateAssertionsTest::assertException )
+		);
 	}
 
 	@Test
 	public void testFindWithMutiny(TestContext testContext) {
-		final Async async = testContext.async();
 		Uni<Mutiny.Session> sessionUni = getMutinySessionFactory().openSession();
 		currentSession = sessionUni;
-		Context testVertxContext = Vertx.currentContext();
 
-		// Create a different new context
-		Context newContext = Vertx.vertx().getOrCreateContext();
-		Assertions.assertThat( testVertxContext ).isNotEqualTo( newContext );
+		ThreadPerCommandExecutor executor = new ThreadPerCommandExecutor();
 
-		test( testContext, sessionUni.invoke( session -> {
-			// Run test in the new context
-			newContext.runOnContext( event -> test( async, testContext, session
-					.find( Competition.class, "Chess boxing" )
-					.onItemOrFailure()
-					.transformToUni( (unused, e) -> Uni.createFrom().completionStage( assertExceptionThrown( e ) ) ) )
-			);
-		} ) );
+		test( testContext, assertThrown( IllegalStateException.class, sessionUni
+				.chain( session -> session
+						.find( Competition.class, "Chess boxing" )
+						.runSubscriptionOn( executor ) ) )
+				.invoke( InternalStateAssertionsTest::assertException )
+		);
 	}
 
-	// Check that at least one exception has the expected message
-	private static CompletableFuture<Void> assertExceptionThrown(Throwable e) {
-		CompletableFuture<Void> result = new CompletableFuture<>();
-		Throwable t = e;
-		while ( t != null ) {
-			if ( t.getClass().equals( IllegalStateException.class )
-					&& expectedMessage( t ) ) {
-				result.complete( null );
-				return result;
-			}
-			t = t.getCause();
+	private static void assertException(Throwable t) {
+		assertThat( t.getMessage() ).startsWith( ERROR_CODE );
+	}
+
+	/**
+	 * Run a task in a thread that's different from the "Test worker"
+	 */
+	private static class ThreadPerCommandExecutor implements Executor {
+		@Override
+		public void execute(Runnable command) {
+			new Thread( command, InternalStateAssertionsTest.class.getName() + "-thread" ).start();
 		}
-		result.completeExceptionally( new AssertionError( "Expected exception not thrown. Exception thrown: " + e ) );
-		return result;
-	}
-
-	private static boolean expectedMessage(Throwable t) {
-		return t.getMessage().toLowerCase( Locale.ROOT )
-				.contains( ERROR_MESSAGE_LOWER_CASED );
 	}
 
 	@Entity
