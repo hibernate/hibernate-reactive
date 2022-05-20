ac952386c6b04181ca8f01a80d841764b288083b
Davide <davide@hibernate.org>
2022-05-20 16:37:29 +0100

    [#1306] Upgrade testcontainers to 1.17.2

--- hibernate-reactive-core/src/test/java/org/hibernate/reactive/UriPoolConfiguration.java
+++ hibernate-reactive-core/src/test/java/org/hibernate/reactive/UriPoolConfiguration.java
@@ -12,6 +12,13 @@ import org.hibernate.reactive.pool.impl.SqlClientPoolConfiguration;
 import io.vertx.sqlclient.PoolOptions;
 import io.vertx.sqlclient.SqlConnectOptions;
 
+/**
+ * This class is used by {@link UriConfigTest} to test that one can use a different implementation of
+ * {@link SqlClientPoolConfiguration}.
+ * <p>
+ * But, in reality, it also checks {@link SqlConnectOptions#fromUri(String)} works for each database.
+ *</p>
+ */
 public class UriPoolConfiguration implements SqlClientPoolConfiguration {
 	@Override
 	public PoolOptions poolOptions() {
@@ -21,7 +28,12 @@ public class UriPoolConfiguration implements SqlClientPoolConfiguration {
 	@Override
 	public SqlConnectOptions connectOptions(URI uri) {
 		// For CockroachDB we use the PostgreSQL Vert.x client
-		String uriWithPgScheme = uri.toString().replaceAll( "^cockroach(db)?:", "postgres:" );
-		return SqlConnectOptions.fromUri( uriWithPgScheme );
+		String uriString = uri.toString().replaceAll( "^cockroach(db)?:", "postgres:" );
+		if ( uriString.startsWith( "sqlserver" ) ) {
+			// Testscontainer adds encrypt=false to the url. The problem is that it uses the JDBC syntax that's
+			// different from the supported one by the Vert.x driver.
+			uriString = uriString.replaceAll( ";encrypt=false", "?encrypt=false" );
+		}
+		return SqlConnectOptions.fromUri( uriString );
 	}
 }
