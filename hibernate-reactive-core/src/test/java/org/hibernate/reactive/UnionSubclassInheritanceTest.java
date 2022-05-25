64d8cbce2b485b0a36aa9bd8de299cc30da96777
Davide <davide@hibernate.org>
2022-05-25 13:56:01 +0100

    [#1168] Re-enable Oracle tests
    They were disabled because before Vert.x 4.3.1 insert-select queries
    weren't working.

--- hibernate-reactive-core/src/test/java/org/hibernate/reactive/UnionSubclassInheritanceTest.java
+++ hibernate-reactive-core/src/test/java/org/hibernate/reactive/UnionSubclassInheritanceTest.java
@@ -21,19 +21,11 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
-import org.hibernate.reactive.testing.DatabaseSelectionRule;
-
-import org.junit.Rule;
 import org.junit.Test;
 
 import io.vertx.ext.unit.TestContext;
 
-import static org.hibernate.reactive.containers.DatabaseConfiguration.DBType.ORACLE;
-
 public class UnionSubclassInheritanceTest extends BaseReactiveTest {
-
-	@Rule // We cannot read a generated id in Oracle
-	public DatabaseSelectionRule dbRule = DatabaseSelectionRule.skipTestsFor( ORACLE );
 
 	@Override
 	protected Collection<Class<?>> annotatedEntities() {
