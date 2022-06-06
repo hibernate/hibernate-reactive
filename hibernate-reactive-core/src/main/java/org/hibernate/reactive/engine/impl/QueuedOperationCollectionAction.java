f965a7f74d6bcd981aa25dc618f920725c5f859f
Gavin King <gavin@hibernate.org>
2022-06-06 23:02:58 +0100

    fix for #1207
    add reactive version of QueuedOperationCollectionAction

---
+++ hibernate-reactive-core/src/main/java/org/hibernate/reactive/engine/impl/QueuedOperationCollectionAction.java
@@ -0,0 +1,70 @@
+/* Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright: Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.reactive.engine.impl;
+
+import java.io.Serializable;
+import java.util.concurrent.CompletionStage;
+
+import org.hibernate.HibernateException;
+import org.hibernate.action.internal.CollectionAction;
+import org.hibernate.collection.internal.AbstractPersistentCollection;
+import org.hibernate.collection.spi.PersistentCollection;
+import org.hibernate.engine.spi.CollectionEntry;
+import org.hibernate.engine.spi.SharedSessionContractImplementor;
+import org.hibernate.persister.collection.CollectionPersister;
+import org.hibernate.reactive.engine.ReactiveExecutable;
+import org.hibernate.reactive.util.impl.CompletionStages;
+
+/**
+ * Copy of {@link  QueuedOperationCollectionAction}, that adapts to {@link ReactiveExecutable}
+ *
+ * @author Gavin King
+ */
+public final class QueuedOperationCollectionAction extends CollectionAction implements ReactiveExecutable {
+
+	/**
+	 * Constructs a CollectionUpdateAction
+	 *
+	 * @param collection The collection to update
+	 * @param persister The collection persister
+	 * @param id The collection key
+	 * @param session The session
+	 */
+	public QueuedOperationCollectionAction(
+			final PersistentCollection collection,
+			final CollectionPersister persister,
+			final Serializable id,
+			final SharedSessionContractImplementor session) {
+		super( persister, collection, id, session );
+	}
+
+	@Override
+	public CompletionStage<Void> reactiveExecute() {
+		// this QueuedOperationCollectionAction has to be executed before any other
+		// CollectionAction involving the same collection.
+
+		// this is not needed in HR, unless we someday add support for extra-lazy collections
+//		getPersister().processQueuedOps( getCollection(), getKey(), getSession() );
+
+		// TODO: It would be nice if this could be done safely by CollectionPersister#processQueuedOps;
+		//       Can't change the SPI to do this though.
+		((AbstractPersistentCollection) getCollection() ).clearOperationQueue();
+
+		// The other CollectionAction types call CollectionEntry#afterAction, which
+		// clears the dirty flag. We don't want to call CollectionEntry#afterAction unless
+		// there is no other CollectionAction that will be executed on the same collection.
+		final CollectionEntry ce = getSession().getPersistenceContextInternal().getCollectionEntry( getCollection() );
+		if ( !ce.isDoremove() && !ce.isDoupdate() && !ce.isDorecreate() ) {
+			ce.afterAction( getCollection() );
+		}
+		return CompletionStages.voidFuture();
+	}
+
+	@Override
+	public void execute() throws HibernateException {
+		throw new UnsupportedOperationException();
+	}
+}
