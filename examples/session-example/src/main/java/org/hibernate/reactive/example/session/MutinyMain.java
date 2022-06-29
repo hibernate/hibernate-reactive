a06d69e025c186088dab635174a766610d1592cc
Gavin King <gavin@hibernate.org>
2022-06-29 13:36:42 +0200

    fix #975 for stateful sessions

--- examples/session-example/src/main/java/org/hibernate/reactive/example/session/MutinyMain.java
+++ examples/session-example/src/main/java/org/hibernate/reactive/example/session/MutinyMain.java
@@ -11,7 +11,6 @@ import jakarta.persistence.criteria.Root;
 
 import java.time.LocalDate;
 
-import org.hibernate.reactive.mutiny.Mutiny;
 
 import static java.lang.System.out;
 import static java.time.Month.JANUARY;
@@ -62,7 +61,7 @@ public class MutinyMain {
 							// retrieve an Author from Book (lazy many-to one)
 							session -> session.find( Book.class, book1.getId() )
 									// fetch a lazy field of the Book
-									.chain( book -> Mutiny.fetch( book.getAuthor() )
+									.chain( book -> fetch( book.getAuthor() )
 											// print the lazy field
 											.invoke( author -> out.printf( "'%s' has written '%s'\n", author.getName(), book1.getTitle() ) )
 									)
