e895b1bba3ed7ccd4d653c92eb2a848155141092
Davide D'Alto <davide@hibernate.org>
2022-08-05 14:36:51 +0100

    Upgrade MariaDB docker image to 10.8.3

--- hibernate-reactive-core/src/test/java/org/hibernate/reactive/containers/MariaDatabase.java
+++ hibernate-reactive-core/src/test/java/org/hibernate/reactive/containers/MariaDatabase.java
@@ -21,7 +21,7 @@ class MariaDatabase extends MySQLDatabase {
 	 * TIP: To reuse the same containers across multiple runs, set `testcontainers.reuse.enable=true` in a file located
 	 * at `$HOME/.testcontainers.properties` (create the file if it does not exist).
 	 */
-	public static final MariaDBContainer<?> maria = new MariaDBContainer<>( imageName( "mariadb", "10.7.3" ) )
+	public static final MariaDBContainer<?> maria = new MariaDBContainer<>( imageName( "mariadb", "10.8.3" ) )
 			.withUsername( DatabaseConfiguration.USERNAME )
 			.withPassword( DatabaseConfiguration.PASSWORD )
 			.withDatabaseName( DatabaseConfiguration.DB_NAME )
