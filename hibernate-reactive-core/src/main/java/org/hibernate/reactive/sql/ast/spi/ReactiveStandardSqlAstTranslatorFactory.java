4ad8a7ee7903fb2582ed71c53dc16bd36f8174e1
Davide D'Alto <davide@hibernate.org>
2022-11-30 10:03:39 +0000

    Fix

--- hibernate-reactive-core/src/main/java/org/hibernate/reactive/sql/ast/spi/ReactiveStandardSqlAstTranslatorFactory.java
+++ hibernate-reactive-core/src/main/java/org/hibernate/reactive/sql/ast/spi/ReactiveStandardSqlAstTranslatorFactory.java
@@ -8,11 +8,18 @@ package org.hibernate.reactive.sql.ast.spi;
 import org.hibernate.engine.spi.SessionFactoryImplementor;
 import org.hibernate.sql.ast.SqlAstTranslator;
 import org.hibernate.sql.ast.SqlAstTranslatorFactory;
-import org.hibernate.sql.ast.spi.StandardSqlAstTranslatorFactory;
+import org.hibernate.sql.ast.tree.delete.DeleteStatement;
+import org.hibernate.sql.ast.tree.insert.InsertStatement;
 import org.hibernate.sql.ast.tree.select.SelectStatement;
+import org.hibernate.sql.ast.tree.update.UpdateStatement;
+import org.hibernate.sql.exec.spi.JdbcOperationQueryDelete;
+import org.hibernate.sql.exec.spi.JdbcOperationQueryInsert;
 import org.hibernate.sql.exec.spi.JdbcOperationQuerySelect;
+import org.hibernate.sql.exec.spi.JdbcOperationQueryUpdate;
+import org.hibernate.sql.model.ast.TableMutation;
+import org.hibernate.sql.model.jdbc.JdbcMutationOperation;
 
-public class ReactiveStandardSqlAstTranslatorFactory extends StandardSqlAstTranslatorFactory {
+public class ReactiveStandardSqlAstTranslatorFactory implements SqlAstTranslatorFactory {
 
 	private final SqlAstTranslatorFactory delegate;
 
@@ -21,8 +28,37 @@ public class ReactiveStandardSqlAstTranslatorFactory extends StandardSqlAstTrans
 	}
 
 	@Override
-	public SqlAstTranslator<JdbcOperationQuerySelect> buildSelectTranslator(SessionFactoryImplementor sessionFactory, SelectStatement statement) {
-		return new ReactiveSqlAstTranslator<>( sessionFactory, statement );
+	public SqlAstTranslator<JdbcOperationQuerySelect> buildSelectTranslator(
+			SessionFactoryImplementor sessionFactory,
+			SelectStatement statement) {
+		return new ReactiveSqlAstTranslator<>( delegate.buildSelectTranslator( sessionFactory, statement ) );
+	}
 
+	@Override
+	public SqlAstTranslator<JdbcOperationQueryDelete> buildDeleteTranslator(
+			SessionFactoryImplementor sessionFactory,
+			DeleteStatement statement) {
+		return delegate.buildDeleteTranslator( sessionFactory, statement );
+	}
+
+	@Override
+	public SqlAstTranslator<JdbcOperationQueryInsert> buildInsertTranslator(
+			SessionFactoryImplementor sessionFactory,
+			InsertStatement statement) {
+		return delegate.buildInsertTranslator( sessionFactory, statement );
+	}
+
+	@Override
+	public SqlAstTranslator<JdbcOperationQueryUpdate> buildUpdateTranslator(
+			SessionFactoryImplementor sessionFactory,
+			UpdateStatement statement) {
+		return delegate.buildUpdateTranslator( sessionFactory, statement );
+	}
+
+	@Override
+	public <O extends JdbcMutationOperation> SqlAstTranslator<O> buildModelMutationTranslator(
+			TableMutation<O> mutation,
+			SessionFactoryImplementor sessionFactory) {
+		return delegate.buildModelMutationTranslator( mutation, sessionFactory );
 	}
 }
