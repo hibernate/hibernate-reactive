e14efae527ff6005c644ea15048889e2b2d1aecc
Davide D'Alto <davide@hibernate.org>
2022-12-05 13:39:18 +0000

    fix

---
+++ hibernate-reactive-core/src/main/java/org/hibernate/reactive/engine/impl/ReactiveMutationStatementPreparer.java
@@ -0,0 +1,12 @@
+/* Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright: Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.reactive.engine.impl;
+
+/**
+ * @see MutationStatementPreparerImpl
+ */
+public class ReactiveMutationStatementPreparer {
+}
