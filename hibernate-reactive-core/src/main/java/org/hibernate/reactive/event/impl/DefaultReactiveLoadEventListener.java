92b7b0fc8550b8ecd7a7124d70fdba3859f8ce7f
Georgios Andrianakis <geoand@gmail.com>
2023-02-15 13:49:30 +0200

    Fix LockOnUpdateTest
    This is done by following same pattern that we have in main:
    essentially make a reactive copy of the CacheEntityLoaderHelper
    (which was also have in main but was deleted to make the project
    compile against ORM 6)
    
    With this change org.hibernate.reactive.LockOnUpdateTest
    passes and there are no new failures

--- hibernate-reactive-core/src/main/java/org/hibernate/reactive/event/impl/DefaultReactiveLoadEventListener.java
+++ hibernate-reactive-core/src/main/java/org/hibernate/reactive/event/impl/DefaultReactiveLoadEventListener.java
@@ -39,6 +39,7 @@ import org.hibernate.persister.entity.EntityPersister;
 import org.hibernate.proxy.HibernateProxy;
 import org.hibernate.proxy.LazyInitializer;
 import org.hibernate.reactive.event.ReactiveLoadEventListener;
+import org.hibernate.reactive.loader.entity.ReactiveCacheEntityLoaderHelper;
 import org.hibernate.reactive.logging.impl.Log;
 import org.hibernate.reactive.logging.impl.LoggerFactory;
 import org.hibernate.reactive.persister.entity.impl.ReactiveEntityPersister;
@@ -617,7 +618,7 @@ public class DefaultReactiveLoadEventListener implements LoadEventListener, Reac
 			);
 		}
 
-		CacheEntityLoaderHelper.PersistenceContextEntry persistenceContextEntry = CacheEntityLoaderHelper.INSTANCE
+		CacheEntityLoaderHelper.PersistenceContextEntry persistenceContextEntry = ReactiveCacheEntityLoaderHelper.INSTANCE
 				.loadFromSessionCache( event, keyToLoad, options );
 		Object entity = persistenceContextEntry.getEntity();
 		if ( entity != null ) {
