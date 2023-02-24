84c9ec84984c892325051d6ca3424f555d0165d6
Sanne Grinovero <sanne@hibernate.org>
2023-02-24 22:53:57 +0000

    No longer need to override the SqlAstTranslatorFactory

--- hibernate-reactive-core/src/main/java/org/hibernate/reactive/dialect/ReactiveDialectWrapper.java
+++ hibernate-reactive-core/src/main/java/org/hibernate/reactive/dialect/ReactiveDialectWrapper.java
@@ -7,9 +7,7 @@ package org.hibernate.reactive.dialect;
 
 import org.hibernate.dialect.Dialect;
 import org.hibernate.dialect.DialectDelegateWrapper;
-import org.hibernate.dialect.PostgreSQLDialect;
 import org.hibernate.dialect.identity.IdentityColumnSupport;
-import org.hibernate.engine.spi.SessionFactoryImplementor;
 import org.hibernate.metamodel.mapping.EntityMappingType;
 import org.hibernate.metamodel.spi.RuntimeModelCreationContext;
 import org.hibernate.query.sqm.mutation.spi.SqmMultiTableInsertStrategy;
@@ -17,12 +15,6 @@ import org.hibernate.query.sqm.mutation.spi.SqmMultiTableMutationStrategy;
 import org.hibernate.reactive.dialect.identity.ReactiveIdentityColumnSupportAdapter;
 import org.hibernate.reactive.query.sqm.mutation.internal.cte.ReactiveCteInsertStrategy;
 import org.hibernate.reactive.query.sqm.mutation.internal.cte.ReactiveCteMutationStrategy;
-import org.hibernate.reactive.sql.ast.spi.ReactivePostgreSQLSqlAstTranslator;
-import org.hibernate.sql.ast.SqlAstTranslator;
-import org.hibernate.sql.ast.SqlAstTranslatorFactory;
-import org.hibernate.sql.ast.spi.StandardSqlAstTranslatorFactory;
-import org.hibernate.sql.ast.tree.Statement;
-import org.hibernate.sql.exec.spi.JdbcOperation;
 
 /**
  * Wraps the given dialect to make some internal components reactive;
@@ -31,27 +23,8 @@ import org.hibernate.sql.exec.spi.JdbcOperation;
  */
 public final class ReactiveDialectWrapper extends DialectDelegateWrapper {
 
-	//FIXME remove PostgreSQLDialect specific workarounds after HHH-16229
-	private final boolean requiresPostgreSQLSyntaxProcessing;
-
 	public ReactiveDialectWrapper(Dialect wrapped) {
 		super( wrapped );
-		this.requiresPostgreSQLSyntaxProcessing = ( wrapped instanceof PostgreSQLDialect );
-	}
-
-	@Override
-	public SqlAstTranslatorFactory getSqlAstTranslatorFactory() {
-		if ( requiresPostgreSQLSyntaxProcessing ) {
-			return new StandardSqlAstTranslatorFactory() {
-				@Override
-				protected <T extends JdbcOperation> SqlAstTranslator<T> buildTranslator(SessionFactoryImplementor sessionFactory, Statement statement) {
-					return new ReactivePostgreSQLSqlAstTranslator<>( sessionFactory, statement );
-				}
-			};
-		}
-		else {
-			return wrapped.getSqlAstTranslatorFactory();
-		}
 	}
 
 	@Override
@@ -60,12 +33,16 @@ public final class ReactiveDialectWrapper extends DialectDelegateWrapper {
 	}
 
 	@Override
-	public SqmMultiTableMutationStrategy getFallbackSqmMutationStrategy(EntityMappingType rootEntityDescriptor, RuntimeModelCreationContext runtimeModelCreationContext) {
+	public SqmMultiTableMutationStrategy getFallbackSqmMutationStrategy(
+			EntityMappingType rootEntityDescriptor,
+			RuntimeModelCreationContext runtimeModelCreationContext) {
 		return new ReactiveCteMutationStrategy( rootEntityDescriptor, runtimeModelCreationContext );
 	}
 
 	@Override
-	public SqmMultiTableInsertStrategy getFallbackSqmInsertStrategy(EntityMappingType rootEntityDescriptor, RuntimeModelCreationContext runtimeModelCreationContext) {
+	public SqmMultiTableInsertStrategy getFallbackSqmInsertStrategy(
+			EntityMappingType rootEntityDescriptor,
+			RuntimeModelCreationContext runtimeModelCreationContext) {
 		return new ReactiveCteInsertStrategy( rootEntityDescriptor, runtimeModelCreationContext );
 	}
 
