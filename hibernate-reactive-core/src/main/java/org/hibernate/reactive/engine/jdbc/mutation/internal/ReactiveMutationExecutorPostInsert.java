43f1d53d5741bcea1cd8bb60aa6243c8b3d5139b
Sanne Grinovero <sanne@hibernate.org>
2023-02-24 23:56:14 +0000

    ReactiveMutationExecutorPostInsert needs to implement ReactiveMutationExecutor

--- hibernate-reactive-core/src/main/java/org/hibernate/reactive/engine/jdbc/mutation/internal/ReactiveMutationExecutorPostInsert.java
+++ hibernate-reactive-core/src/main/java/org/hibernate/reactive/engine/jdbc/mutation/internal/ReactiveMutationExecutorPostInsert.java
@@ -5,16 +5,28 @@
  */
 package org.hibernate.reactive.engine.jdbc.mutation.internal;
 
-import org.hibernate.engine.jdbc.mutation.MutationExecutor;
+import org.hibernate.engine.jdbc.mutation.OperationResultChecker;
+import org.hibernate.engine.jdbc.mutation.TableInclusionChecker;
 import org.hibernate.engine.jdbc.mutation.internal.MutationExecutorPostInsert;
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
+import org.hibernate.reactive.engine.jdbc.env.internal.ReactiveMutationExecutor;
 import org.hibernate.sql.model.MutationOperationGroup;
+import org.hibernate.sql.model.ValuesAnalysis;
 
-public class ReactiveMutationExecutorPostInsert extends MutationExecutorPostInsert implements MutationExecutor {
+public class ReactiveMutationExecutorPostInsert extends MutationExecutorPostInsert implements ReactiveMutationExecutor {
 
 	public ReactiveMutationExecutorPostInsert(
 			MutationOperationGroup mutationOperationGroup,
 			SharedSessionContractImplementor session) {
 		super( mutationOperationGroup, session );
+	}
+
+	public Object execute(
+			Object modelReference,
+			ValuesAnalysis valuesAnalysis,
+			TableInclusionChecker inclusionChecker,
+			OperationResultChecker resultChecker,
+			SharedSessionContractImplementor session) {
+		throw LOG.nonReactiveMethodCall( "executeReactive" );
 	}
 }
