1a6fbfac9c6c158eb707be0bed1cff0ffa5a74b1
Davide D'Alto <davide@hibernate.org>
2023-03-10 10:12:22 +0100

    Test compound natural id in NaturalIdTest

--- hibernate-reactive-core/src/test/java/org/hibernate/reactive/NaturalIdTest.java
+++ hibernate-reactive-core/src/test/java/org/hibernate/reactive/NaturalIdTest.java
@@ -7,17 +7,18 @@ package org.hibernate.reactive;
 
 import java.util.Collection;
 import java.util.List;
-
-import io.vertx.ext.unit.TestContext;
+import java.util.Objects;
 
 import org.hibernate.annotations.NaturalId;
 
 import org.junit.Test;
 
+import io.vertx.ext.unit.TestContext;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hibernate.reactive.common.Identifier.composite;
 import static org.hibernate.reactive.common.Identifier.id;
 
@@ -25,43 +26,88 @@ public class NaturalIdTest extends BaseReactiveTest {
 
 	@Override
 	protected Collection<Class<?>> annotatedEntities() {
-		return List.of( Thing.class );
+		return List.of( SimpleThing.class, CompoundThing.class );
 	}
 
 	@Test
-	public void test(TestContext context) {
-		Thing thing1 = new Thing();
+	public void testSimpleNaturalIdSingleLoad(TestContext context) {
+		SimpleThing thing1 = new SimpleThing();
 		thing1.naturalKey = "abc123";
+		SimpleThing thing2 = new SimpleThing();
+		thing2.naturalKey = "def456";
+		test(
+				context,
+				getSessionFactory()
+						.withSession( session -> session.persist( thing1, thing2 ).thenCompose( v -> session.flush() ) )
+						.thenCompose( v -> getSessionFactory().withSession(
+								session -> session.find( SimpleThing.class, id( "naturalKey", "abc123" ) )
+						) )
+						.thenAccept( t -> assertThat( t ).isEqualTo( thing1 ) )
+						.thenCompose( v -> getSessionFactory().withSession(
+								session -> session.find( SimpleThing.class, id( SimpleThing.class, "naturalKey", "not an id" ) )
+						) )
+						.thenAccept( context::assertNull )
+		);
+	}
+
+	@Test
+	public void testCompoundNaturalIdSingleLoad(TestContext context) {
+		CompoundThing thing1 = new CompoundThing();
+		thing1.naturalKey = "xyz666";
 		thing1.version = 1;
-		Thing thing2 = new Thing();
-		thing2.naturalKey = "abc123";
+		CompoundThing thing2 = new CompoundThing();
+		thing2.naturalKey = "xyz666";
 		thing2.version = 2;
 		test(
 				context,
 				getSessionFactory()
 						.withSession( session -> session.persist( thing1, thing2 ).thenCompose( v -> session.flush() ) )
 						.thenCompose( v -> getSessionFactory().withSession(
-								session -> session.find( Thing.class, composite(
-										id( "naturalKey", "abc123" ),
+								session -> session.find( CompoundThing.class, composite(
+										id( "naturalKey", "xyz666" ),
 										id( "version", 1 )
 								) )
 						) )
-						.thenAccept( t -> {
-							context.assertNotNull( t );
-							context.assertEquals( thing1.id, t.id );
-						} )
+
+						.thenAccept( t -> assertThat( t ).isEqualTo( thing1 ) )
 						.thenCompose( v -> getSessionFactory().withSession(
-								session -> session.find( Thing.class, composite(
-										id( Thing.class, "naturalKey", "abc123" ),
-										id( Thing.class, "version", 3 )
+								session -> session.find( CompoundThing.class, composite(
+										id( CompoundThing.class, "naturalKey", "xyz666" ),
+										id( CompoundThing.class, "version", 3 )
 								) )
 						) )
 						.thenAccept( context::assertNull )
 		);
 	}
 
-	@Entity(name = "Thing")
-	static class Thing {
+	@Entity(name = "SimpleThing")
+	static class SimpleThing {
+		@Id
+		@GeneratedValue
+		long id;
+		@NaturalId
+		String naturalKey;
+
+		@Override
+		public boolean equals(Object o) {
+			if ( this == o ) {
+				return true;
+			}
+			if ( o == null || getClass() != o.getClass() ) {
+				return false;
+			}
+			SimpleThing that = (SimpleThing) o;
+			return Objects.equals( naturalKey, that.naturalKey );
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash( naturalKey );
+		}
+	}
+
+	@Entity(name = "CompoundThing")
+	static class CompoundThing {
 		@Id
 		@GeneratedValue
 		long id;
@@ -69,5 +115,22 @@ public class NaturalIdTest extends BaseReactiveTest {
 		String naturalKey;
 		@NaturalId
 		int version;
+
+		@Override
+		public boolean equals(Object o) {
+			if ( this == o ) {
+				return true;
+			}
+			if ( o == null || getClass() != o.getClass() ) {
+				return false;
+			}
+			CompoundThing that = (CompoundThing) o;
+			return version == that.version && Objects.equals( naturalKey, that.naturalKey );
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash( naturalKey, version );
+		}
 	}
 }
