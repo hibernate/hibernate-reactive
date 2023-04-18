68f520f2e9366b7891090f8ce5e18bf3b07d1d9d
Davide D'Alto <davide@hibernate.org>
2023-04-18 11:06:02 +0100

    [#1546] Replace parameters for dialect using TemporaryTableExporter
    Hibernate ORM doesn't generate the query using the correct parameters
    marker.
    
    See [HHH-16486](https://hibernate.atlassian.net/browse/HHH-16486)

--- hibernate-reactive-core/src/main/java/org/hibernate/reactive/query/sqm/mutation/internal/temptable/ReactiveTemporaryTableHelper.java
+++ hibernate-reactive-core/src/main/java/org/hibernate/reactive/query/sqm/mutation/internal/temptable/ReactiveTemporaryTableHelper.java
@@ -22,6 +22,7 @@ import org.hibernate.reactive.adaptor.impl.PreparedStatementAdaptor;
 import org.hibernate.reactive.logging.impl.Log;
 import org.hibernate.reactive.logging.impl.LoggerFactory;
 import org.hibernate.reactive.pool.ReactiveConnection;
+import org.hibernate.reactive.pool.impl.Parameters;
 import org.hibernate.reactive.session.ReactiveConnectionSupplier;
 import org.hibernate.reactive.util.impl.CompletionStages;
 
@@ -145,7 +146,10 @@ public class ReactiveTemporaryTableHelper {
 			TemporaryTableExporter exporter,
 			Function<SharedSessionContractImplementor, String> sessionUidAccess,
 			SharedSessionContractImplementor session) {
-		final String sql = exporter.getSqlTruncateCommand( temporaryTable, sessionUidAccess, session );
+		// Workaround for https://hibernate.atlassian.net/browse/HHH-16486
+		final String sql = Parameters.instance( temporaryTable.getDialect() )
+				.process( exporter.getSqlTruncateCommand( temporaryTable, sessionUidAccess, session ) );
+
 		Object[] params = PreparedStatementAdaptor.bind( ps -> {
 			if ( temporaryTable.getSessionUidColumn() != null ) {
 				final String sessionUid = sessionUidAccess.apply( session );
