89ec95d7531d85a60bf7b39db4a808d1fca1ae8b
Davide D'Alto <davide@hibernate.org>
2023-06-01 19:18:38 +0200

    [#1626] Update and re-enable the jbang based examples

--- tooling/jbang/ReactiveTest.java
+++ tooling/jbang/ReactiveTest.java
@@ -5,12 +5,12 @@
  */
 
 ///usr/bin/env jbang "$0" "$@" ; exit $?
-//DEPS io.vertx:vertx-pg-client:${vertx.version:4.3.8}
+//DEPS io.vertx:vertx-pg-client:${vertx.version:4.4.2}
 //DEPS com.ongres.scram:client:2.1
-//DEPS io.vertx:vertx-db2-client:${vertx.version:4.3.8}
-//DEPS io.vertx:vertx-mysql-client:${vertx.version:4.3.8}
-//DEPS io.vertx:vertx-unit:${vertx.version:4.3.8}
-//DEPS org.hibernate.reactive:hibernate-reactive-core:${hibernate-reactive.version:1.1.9.Final}
+//DEPS io.vertx:vertx-db2-client:${vertx.version:4.4.2}
+//DEPS io.vertx:vertx-mysql-client:${vertx.version:4.4.2}
+//DEPS io.vertx:vertx-unit:${vertx.version:4.4.2}
+//DEPS org.hibernate.reactive:hibernate-reactive-core:${hibernate-reactive.version:2.0.0.Final}
 //DEPS org.assertj:assertj-core:3.24.2
 //DEPS junit:junit:4.13.2
 //DEPS org.testcontainers:postgresql:1.18.0
@@ -22,8 +22,8 @@
 //// Testcontainer needs the JDBC drivers to start the containers
 //// Hibernate Reactive doesn't use them
 //DEPS org.postgresql:postgresql:42.5.0
-//DEPS com.mysql:mysql-connector-j:8.0.32
-//DEPS org.mariadb.jdbc:mariadb-java-client:2.7.3
+//DEPS com.mysql:mysql-connector-j:8.0.33
+//DEPS org.mariadb.jdbc:mariadb-java-client:3.1.4
 //
 
 import java.util.function.Supplier;
