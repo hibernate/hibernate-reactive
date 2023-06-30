280ee9ca95207a79576c07d98ce5cab4875b4ab9
Davide D'Alto <davide@hibernate.org>
2023-06-30 15:29:27 +0200

    Upgrade JBang templates to HR 2.0.2.Final

--- tooling/jbang/Example.java
+++ tooling/jbang/Example.java
@@ -9,7 +9,7 @@
 //DEPS io.vertx:vertx-pg-client:${vertx.version:4.4.4}
 //DEPS io.vertx:vertx-mysql-client:${vertx.version:4.4.4}
 //DEPS io.vertx:vertx-db2-client:${vertx.version:4.4.4}
-//DEPS org.hibernate.reactive:hibernate-reactive-core:${hibernate-reactive.version:2.0.1.Final}
+//DEPS org.hibernate.reactive:hibernate-reactive-core:${hibernate-reactive.version:2.0.2.Final}
 //DEPS org.slf4j:slf4j-simple:2.0.7
 //DESCRIPTION Allow authentication to PostgreSQL using SCRAM:
 
