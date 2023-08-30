93cf559518e62336d885fbe8791ea511f9848547
Davide D'Alto <davide@hibernate.org>
2023-08-30 20:03:05 +0200

    [#1739] Upgrade Vert.x SQL client to 4.4.5

--- tooling/jbang/ReactiveTest.java
+++ tooling/jbang/ReactiveTest.java
@@ -5,11 +5,11 @@
  */
 
 ///usr/bin/env jbang "$0" "$@" ; exit $?
-//DEPS io.vertx:vertx-pg-client:${vertx.version:4.4.4}
+//DEPS io.vertx:vertx-pg-client:${vertx.version:4.4.5}
 //DEPS com.ongres.scram:client:2.1
-//DEPS io.vertx:vertx-db2-client:${vertx.version:4.4.4}
-//DEPS io.vertx:vertx-mysql-client:${vertx.version:4.4.4}
-//DEPS io.vertx:vertx-unit:${vertx.version:4.4.4}
+//DEPS io.vertx:vertx-db2-client:${vertx.version:4.4.5}
+//DEPS io.vertx:vertx-mysql-client:${vertx.version:4.4.5}
+//DEPS io.vertx:vertx-unit:${vertx.version:4.4.5}
 //DEPS org.hibernate.reactive:hibernate-reactive-core:${hibernate-reactive.version:2.0.2.Final}
 //DEPS org.assertj:assertj-core:3.24.2
 //DEPS junit:junit:4.13.2
