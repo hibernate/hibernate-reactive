3e71ceb28fce6740040c33e848431a0545f07541
Davide D'Alto <davide@hibernate.org>
2023-11-29 11:55:31 +0100

    [#1369] Test ConstraintViolationException SQLState code

--- hibernate-reactive-core/src/test/java/org/hibernate/reactive/MutinyExceptionsTest.java
+++ hibernate-reactive-core/src/test/java/org/hibernate/reactive/MutinyExceptionsTest.java
@@ -5,6 +5,7 @@
  */
 package org.hibernate.reactive;
 
+import java.sql.SQLException;
 import java.util.Collection;
 import java.util.List;
 
@@ -21,8 +22,11 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 import static java.util.concurrent.TimeUnit.MINUTES;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hibernate.reactive.containers.DatabaseConfiguration.DBType.DB2;
+import static org.hibernate.reactive.containers.DatabaseConfiguration.DBType.SQLSERVER;
+import static org.hibernate.reactive.containers.DatabaseConfiguration.dbType;
+import static org.hibernate.reactive.testing.ReactiveAssertions.assertThrown;
 
 @Timeout(value = 10, timeUnit = MINUTES)
 
@@ -33,23 +37,39 @@ public class MutinyExceptionsTest extends BaseReactiveTest {
 		return List.of( Person.class );
 	}
 
-	Class<?> getExpectedException() {
-		return ConstraintViolationException.class;
-	}
-
 	@Test
 	public void testDuplicateKeyException(VertxTestContext context) {
-		test( context, openMutinySession()
+		test( context, assertThrown( ConstraintViolationException.class, openMutinySession()
 				.call( session -> session.persist( new Person( "testFLush1", "unique" ) ) )
 				.call( Mutiny.Session::flush )
 				.call( session -> session.persist( new Person( "testFlush2", "unique" ) ) )
-				.call( Mutiny.Session::flush )
-				.invoke( ignore -> fail( "Expected exception not thrown" ) )
-				.onFailure().recoverWithItem( err -> {
-					assertEquals( getExpectedException(), err.getClass() );
-					return null;
-				} )
+				.call( Mutiny.Session::flush ) )
+				.invoke( MutinyExceptionsTest::assertSqlStateCode )
+				.invoke( MutinyExceptionsTest::assertConstraintName )
 		);
+	}
+
+	private static void assertSqlStateCode(ConstraintViolationException exception) {
+		if ( dbType() == SQLSERVER ) {
+			// The SQL state code is always null in Sql Server (see https://github.com/eclipse-vertx/vertx-sql-client/issues/1385)
+			// We test the vendor code for now
+			SQLException sqlException = (SQLException) exception.getCause();
+			assertThat( sqlException.getErrorCode() ).isEqualTo( 2601 );
+		}
+		else {
+			assertThat( exception.getSQLState() )
+					.as( "Constraint violation SQL state code should start with 23" )
+					.matches( "23\\d{3}" );
+		}
+	}
+
+	private static void assertConstraintName(ConstraintViolationException exception) {
+		// DB2 does not return the constraint name
+		if ( dbType() != DB2 ) {
+			assertThat( exception.getConstraintName() )
+					.as( "Failed constraint name should not be null" )
+					.isNotNull();
+		}
 	}
 
 	@Entity(name = "Person")
