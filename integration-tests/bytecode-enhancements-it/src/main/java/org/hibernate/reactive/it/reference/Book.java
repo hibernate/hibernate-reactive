b12770774882f9aaac76aadf0cf1e12e141e3386
Davide D'Alto <davide@hibernate.org>
2024-04-23 11:38:46 +0200

    [#1891] Add test case for getReference with BE
    Test `session.getReference` with Bytecode Enhancements enabled

---
+++ integration-tests/bytecode-enhancements-it/src/main/java/org/hibernate/reactive/it/reference/Book.java
@@ -0,0 +1,89 @@
+/* Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright: Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.reactive.it.reference;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.Version;
+
+@Entity(name = "Tome")
+@Table(name = "TBook")
+public class Book {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+	@Version
+	private Integer version = 1;
+	private String title;
+
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "book")
+	private List<Author> authors = new ArrayList<>();
+
+	public Book() {
+	}
+
+	public Book(String title) {
+		this.title = title;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public List<Author> getAuthors() {
+		return authors;
+	}
+
+	public void setAuthors(List<Author> authors) {
+		this.authors = authors;
+	}
+
+	public Integer getVersion() {
+		return version;
+	}
+
+	public void setVersion(Integer version) {
+		this.version = version;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if ( this == o ) {
+			return true;
+		}
+		if ( o == null || getClass() != o.getClass() ) {
+			return false;
+		}
+		Book book = (Book) o;
+		return Objects.equals( title, book.title );
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash( title );
+	}
+}
