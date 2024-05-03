33f1eb40b480240550631b384404e816435bbf60
Davide D'Alto <davide@hibernate.org>
2024-05-03 12:05:16 +0200

    [#1907] Hibernate Validator integration test

---
+++ integration-tests/hibernate-validator-postgres-it/src/main/java/org/hibernate/reactive/it/quarkus/qe/database/Author.java
@@ -0,0 +1,75 @@
+/* Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright: Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.reactive.it.quarkus.qe.database;
+
+import static jakarta.persistence.CascadeType.PERSIST;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+
+@Entity
+@Table(name = "authors")
+public class Author {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@NotNull
+	@Size(max = 10)
+	private String name;
+
+	@OneToMany(cascade = PERSIST)
+	@JoinColumn(name = "author")
+	private List<Book> books = new ArrayList<>();
+
+	public Author(String name) {
+		this.name = name;
+	}
+
+	public Author() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public @NotNull @Size(max = 10) String getName() {
+		return name;
+	}
+
+	public void setName(@NotNull @Size(max = 10) String name) {
+		this.name = name;
+	}
+
+	public List<Book> getBooks() {
+		return books;
+	}
+
+	public void setBooks(List<Book> books) {
+		this.books = books;
+	}
+
+	@Override
+	public String toString() {
+		return id + ":" + name;
+	}
+}
---
+++ integration-tests/hibernate-validator-postgres-it/src/main/java/org/hibernate/reactive/it/quarkus/qe/database/Book.java
@@ -0,0 +1,83 @@
+/* Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright: Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.reactive.it.quarkus.qe.database;
+
+import jakarta.persistence.Convert;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+
+@Entity
+@Table(name = "books")
+@NamedQuery(name = "find_by_title_prefix", query = "from Book where title like :prefix")
+public class Book {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@NotNull
+	@Size(max = 10)
+	private String title;
+
+	@NotNull
+	@ManyToOne
+	private Author author;
+
+	@Convert(converter = ISBNConverter.class)
+	private long isbn;
+
+	public Book() {
+	}
+
+	public Book(String title) {
+		this.title = title;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public @NotNull @Size(max = 10) String getTitle() {
+		return title;
+	}
+
+	public void setTitle(@NotNull @Size(max = 10) String title) {
+		this.title = title;
+	}
+
+	public @NotNull Author getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(@NotNull Author author) {
+		this.author = author;
+	}
+
+	public long getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(long isbn) {
+		this.isbn = isbn;
+	}
+
+	@Override
+	public String toString() {
+		return id + ":" + title + ":" + isbn + ":" + author;
+	}
+}
