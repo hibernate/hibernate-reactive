7e318e4868674538014a4ebf72a52a2116b056f2
Davide D'Alto <davide@hibernate.org>
2024-05-03 12:05:16 +0200

    [#1907] Hibernate Validator integration test

---
+++ integration-tests/hibernate-validator-postgres-it/src/main/java/org/hibernate/reactive/it/quarkus/qe/database/ISBNConverter.java
@@ -0,0 +1,44 @@
+/* Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright: Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.reactive.it.quarkus.qe.database;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter
+public class ISBNConverter implements AttributeConverter<Long, String> {
+
+	private static final int ISBN_MAX_LENGTH = 13;
+
+	@Override
+	/**
+	 * Canonical ISBN format: 978-3-16-148410-0, can be prefixed with zeroes if there is less than 13 digits
+	 */
+	public String convertToDatabaseColumn(Long number) {
+		String s = number.toString();
+		StringBuilder result = new StringBuilder( s );
+		if ( s.length() > ISBN_MAX_LENGTH ) {
+			throw new IllegalStateException( "ISBN " + s + " has a wrong length: " + number );
+		}
+		int paddingLength = ISBN_MAX_LENGTH - s.length();
+		result.insert( 0, "0".repeat( paddingLength ) );
+		result.insert( 3, '-' );
+		result.insert( 5, '-' );
+		result.insert( 8, '-' );
+		result.insert( 15, '-' );
+		return result.toString();
+	}
+
+	@Override
+	public Long convertToEntityAttribute(String s) {
+		if ( s == null ) {
+			return 0L;
+		}
+		else {
+			return Long.parseLong( s.replaceAll( "-", "" ) );
+		}
+	}
+}
---
+++ integration-tests/hibernate-validator-postgres-it/src/test/java/org/hibernate/reactive/it/quarkus/qe/database/DatabaseHibernateReactiveTest.java
@@ -0,0 +1,73 @@
+/* Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright: Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.reactive.it.quarkus.qe.database;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+
+import org.hibernate.HibernateException;
+
+import org.junit.jupiter.api.Test;
+
+import io.smallrye.mutiny.Uni;
+import io.vertx.junit5.Timeout;
+import io.vertx.junit5.VertxTestContext;
+import jakarta.validation.ConstraintViolationException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Timeout(value = 10, timeUnit = TimeUnit.MINUTES)
+public class DatabaseHibernateReactiveTest extends BaseReactiveIT {
+
+	@Override
+	protected Collection<Class<?>> annotatedEntities() {
+		return List.of( Book.class, Author.class );
+	}
+
+	/**
+	 * Assert that the expected exception type is thrown.
+	 *
+	 * @return a {@link Uni} with the expected exception thrown by the uni we were testing.
+	 */
+	public static <U extends Throwable> Uni<U> assertThrown(Class<U> expectedException, Uni<?> uni) {
+		return uni.onItemOrFailure().transform( (s, e) -> {
+			assertThat( e ).isInstanceOf( expectedException );
+			return (U) e;
+		} );
+	}
+
+	@Test
+	public void nameIsTooLong(VertxTestContext context) {
+		test( context, assertThrown( HibernateException.class, getMutinySessionFactory()
+				.withTransaction( s -> {
+					Author author = new Author();
+					author.setName( "A very long long long long name ... " );
+					return s.persist( author );
+				} ) )
+				.invoke( he -> assertThat( he.getCause() )
+						.isInstanceOf( ConstraintViolationException.class )
+						.hasMessageContaining( "size must be between" )
+				)
+		);
+	}
+
+	@Test
+	public void nameIsNull(VertxTestContext context) {
+		test( context, assertThrown( HibernateException.class, getMutinySessionFactory()
+				.withTransaction( s -> {
+					Author author = new Author();
+					author.setName( null );
+					return s.persist( author );
+				} ) )
+				.invoke( he -> assertThat( he.getCause() )
+						.isInstanceOf( ConstraintViolationException.class )
+						.hasMessageContaining( "must not be null" )
+				)
+		);
+	}
+}
