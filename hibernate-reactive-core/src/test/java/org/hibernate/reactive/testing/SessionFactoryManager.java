4d7c523317fc4429fe9ff10951b38a2e49868d76
Andrea Boriero <andrea@hibernate.org>
2025-02-12 10:37:20 +0100

    [#2055] SessionFactoryManager.stop() should always call SessionFactory.close()
    PersistentTemporaryTable dropping is obtained setting PersistentTableStrategy.DROP_ID_TABLES to true

--- hibernate-reactive-core/src/test/java/org/hibernate/reactive/testing/SessionFactoryManager.java
+++ hibernate-reactive-core/src/test/java/org/hibernate/reactive/testing/SessionFactoryManager.java
@@ -5,20 +5,13 @@
  */
 package org.hibernate.reactive.testing;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.CompletionStage;
 import java.util.function.Supplier;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.engine.spi.SessionFactoryImplementor;
-import org.hibernate.metamodel.spi.MappingMetamodelImplementor;
-import org.hibernate.persister.entity.EntityPersister;
-import org.hibernate.query.sqm.mutation.internal.temptable.PersistentTableStrategy;
 import org.hibernate.reactive.pool.ReactiveConnectionPool;
-import org.hibernate.reactive.query.sqm.mutation.internal.temptable.ReactivePersistentTableStrategy;
 
-import static org.hibernate.reactive.util.impl.CompletionStages.loop;
 import static org.hibernate.reactive.util.impl.CompletionStages.voidFuture;
 
 /**
@@ -60,24 +53,8 @@ public class SessionFactoryManager {
 	public CompletionStage<Void> stop() {
 		CompletionStage<Void> releasedStage = voidFuture();
 		if ( sessionFactory != null && sessionFactory.isOpen() ) {
-			SessionFactoryImplementor sessionFactoryImplementor = sessionFactory.unwrap( SessionFactoryImplementor.class );
-			MappingMetamodelImplementor mappingMetamodel = sessionFactoryImplementor
-					.getRuntimeMetamodels()
-					.getMappingMetamodel();
-			final List<ReactivePersistentTableStrategy> reactiveStrategies = new ArrayList<>();
-			mappingMetamodel.forEachEntityDescriptor(
-					entityPersister -> addPersistentTableStrategy( reactiveStrategies, entityPersister )
-			);
-			if ( !reactiveStrategies.isEmpty() ) {
-				releasedStage = loop( reactiveStrategies, strategy -> {
-					( (PersistentTableStrategy) strategy )
-							.release( sessionFactory.unwrap( SessionFactoryImplementor.class ), null );
-					return strategy.getDropTableActionStage();
-				} );
-
-				releasedStage = releasedStage
-						.whenComplete( (unused, throwable) -> sessionFactory.close() );
-			}
+			releasedStage = releasedStage
+					.whenComplete( (unused, throwable) -> sessionFactory.close() );
 		}
 		return releasedStage
 				.thenCompose( unused -> {
@@ -92,14 +69,5 @@ public class SessionFactoryManager {
 					sessionFactory = null;
 					return closeFuture;
 				} );
-	}
-
-	private void addPersistentTableStrategy(List<ReactivePersistentTableStrategy> reactiveStrategies, EntityPersister entityPersister) {
-		if ( entityPersister.getSqmMultiTableMutationStrategy() instanceof ReactivePersistentTableStrategy ) {
-			reactiveStrategies.add( (ReactivePersistentTableStrategy) entityPersister.getSqmMultiTableMutationStrategy() );
-		}
-		if ( entityPersister.getSqmMultiTableInsertStrategy() instanceof ReactivePersistentTableStrategy ) {
-			reactiveStrategies.add( (ReactivePersistentTableStrategy) entityPersister.getSqmMultiTableInsertStrategy() );
-		}
 	}
 }
