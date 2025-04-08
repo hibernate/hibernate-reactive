6e201f98910fcd3e0963fa8364290db91a27879b
Davide D'Alto <davide@hibernate.org>
2025-04-08 15:11:48 +0200

    [#2190] Upgrade CockroachDB to 24.1.15

--- tooling/jbang/ReactiveTest.java
+++ tooling/jbang/ReactiveTest.java
@@ -232,7 +232,7 @@ public class ReactiveTest {
 		MYSQL( () -> new MySQLContainer( "mysql:9.2.0" ) ),
 		DB2( () -> new Db2Container( "docker.io/icr.io/db2_community/db2:12.1.0.0" ).acceptLicense() ),
 		MARIADB( () -> new MariaDBContainer( "mariadb:11.7.2" ) ),
-		COCKROACHDB( () -> new CockroachContainer( "cockroachdb/cockroach:v24.1.0" ) );
+		COCKROACHDB( () -> new CockroachContainer( "cockroachdb/cockroach:v24.1.15" ) );
 
 		private final Supplier<JdbcDatabaseContainer<?>> containerSupplier;
 
