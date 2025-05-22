7e21227c1ce0160fbe0c96c4ff08038223d0a2a5
Davide D'Alto <davide@hibernate.org>
2025-05-22 14:52:56 +0200

    [#2272] Switch from 3.0 to 4.0
    * Update JBang templates
    * Update development version to 4.0.0-SNAPSHOT
    * Enable GitHub workflow on the 4 branches and tags

--- tooling/jbang/Example.java
+++ tooling/jbang/Example.java
@@ -9,7 +9,7 @@
 //DEPS io.vertx:vertx-pg-client:${vertx.version:4.5.15}
 //DEPS io.vertx:vertx-mysql-client:${vertx.version:4.5.15}
 //DEPS io.vertx:vertx-db2-client:${vertx.version:4.5.15}
-//DEPS org.hibernate.reactive:hibernate-reactive-core:${hibernate-reactive.version:3.0.0.Final}
+//DEPS org.hibernate.reactive:hibernate-reactive-core:${hibernate-reactive.version:4.0.0.Final}
 //DEPS org.slf4j:slf4j-simple:2.0.7
 //DESCRIPTION Allow authentication to PostgreSQL using SCRAM:
 
