10a5bd39541d2b9bbedfee6b5939f403b22f049a
Andrea Boriero <andrea@hibernate.org>
2025-06-07 16:08:10 +0200

    [#2154] Composite key with generated value not working properly

--- hibernate-reactive-core/src/main/java/org/hibernate/reactive/tuple/entity/ReactiveEntityMetamodel.java
+++ hibernate-reactive-core/src/main/java/org/hibernate/reactive/tuple/entity/ReactiveEntityMetamodel.java
@@ -8,8 +8,11 @@ package org.hibernate.reactive.tuple.entity;
 import java.util.function.Function;
 
 
+import org.hibernate.boot.model.relational.Database;
+import org.hibernate.boot.model.relational.SqlStringGenerationContext;
 import org.hibernate.generator.Generator;
 import org.hibernate.generator.GeneratorCreationContext;
+import org.hibernate.id.CompositeNestedGeneratedValueGenerator;
 import org.hibernate.id.Configurable;
 import org.hibernate.id.IdentifierGenerator;
 import org.hibernate.id.SelectGenerator;
@@ -18,18 +21,23 @@ import org.hibernate.id.enhanced.SequenceStructure;
 import org.hibernate.id.enhanced.SequenceStyleGenerator;
 import org.hibernate.id.enhanced.TableGenerator;
 import org.hibernate.id.enhanced.TableStructure;
-import org.hibernate.mapping.GeneratorCreator;
+import org.hibernate.mapping.GeneratorSettings;
 import org.hibernate.mapping.PersistentClass;
+import org.hibernate.mapping.Property;
+import org.hibernate.mapping.RootClass;
 import org.hibernate.mapping.SimpleValue;
 import org.hibernate.metamodel.spi.RuntimeModelCreationContext;
 import org.hibernate.persister.entity.EntityPersister;
 import org.hibernate.reactive.id.ReactiveIdentifierGenerator;
 import org.hibernate.reactive.id.impl.EmulatedSequenceReactiveIdentifierGenerator;
+import org.hibernate.reactive.id.impl.ReactiveCompositeNestedGeneratedValueGenerator;
 import org.hibernate.reactive.id.impl.ReactiveGeneratorWrapper;
 import org.hibernate.reactive.id.impl.ReactiveSequenceIdentifierGenerator;
 import org.hibernate.reactive.id.impl.TableReactiveIdentifierGenerator;
 import org.hibernate.reactive.logging.impl.Log;
+import org.hibernate.service.ServiceRegistry;
 import org.hibernate.tuple.entity.EntityMetamodel;
+import org.hibernate.type.Type;
 
 import static java.lang.invoke.MethodHandles.lookup;
 import static org.hibernate.reactive.logging.impl.LoggerFactory.make;
@@ -67,20 +75,22 @@ public class ReactiveEntityMetamodel extends EntityMetamodel {
 			return existing;
 		}
 		else {
-			SimpleValue identifier = (SimpleValue) persistentClass.getIdentifier();
-			GeneratorCreator customIdGeneratorCreator = identifier.getCustomIdGeneratorCreator();
-			identifier.setCustomIdGeneratorCreator( context -> {
-				Generator generator = customIdGeneratorCreator.createGenerator( context );
-				return augmentWithReactiveGenerator( generator, context, creationContext );
-			} );
-			final Generator idgenerator = identifier
-					// returns the cached Generator if it was already created
-					.createGenerator(
+			final SimpleValue identifier = (SimpleValue) persistentClass.getIdentifier();
+			final Generator idgenerator = augmentWithReactiveGenerator(
+					identifier.createGenerator(
 							creationContext.getDialect(),
 							persistentClass.getRootClass(),
 							persistentClass.getIdentifierProperty(),
 							creationContext.getGeneratorSettings()
-					);
+					),
+					new IdGeneratorCreationContext(
+							persistentClass.getRootClass(),
+							persistentClass.getIdentifierProperty(),
+							creationContext.getGeneratorSettings(),
+							identifier,
+							creationContext
+					),
+					creationContext );
 			creationContext.getGenerators().put( rootName, idgenerator );
 			return idgenerator;
 		}
@@ -90,8 +100,8 @@ public class ReactiveEntityMetamodel extends EntityMetamodel {
 			Generator generator,
 			GeneratorCreationContext creationContext,
 			RuntimeModelCreationContext runtimeModelCreationContext) {
-		if ( generator instanceof SequenceStyleGenerator ) {
-			final DatabaseStructure structure = ( (SequenceStyleGenerator) generator ).getDatabaseStructure();
+		if ( generator instanceof SequenceStyleGenerator sequenceStyleGenerator) {
+			final DatabaseStructure structure = sequenceStyleGenerator.getDatabaseStructure();
 			if ( structure instanceof TableStructure ) {
 				return initialize( (IdentifierGenerator) generator, new EmulatedSequenceReactiveIdentifierGenerator( (TableStructure) structure, runtimeModelCreationContext ), creationContext );
 			}
@@ -100,15 +110,27 @@ public class ReactiveEntityMetamodel extends EntityMetamodel {
 			}
 			throw LOG.unknownStructureType();
 		}
-		if ( generator instanceof TableGenerator ) {
+		if ( generator instanceof TableGenerator tableGenerator ) {
 			return initialize(
 					(IdentifierGenerator) generator,
-					new TableReactiveIdentifierGenerator( (TableGenerator) generator, runtimeModelCreationContext ),
+					new TableReactiveIdentifierGenerator( tableGenerator, runtimeModelCreationContext ),
 					creationContext
 			);
 		}
 		if ( generator instanceof SelectGenerator ) {
 			throw LOG.selectGeneratorIsNotSupportedInHibernateReactive();
+		}
+		if ( generator instanceof CompositeNestedGeneratedValueGenerator compositeNestedGeneratedValueGenerator ) {
+			final ReactiveCompositeNestedGeneratedValueGenerator reactiveCompositeNestedGeneratedValueGenerator = new ReactiveCompositeNestedGeneratedValueGenerator(
+					compositeNestedGeneratedValueGenerator,
+					creationContext,
+					runtimeModelCreationContext
+			);
+			return initialize(
+					(IdentifierGenerator) generator,
+					reactiveCompositeNestedGeneratedValueGenerator,
+					creationContext
+			);
 		}
 		//nothing to do
 		return generator;
@@ -120,5 +142,58 @@ public class ReactiveEntityMetamodel extends EntityMetamodel {
 			GeneratorCreationContext creationContext) {
 		( (Configurable) reactiveIdGenerator ).initialize( creationContext.getSqlStringGenerationContext() );
 		return new ReactiveGeneratorWrapper( reactiveIdGenerator, idGenerator );
+	}
+
+	private record IdGeneratorCreationContext(
+			RootClass rootClass,
+			Property property,
+			GeneratorSettings defaults,
+			SimpleValue identifier,
+			RuntimeModelCreationContext buildingContext) implements GeneratorCreationContext {
+
+		@Override
+		public Database getDatabase() {
+			return buildingContext.getBootModel().getDatabase();
+		}
+
+		@Override
+		public ServiceRegistry getServiceRegistry() {
+			return buildingContext.getBootstrapContext().getServiceRegistry();
+		}
+
+		@Override
+		public SqlStringGenerationContext getSqlStringGenerationContext() {
+			return defaults.getSqlStringGenerationContext();
+		}
+
+		@Override
+		public String getDefaultCatalog() {
+			return defaults.getDefaultCatalog();
+		}
+
+		@Override
+		public String getDefaultSchema() {
+			return defaults.getDefaultSchema();
+		}
+
+		@Override
+		public RootClass getRootClass() {
+			return rootClass;
+		}
+
+		@Override
+		public PersistentClass getPersistentClass() {
+			return rootClass;
+		}
+
+		@Override
+		public Property getProperty() {
+			return property;
+		}
+
+		@Override
+		public Type getType() {
+			return identifier.getType();
+		}
 	}
 }
