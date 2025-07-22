f7851a78576ba86baa83bb07bdaa342ef69a0ac9
Davide D'Alto <davide@hibernate.org>
2025-07-22 15:05:36 +0200

    Adapt to the rename of SqmQueryImpl

--- hibernate-reactive-core/src/main/java/org/hibernate/reactive/query/sqm/ReactiveSqmSelectionQuery.java
+++ hibernate-reactive-core/src/main/java/org/hibernate/reactive/query/sqm/ReactiveSqmSelectionQuery.java
@@ -24,7 +24,7 @@ import jakarta.persistence.metamodel.Type;
 /**
  * @see org.hibernate.query.sqm.SqmSelectionQuery
  */
-public interface ReactiveSqmSelectionQuery<R> extends ReactiveSelectionQuery<R>, SqmQuery {
+public interface ReactiveSqmSelectionQuery<R> extends ReactiveSelectionQuery<R>, SqmQuery<R> {
 
 	@Override
 	ReactiveSqmSelectionQuery<R> setParameter(String name, Object value);
