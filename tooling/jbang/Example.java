9121ab41d844ee0f9d23b5bd87e4a1a7d6e2c897
Davide D'Alto <davide@hibernate.org>
2025-08-11 10:42:54 +0200

    [#2413] Upgrade Vert.x to 5.0.2

--- tooling/jbang/Example.java
+++ tooling/jbang/Example.java
@@ -6,9 +6,9 @@
  */
 
 //DEPS com.ongres.scram:scram-client:3.1
-//DEPS io.vertx:vertx-pg-client:${vertx.version:5.0.0}
-//DEPS io.vertx:vertx-mysql-client:${vertx.version:5.0.0}
-//DEPS io.vertx:vertx-db2-client:${vertx.version:5.0.0}
+//DEPS io.vertx:vertx-pg-client:${vertx.version:5.0.2}
+//DEPS io.vertx:vertx-mysql-client:${vertx.version:5.0.2}
+//DEPS io.vertx:vertx-db2-client:${vertx.version:5.0.2}
 //DEPS org.hibernate.reactive:hibernate-reactive-core:${hibernate-reactive.version:4.0.0.Final}
 //DEPS org.slf4j:slf4j-simple:2.0.7
 //DESCRIPTION Allow authentication to PostgreSQL using SCRAM:
