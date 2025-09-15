0090099f8f7656dcdecf05ae97afb32794cb5ccc
Andrea Boriero <andrea@hibernate.org>
2025-09-15 14:54:00 +0200

    [#1770] Fix Better error message when transparently loading lazy fields

---
+++ hibernate-reactive-core/src/main/java/org/hibernate/reactive/metamodel/internal/ReactiveEntityInstantiatorPojoOptimized.java
@@ -0,0 +1,42 @@
+/* Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright: Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.reactive.metamodel.internal;
+
+import org.hibernate.bytecode.spi.ReflectionOptimizer;
+import org.hibernate.mapping.PersistentClass;
+import org.hibernate.metamodel.internal.EntityInstantiatorPojoOptimized;
+import org.hibernate.reactive.bythecode.enhance.spi.internal.ReactiveLazyAttributeLoadingInterceptor;
+import org.hibernate.tuple.entity.EntityMetamodel;
+import org.hibernate.type.descriptor.java.JavaType;
+
+import static org.hibernate.engine.internal.ManagedTypeHelper.asPersistentAttributeInterceptable;
+
+/**
+ * Extends {@link EntityInstantiatorPojoOptimized} to apply a {@link ReactiveLazyAttributeLoadingInterceptor}
+ */
+public class ReactiveEntityInstantiatorPojoOptimized extends EntityInstantiatorPojoOptimized {
+
+	public ReactiveEntityInstantiatorPojoOptimized(
+			EntityMetamodel entityMetamodel,
+			PersistentClass persistentClass,
+			JavaType<?> javaType,
+			ReflectionOptimizer.InstantiationOptimizer instantiationOptimizer) {
+		super( entityMetamodel, persistentClass, javaType, instantiationOptimizer );
+	}
+
+	@Override
+	protected Object applyInterception(Object entity) {
+		if ( isApplyBytecodeInterception() ) {
+			asPersistentAttributeInterceptable( entity )
+					.$$_hibernate_setInterceptor( new ReactiveLazyAttributeLoadingInterceptor(
+							getLoadingInterceptorState(),
+							null,
+							null
+					) );
+		}
+		return entity;
+	}
+}
