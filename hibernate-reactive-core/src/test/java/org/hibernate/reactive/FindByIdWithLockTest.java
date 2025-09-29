bc0d97e615c61562ccdc95df223e00bc90622edf
Andrea Boriero <andrea@hibernate.org>
2025-09-29 17:58:01 +0200

    [#2534] Add test for session.find() will not respect NOWAIT locks starting from HR 3.1

--- hibernate-reactive-core/src/test/java/org/hibernate/reactive/FindByIdWithLockTest.java
+++ hibernate-reactive-core/src/test/java/org/hibernate/reactive/FindByIdWithLockTest.java
@@ -5,11 +5,18 @@
  */
 package org.hibernate.reactive;
 
+import org.hibernate.LockMode;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.reactive.containers.DatabaseConfiguration;
+import org.hibernate.reactive.testing.SqlStatementTracker;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import io.vertx.junit5.Timeout;
@@ -22,10 +29,43 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hibernate.reactive.containers.DatabaseConfiguration.dbType;
 
 @Timeout(value = 10, timeUnit = TimeUnit.MINUTES)
 public class FindByIdWithLockTest extends BaseReactiveTest {
 	private static final Long CHILD_ID = 1L;
+
+	private static SqlStatementTracker sqlTracker;
+
+	@Override
+	protected Configuration constructConfiguration() {
+		Configuration configuration = super.constructConfiguration();
+
+		// Construct a tracker that collects query statements via the SqlStatementLogger framework.
+		// Pass in configuration properties to hand off any actual logging properties
+		sqlTracker = new SqlStatementTracker( FindByIdWithLockTest::filter, configuration.getProperties() );
+		return configuration;
+	}
+
+	@BeforeEach
+	public void clearTracker() {
+		sqlTracker.clear();
+	}
+
+	protected void addServices(StandardServiceRegistryBuilder builder) {
+		sqlTracker.registerService( builder );
+	}
+
+	private static boolean filter(String s) {
+		String[] accepted = { "select " };
+		for ( String valid : accepted ) {
+			if ( s.toLowerCase().startsWith( valid ) ) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 
 	@Override
 	protected Collection<Class<?>> annotatedEntities() {
@@ -49,6 +89,43 @@ public class FindByIdWithLockTest extends BaseReactiveTest {
 										) ) )
 		);
 	}
+
+	@Test
+	public void testFindUpgradeNoWait(VertxTestContext context) {
+		Child child = new Child( CHILD_ID, "And" );
+		test(
+				context, getMutinySessionFactory()
+						.withTransaction( session -> session.persistAll( child ) )
+						.invoke( () -> sqlTracker.clear() )
+						.chain( () -> getMutinySessionFactory().withTransaction( session -> session
+										.find( Child.class, CHILD_ID, LockMode.UPGRADE_NOWAIT )
+								.invoke( c -> {
+											 assertThat( c ).isNotNull();
+											 assertThat( c.getId() ).isEqualTo( CHILD_ID );
+											 String selectQuery = sqlTracker.getLoggedQueries().get( 0 );
+											 assertThat( sqlTracker.getLoggedQueries() ).hasSize( 1 );
+											 switch ( DatabaseConfiguration.dbType() ) {
+												 case POSTGRESQL -> assertThat( selectQuery )
+														 .endsWith( "for no key update of c1_0 nowait" );
+												 case COCKROACHDB -> assertThat( selectQuery )
+														 .endsWith( "for update of c1_0 nowait" );
+												 case SQLSERVER -> assertThat( selectQuery )
+														 .contains( "with (updlock,holdlock,rowlock,nowait)" );
+												 case ORACLE -> assertThat( selectQuery )
+														 .contains( "for update of c1_0.id nowait" );
+												 case DB2 -> assertThat( selectQuery )
+														 .contains( "for read only with rs use and keep update locks" ); // DB2 does not support nowait
+												 case MARIA -> assertThat( selectQuery )
+														 .contains( "for update nowait" );
+												 case MYSQL -> assertThat( selectQuery )
+														 .contains( "for update of c1_0 nowait" );
+												 default -> throw new IllegalArgumentException( "Database not recognized: " + dbType().name() );
+											 }
+										 }
+										) ) )
+		);
+	}
+
 
 	@Entity(name = "Parent")
 	public static class Parent {
