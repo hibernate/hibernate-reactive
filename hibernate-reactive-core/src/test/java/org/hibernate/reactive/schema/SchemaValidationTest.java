22f307c415060a198701ea3ed1e3ff4b081eb86f
Davide D'Alto <davide@hibernate.org>
2025-11-07 11:41:37 +0100

    [#2738] Test column type validation in Oracle

--- hibernate-reactive-core/src/test/java/org/hibernate/reactive/schema/SchemaValidationTest.java
+++ hibernate-reactive-core/src/test/java/org/hibernate/reactive/schema/SchemaValidationTest.java
@@ -6,13 +6,16 @@
 package org.hibernate.reactive.schema;
 
 
+import java.net.URL;
 import java.util.concurrent.CompletionStage;
 import java.util.stream.Stream;
 
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.reactive.BaseReactiveTest;
 import org.hibernate.reactive.annotations.DisabledFor;
+import org.hibernate.reactive.annotations.EnabledFor;
 import org.hibernate.reactive.provider.Settings;
 import org.hibernate.tool.schema.spi.SchemaManagementException;
 
@@ -23,6 +26,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import io.vertx.junit5.Timeout;
 import io.vertx.junit5.VertxTestContext;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -34,6 +38,7 @@ import static org.hibernate.cfg.SchemaToolingSettings.HBM2DDL_JDBC_METADATA_EXTR
 import static org.hibernate.reactive.containers.DatabaseConfiguration.DBType.DB2;
 import static org.hibernate.reactive.containers.DatabaseConfiguration.DBType.MARIA;
 import static org.hibernate.reactive.containers.DatabaseConfiguration.DBType.MYSQL;
+import static org.hibernate.reactive.containers.DatabaseConfiguration.DBType.ORACLE;
 import static org.hibernate.reactive.testing.ReactiveAssertions.assertThrown;
 import static org.hibernate.tool.schema.JdbcMetadataAccessStrategy.GROUPED;
 import static org.hibernate.tool.schema.JdbcMetadataAccessStrategy.INDIVIDUALLY;
@@ -47,7 +52,7 @@ import static org.junit.jupiter.params.provider.Arguments.arguments;
  * - TODO: Test that validation fails when a column is the wrong type
  */
 @DisabledFor(value = DB2, reason = "We don't have an information extractor. See https://github.com/hibernate/hibernate-reactive/issues/911")
-@DisabledFor(value = {MARIA, MYSQL}, reason = "HHH-18869: Schema creation creates an invalid schema")
+@DisabledFor(value = { MARIA, MYSQL }, reason = "HHH-18869: Schema creation creates an invalid schema")
 public class SchemaValidationTest extends BaseReactiveTest {
 
 	static Stream<Arguments> settings() {
@@ -77,9 +82,18 @@ public class SchemaValidationTest extends BaseReactiveTest {
 	}
 
 	public CompletionStage<Void> setupFactory(String strategy, String type) {
+		return setupFactory( strategy, type, null );
+	}
+
+	public CompletionStage<Void> setupFactory(String strategy, String type, String importFile) {
 		Configuration createConf = constructConfiguration( "create", strategy, type );
 		createConf.addAnnotatedClass( BasicTypesTestEntity.class );
-
+		if ( importFile != null ) {
+			final URL importFileURL = Thread.currentThread()
+					.getContextClassLoader()
+					.getResource( importFile );
+			createConf.setProperty( AvailableSettings.JAKARTA_HBM2DDL_LOAD_SCRIPT_SOURCE, importFileURL.getFile() );
+		}
 		// Make sure that the extra table is not in the db
 		Configuration dropConf = constructConfiguration( "drop", strategy, type );
 		dropConf.addAnnotatedClass( Extra.class );
@@ -95,6 +109,21 @@ public class SchemaValidationTest extends BaseReactiveTest {
 	public void after(VertxTestContext context) {
 		super.after( context );
 		closeFactory( context );
+	}
+
+	@ParameterizedTest
+	@MethodSource("settings")
+	@EnabledFor( ORACLE )
+	public void testOracleColumnTypeValidation(final String strategy, final String type, VertxTestContext context) {
+		test(
+				context, setupFactory( strategy, type, "oracle-SchemaValidationTest.sql" )
+						.thenCompose( v -> {
+							Configuration validateConf = constructConfiguration( "validate", strategy, type );
+							validateConf.addAnnotatedClass( Fruit.class );
+							new StandardServiceRegistryBuilder().applySettings( validateConf.getProperties() );
+							return setupSessionFactory( validateConf );
+						} )
+		);
 	}
 
 	// When we have created the table, the validation should pass
@@ -146,5 +175,44 @@ public class SchemaValidationTest extends BaseReactiveTest {
 		private Integer id;
 
 		private String description;
+	}
+
+	@Entity(name = "Fruit")
+	public static class Fruit {
+
+		@Id
+		@GeneratedValue
+		private Integer id;
+
+		@Column(name = "something_name", nullable = false, updatable = false)
+		private String name;
+
+		public Fruit() {
+		}
+
+		public Fruit(String name) {
+			this.name = name;
+		}
+
+		public Integer getId() {
+			return id;
+		}
+
+		public void setId(Integer id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		@Override
+		public String toString() {
+			return "Fruit{" + id + "," + name + '}';
+		}
 	}
 }
