b4b485d07a78947455720195bdf597b57e540bec
Davide D'Alto <davide@hibernate.org>
2025-12-01 18:44:44 +0100

    [#2855] Update spotless.license.java
    Hibernate Reactive is using a license header for the
    java file that's a little different from the other projects

--- spotless.license.java
+++ spotless.license.java
@@ -1,5 +1,4 @@
-/* Hibernate, Relational Persistence for Idiomatic Java
- *
+/*
  * SPDX-License-Identifier: Apache-2.0
- * Copyright: Red Hat Inc. and Hibernate Authors
+ * Copyright Red Hat Inc. and Hibernate Authors
  */
