603c9ac4720a67504386dd2495eae6ba882dc76f
Davide D'Alto <davide@hibernate.org>
2025-12-02 15:36:12 +0100

    [#2855] Update license header in classes and package-info

--- hibernate-reactive-core/src/test/java/org/hibernate/reactive/package-info.java
+++ hibernate-reactive-core/src/test/java/org/hibernate/reactive/package-info.java
@@ -1,3 +1,7 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright Red Hat Inc. and Hibernate Authors
+ */
 /**
  * Used in {@link org.hibernate.reactive.FilterTest}
  */
--- hibernate-reactive-core/src/test/java/org/hibernate/reactive/testing/ReactiveAssertions.java
+++ hibernate-reactive-core/src/test/java/org/hibernate/reactive/testing/ReactiveAssertions.java
@@ -1,7 +1,6 @@
-/* Hibernate, Relational Persistence for Idiomatic Java
- *
+/*
  * SPDX-License-Identifier: Apache-2.0
- * Copyright: Red Hat Inc. and Hibernate Authors
+ * Copyright Red Hat Inc. and Hibernate Authors
  */
 package org.hibernate.reactive.testing;
 
