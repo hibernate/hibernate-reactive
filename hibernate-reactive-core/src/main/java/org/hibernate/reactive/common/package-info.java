5a7cf9b0e8786c26d9aefcfb9fc337f55a2c6b62
Davide D'Alto <davide@hibernate.org>
2025-12-02 13:49:39 +0100

    [#2855] Update license header in classes and package-info

--- hibernate-reactive-core/src/main/java/org/hibernate/reactive/common/package-info.java
+++ hibernate-reactive-core/src/main/java/org/hibernate/reactive/common/package-info.java
@@ -1,3 +1,7 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright Red Hat Inc. and Hibernate Authors
+ */
 /**
  * APIs common to both {@link org.hibernate.reactive.stage.Stage}
  * and {@link org.hibernate.reactive.mutiny.Mutiny}.
