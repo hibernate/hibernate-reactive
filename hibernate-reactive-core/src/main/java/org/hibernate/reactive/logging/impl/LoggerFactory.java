0646de75e4f344e5d46bf0a5d714c2d320a6af7e
Rey <89825425+kingg22@users.noreply.github.com>
2026-02-02 09:38:43 -0500

    [#3134] Pass a ROOT locale to loggers to avoid unnecessary class lookups
    JBoss Logging attempts to resolve localized message classes when creating
    message loggers. For libraries that do not ship any translations, this
    results in unnecessary negative class lookups during startup.
    
    Explicitly passing Locale.ROOT avoids these lookups, reducing startup
    overhead and improving AOT friendliness (e.g., Quarkus / native images).
    
    If and when this module introduces localized log messages, this decision
    must be revisited.
    
    See https://github.com/hibernate/hibernate-reactive/pull/3072#discussion_r2753618291

--- hibernate-reactive-core/src/main/java/org/hibernate/reactive/logging/impl/LoggerFactory.java
+++ hibernate-reactive-core/src/main/java/org/hibernate/reactive/logging/impl/LoggerFactory.java
@@ -5,6 +5,7 @@
 package org.hibernate.reactive.logging.impl;
 
 import java.lang.invoke.MethodHandles;
+import java.util.Locale;
 
 import org.jboss.logging.Logger;
 
@@ -14,12 +15,17 @@ public final class LoggerFactory {
 		// Not allowed
 	}
 
+	// NOTE: Locale.ROOT is passed intentionally.
+	//
+	// If localized log messages are introduced in the future, this must be
+	// revisited and the explicit ROOT locale removed.
+
 	public static <T> T make(Class<T> logClass, MethodHandles.Lookup creationContext) {
 		final String className = creationContext.lookupClass().getName();
-		return Logger.getMessageLogger( creationContext, logClass, className );
+		return Logger.getMessageLogger( creationContext, logClass, className, Locale.ROOT );
 	}
 
 	public static <T> T make(Class<T> logClass, LogCategory category, MethodHandles.Lookup creationContext) {
-		return Logger.getMessageLogger( creationContext, logClass, category.getName() );
+		return Logger.getMessageLogger( creationContext, logClass, category.getName(), Locale.ROOT );
 	}
 }
