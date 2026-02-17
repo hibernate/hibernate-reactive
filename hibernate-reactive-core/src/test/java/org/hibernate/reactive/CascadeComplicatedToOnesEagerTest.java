045d0da1dca367ee2167147d611b8df6aa48b75c
Davide D'Alto <davide@hibernate.org>
2026-02-17 09:39:34 +0100

    [#3168] Minor test code clean up
    * Align source code format
    * Use static imports
    * Remove unecessary method parameters
    * Fix typos in javadoc

--- hibernate-reactive-core/src/test/java/org/hibernate/reactive/CascadeComplicatedToOnesEagerTest.java
+++ hibernate-reactive-core/src/test/java/org/hibernate/reactive/CascadeComplicatedToOnesEagerTest.java
@@ -18,67 +18,68 @@ import org.junit.jupiter.api.Test;
 import io.vertx.junit5.Timeout;
 import io.vertx.junit5.VertxTestContext;
 import jakarta.persistence.Basic;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.OneToMany;
 
+import static jakarta.persistence.CascadeType.MERGE;
+import static jakarta.persistence.CascadeType.PERSIST;
+import static jakarta.persistence.CascadeType.REFRESH;
+import static jakarta.persistence.GenerationType.SEQUENCE;
 import static java.util.concurrent.TimeUnit.MINUTES;
-import static org.hibernate.reactive.util.impl.CompletionStages.voidFuture;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hibernate.reactive.util.impl.CompletionStages.voidFuture;
 
 /**
  * This test uses a complicated model that requires Hibernate to delay
  * inserts until non-nullable transient entity dependencies are resolved.
- *
+ * <p>
  * All IDs are generated from a sequence.
- *
+ * <p>
  * JPA cascade types are used (jakarta.persistence.CascadeType).
- *
+ * <p>
  * This test uses the following model:
- *
  * <code>
- *     ------------------------------ N G
- *     |
- *     |                                1
- *     |                                |
- *     |                                |
- *     |                                N
- *     |
- *     |         E N--------------0,1 * F
- *     |
- *     |         1                      N
- *     |         |                      |
- *     |         |                      |
- *     1         N                      |
- *     *                                |
- *     B * N---1 D * 1------------------
- *     *
- *     N         N
- *     |         |
- *     |         |
- *     1         |
- *               |
- *     C * 1-----
- *</code>
- *
+ * ------------------------------ N G
+ * |
+ * |                                1
+ * |                                |
+ * |                                |
+ * |                                N
+ * |
+ * |         E N--------------0,1 * F
+ * |
+ * |         1                      N
+ * |         |                      |
+ * |         |                      |
+ * 1         N                      |
+ * *                                |
+ * B * N---1 D * 1------------------
+ * *
+ * N         N
+ * |         |
+ * |         |
+ * 1         |
+ * |
+ * C * 1-----
+ * </code>
+ * <p>
  * In the diagram, all associations are bidirectional;
- * assocations marked with '*' cascade persist, save, merge operations to the
+ * associations marked with '*' cascade persist, save, merge operations to the
  * associated entities (e.g., B cascades persist to D, but D does not cascade
  * persist to B);
- *
+ * <p>
  * All many-to-one associations are eager; all collection associations are lazy.
- *
+ * <p>
  * b, c, d, e, f, and g are all transient unsaved that are associated with each other.
- *
+ * <p>
  * When persisting b with ORM, the entities are added to the ActionQueue in the following order:
  * c, d (depends on e), f (depends on d, g), e, b, g.
- *
+ * <p>
  * Entities are inserted in the following order:
  * c, e, d, b, g, f.
  */
@@ -112,8 +113,8 @@ public class CascadeComplicatedToOnesEagerTest extends BaseReactiveTest {
 		test(
 				context,
 				openSession()
-						.thenCompose( s -> s.persist(b).thenAccept(v -> bId = b.id).thenCompose(v -> s.flush()) )
-						.thenCompose( ignore -> check( openSession(), context ) )
+						.thenCompose( s -> s.persist( b ).thenAccept( v -> bId = b.id ).thenCompose( v -> s.flush() ) )
+						.thenCompose( ignore -> check( openSession() ) )
 		);
 	}
 
@@ -122,8 +123,10 @@ public class CascadeComplicatedToOnesEagerTest extends BaseReactiveTest {
 		test(
 				context,
 				openSession()
-						.thenCompose( s -> s.merge(b).thenAccept(bMerged -> bId = bMerged.id).thenCompose(v -> s.flush()) )
-						.thenCompose( v -> check(openSession(), context) )
+						.thenCompose( s -> s.merge( b )
+								.thenAccept( bMerged -> bId = bMerged.id )
+								.thenCompose( v -> s.flush() ) )
+						.thenCompose( v -> check( openSession() ) )
 		);
 	}
 
@@ -132,11 +135,14 @@ public class CascadeComplicatedToOnesEagerTest extends BaseReactiveTest {
 		test(
 				context,
 				openSession()
-						.thenCompose( s -> s.persist(b).thenAccept(v -> bId = b.id).thenCompose(v -> s.flush()) )
-						.thenCompose(ignore -> openSession()
-								.thenCompose(s2 -> s2.merge(b))
+						.thenCompose( s -> s.persist( b )
+								.thenAccept( v -> bId = b.id )
+								.thenCompose( v -> s.flush() )
 						)
-						.thenCompose(v -> check(openSession(), context))
+						.thenCompose( ignore -> openSession()
+								.thenCompose( s2 -> s2.merge( b ) )
+						)
+						.thenCompose( v -> check( openSession() ) )
 		);
 	}
 
@@ -147,46 +153,49 @@ public class CascadeComplicatedToOnesEagerTest extends BaseReactiveTest {
 		test(
 				context,
 				openSession()
-						.thenCompose( s -> s.persist(b).thenAccept(v -> bId = b.id).thenCompose(v -> s.flush()) )
-						.thenCompose(ignore -> check( openSession(), context ))
-						.thenAccept(ignore -> {
+						.thenCompose( s -> s.persist( b )
+								.thenAccept( v -> bId = b.id )
+								.thenCompose( v -> s.flush() )
+						)
+						.thenCompose( ignore -> check( openSession() ) )
+						.thenAccept( ignore -> {
 							// Cascade-remove is not configured, so remove all associations.
 							// Everything will need to be merged, then deleted in the proper order
 							prepareEntitiesForDelete();
-						})
-						.thenCompose(v -> openSession())
-						.thenCompose(s2 -> s2.merge(b).thenApply(merged -> {
+						} )
+						.thenCompose( v -> openSession() )
+						.thenCompose( s2 -> s2.merge( b ).thenApply( merged -> {
 							b = merged;
 							return s2;
-						}))
-						.thenCompose(s2 -> s2.merge(c).thenApply(merged -> {
+						} ) )
+						.thenCompose( s2 -> s2.merge( c ).thenApply( merged -> {
 							c = merged;
 							return s2;
-						}))
-						.thenCompose(s2 -> s2.merge(d).thenApply(merged -> {
+						} ) )
+						.thenCompose( s2 -> s2.merge( d ).thenApply( merged -> {
 							d = merged;
 							return s2;
-						}))
-						.thenCompose(s2 -> s2.merge(e).thenApply(merged -> {
+						} ) )
+						.thenCompose( s2 -> s2.merge( e ).thenApply( merged -> {
 							e = merged;
 							return s2;
-						}))
-						.thenCompose(s2 -> s2.merge(f).thenApply(merged -> {
+						} ) )
+						.thenCompose( s2 -> s2.merge( f ).thenApply( merged -> {
 							f = merged;
 							return s2;
-						}))
-						.thenCompose(s2 -> s2.merge(g).thenApply(merged -> {
+						} ) )
+						.thenCompose( s2 -> s2.merge( g ).thenApply( merged -> {
 							g = merged;
 							return s2;
-						}))
-						.thenCompose(s2 -> voidFuture()
-								.thenCompose(v-> s2.remove(f))
-								.thenCompose(v -> s2.remove(g))
-								.thenCompose(v -> s2.remove(b))
-								.thenCompose(v -> s2.remove(d))
-								.thenCompose(v -> s2.remove(e))
-								.thenCompose(v -> s2.remove(c))
-								.thenCompose(v -> s2.flush())
+						} ) )
+						.thenCompose( s2 -> voidFuture()
+								.thenCompose( v -> s2.remove( f ) )
+								.thenCompose( v -> s2.remove( g ) )
+								.thenCompose( v -> s2.remove( b ) )
+								.thenCompose( v -> s2.remove( d ) )
+								.thenCompose( v -> s2.remove( e ) )
+								.thenCompose( v -> s2.remove( c ) )
+								.thenCompose( v -> s2.flush() )
 						)
 		);
 	}
@@ -194,95 +203,95 @@ public class CascadeComplicatedToOnesEagerTest extends BaseReactiveTest {
 	private void prepareEntitiesForDelete() {
 		b.c = null;
 		b.d = null;
-		b.gCollection.remove(g);
+		b.gCollection.remove( g );
 
-		c.bCollection.remove(b);
-		c.dCollection.remove(d);
+		c.bCollection.remove( b );
+		c.dCollection.remove( d );
 
-		d.bCollection.remove(b);
+		d.bCollection.remove( b );
 		d.c = null;
 		d.e = null;
-		d.fCollection.remove(f);
+		d.fCollection.remove( f );
 
-		e.dCollection.remove(d);
+		e.dCollection.remove( d );
 		e.f = null;
 
 		f.d = null;
-		f.eCollection.remove(e);
+		f.eCollection.remove( e );
 		f.g = null;
 
 		g.b = null;
-		g.fCollection.remove(f);
+		g.fCollection.remove( f );
 	}
 
-	private CompletionStage<Object> check(CompletionStage<Stage.Session> sessionStage, VertxTestContext context) {
-		return  sessionStage.thenCompose(sCheck -> sCheck.find(B.class, bId)
-				.thenApply(bCheck -> {
-					assertThat(bCheck).isEqualTo(b);
-					assertThat(Hibernate.isInitialized(bCheck.c)).isTrue();
-					assertThat(bCheck.c).isEqualTo(c);
-					assertThat(Hibernate.isInitialized(bCheck.d)).isTrue();
-					assertThat(bCheck.d).isEqualTo(d);
-					assertThat(bCheck.c).isSameAs(bCheck.d.c);
-					assertThat(bCheck.d.e).isEqualTo(e);
-					assertThat(Hibernate.isInitialized(bCheck.gCollection)).isFalse();
+	private CompletionStage<Object> check(CompletionStage<Stage.Session> sessionStage) {
+		return sessionStage.thenCompose( sCheck -> sCheck.find( B.class, bId )
+				.thenApply( bCheck -> {
+					assertThat( bCheck ).isEqualTo( b );
+					assertThat( Hibernate.isInitialized( bCheck.c ) ).isTrue();
+					assertThat( bCheck.c ).isEqualTo( c );
+					assertThat( Hibernate.isInitialized( bCheck.d ) ).isTrue();
+					assertThat( bCheck.d ).isEqualTo( d );
+					assertThat( bCheck.c ).isSameAs( bCheck.d.c );
+					assertThat( bCheck.d.e ).isEqualTo( e );
+					assertThat( Hibernate.isInitialized( bCheck.gCollection ) ).isFalse();
 					return bCheck;
-				})
-				.thenCompose(bCheck -> sCheck.fetch(bCheck.gCollection)
-						.thenApply(gCollectionCheck -> {
-								final G gElement = gCollectionCheck.iterator().next();
-								assertThat(gElement).isEqualTo(g);
-								assertThat(bCheck.d.e.f.g).isSameAs(gElement);
-								assertThat(bCheck).isSameAs(gElement.b);
-								return bCheck;
-							}
+				} )
+				.thenCompose( bCheck -> sCheck.fetch( bCheck.gCollection )
+						.thenApply( gCollectionCheck -> {
+										final G gElement = gCollectionCheck.iterator().next();
+										assertThat( gElement ).isEqualTo( g );
+										assertThat( bCheck.d.e.f.g ).isSameAs( gElement );
+										assertThat( bCheck ).isSameAs( gElement.b );
+										return bCheck;
+									}
 						)
 				)
-				.thenCompose(bCheck -> sCheck.fetch(bCheck.c.bCollection)
-						.thenApply(bCollectionCheck -> {
-							assertThat(bCheck).isSameAs(bCollectionCheck.iterator().next());
+				.thenCompose( bCheck -> sCheck.fetch( bCheck.c.bCollection )
+						.thenApply( bCollectionCheck -> {
+							assertThat( bCheck ).isSameAs( bCollectionCheck.iterator().next() );
 							return bCheck;
-						})
+						} )
 				)
-				.thenCompose(bCheck -> sCheck.fetch(bCheck.c.dCollection)
-						.thenApply(dCollectionCheck -> {
-							assertThat(bCheck.d).isSameAs(dCollectionCheck.iterator().next());
+				.thenCompose( bCheck -> sCheck.fetch( bCheck.c.dCollection )
+						.thenApply( dCollectionCheck -> {
+							assertThat( bCheck.d ).isSameAs( dCollectionCheck.iterator().next() );
 							return bCheck;
-						})
+						} )
 				)
-				.thenCompose(bCheck -> sCheck.fetch(bCheck.d.bCollection)
-						.thenApply(bCollectionCheck -> {
-							assertThat(bCheck).isSameAs(bCollectionCheck.iterator().next());
+				.thenCompose( bCheck -> sCheck.fetch( bCheck.d.bCollection )
+						.thenApply( bCollectionCheck -> {
+							assertThat( bCheck ).isSameAs( bCollectionCheck.iterator().next() );
 							return bCheck;
-						})
+						} )
 				)
-				.thenCompose(bCheck -> sCheck.fetch(bCheck.d.fCollection)
-						.thenApply(fCollectionCheck -> {
+				.thenCompose( bCheck -> sCheck.fetch( bCheck.d.fCollection )
+						.thenApply( fCollectionCheck -> {
 							final F fElement = fCollectionCheck.iterator().next();
-							assertThat(fElement).isEqualTo(f);
-							assertThat(bCheck.d.e.f).isSameAs(fElement);
-							assertThat(bCheck.d).isSameAs(fElement.d);
+							assertThat( fElement ).isEqualTo( f );
+							assertThat( bCheck.d.e.f ).isSameAs( fElement );
+							assertThat( bCheck.d ).isSameAs( fElement.d );
 							return bCheck;
-						})
+						} )
 				)
-				.thenCompose(bCheck -> sCheck.fetch(bCheck.d.e.dCollection)
-						.thenApply(dCollectionCheck -> {
-							assertThat(bCheck.d).isSameAs(dCollectionCheck.iterator().next());
+				.thenCompose( bCheck -> sCheck.fetch( bCheck.d.e.dCollection )
+						.thenApply( dCollectionCheck -> {
+							assertThat( bCheck.d ).isSameAs( dCollectionCheck.iterator().next() );
 							return bCheck;
-						})
+						} )
 				)
-				.thenCompose(bCheck -> sCheck.fetch(bCheck.d.e.f.eCollection)
-						.thenApply(eCollectionCheck -> {
-							assertThat(bCheck.d.e).isSameAs(eCollectionCheck.iterator().next());
+				.thenCompose( bCheck -> sCheck.fetch( bCheck.d.e.f.eCollection )
+						.thenApply( eCollectionCheck -> {
+							assertThat( bCheck.d.e ).isSameAs( eCollectionCheck.iterator().next() );
 							return bCheck;
-						})
+						} )
 				)
-				.thenCompose(bCheck -> sCheck.fetch(bCheck.d.e.f.g.fCollection)
-						.thenApply(fCollectionCheck -> {
+				.thenCompose( bCheck -> sCheck.fetch( bCheck.d.e.f.g.fCollection )
+						.thenApply( fCollectionCheck -> {
 							final F fElement = fCollectionCheck.iterator().next();
-							assertThat(bCheck.d.e.f).isSameAs(fElement);
+							assertThat( bCheck.d.e.f ).isSameAs( fElement );
 							return bCheck;
-						})
+						} )
 				)
 		);
 	}
@@ -290,7 +299,7 @@ public class CascadeComplicatedToOnesEagerTest extends BaseReactiveTest {
 	@Override
 	@BeforeEach
 	public void before(VertxTestContext context) {
-		super.before(context);
+		super.before( context );
 
 		bId = null;
 
@@ -360,26 +369,20 @@ public class CascadeComplicatedToOnesEagerTest extends BaseReactiveTest {
 	}
 
 	@Entity(name = "B")
-	public static class B extends AbstractEntity{
+	public static class B extends AbstractEntity {
 
 		@Id
-		@GeneratedValue(strategy = GenerationType.SEQUENCE)
+		@GeneratedValue(strategy = SEQUENCE)
 		private Long id;
 
-		@OneToMany(cascade =  {
-				CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH}
-				, mappedBy = "b")
+		@OneToMany(cascade = { MERGE, PERSIST, REFRESH }, mappedBy = "b")
 		private Set<G> gCollection = new java.util.HashSet<>();
 
 
-		@ManyToOne(cascade =  {
-				CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH}
-				, optional = false)
+		@ManyToOne(cascade = { MERGE, PERSIST, REFRESH }, optional = false)
 		private C c;
 
-		@ManyToOne(cascade =  {
-				CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH}
-				, optional = false)
+		@ManyToOne(cascade = { MERGE, PERSIST, REFRESH }, optional = false)
 		private D d;
 	}
 
@@ -387,15 +390,13 @@ public class CascadeComplicatedToOnesEagerTest extends BaseReactiveTest {
 	public static class C extends AbstractEntity {
 
 		@Id
-		@GeneratedValue(strategy = GenerationType.SEQUENCE)
+		@GeneratedValue(strategy = SEQUENCE)
 		private Long id;
 
 		@OneToMany(mappedBy = "c")
 		private Set<B> bCollection = new java.util.HashSet<>();
 
-		@OneToMany(cascade =  {
-				CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH}
-				, mappedBy = "c")
+		@OneToMany(cascade = { MERGE, PERSIST, REFRESH }, mappedBy = "c")
 		private Set<D> dCollection = new java.util.HashSet<>();
 	}
 
@@ -403,7 +404,7 @@ public class CascadeComplicatedToOnesEagerTest extends BaseReactiveTest {
 	public static class D extends AbstractEntity {
 
 		@Id
-		@GeneratedValue(strategy = GenerationType.SEQUENCE)
+		@GeneratedValue(strategy = SEQUENCE)
 		private Long id;
 
 		@OneToMany(mappedBy = "d")
@@ -415,10 +416,7 @@ public class CascadeComplicatedToOnesEagerTest extends BaseReactiveTest {
 		@ManyToOne(optional = false)
 		private E e;
 
-		@OneToMany(cascade =  {
-				CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH},
-				mappedBy = "d"
-		)
+		@OneToMany(cascade = { MERGE, PERSIST, REFRESH }, mappedBy = "d")
 		private Set<F> fCollection = new java.util.HashSet<>();
 	}
 
@@ -426,7 +424,7 @@ public class CascadeComplicatedToOnesEagerTest extends BaseReactiveTest {
 	public static class E extends AbstractEntity {
 
 		@Id
-		@GeneratedValue(strategy = GenerationType.SEQUENCE)
+		@GeneratedValue(strategy = SEQUENCE)
 		private Long id;
 
 		@OneToMany(mappedBy = "e")
@@ -440,12 +438,10 @@ public class CascadeComplicatedToOnesEagerTest extends BaseReactiveTest {
 	public static class F extends AbstractEntity {
 
 		@Id
-		@GeneratedValue(strategy = GenerationType.SEQUENCE)
+		@GeneratedValue(strategy = SEQUENCE)
 		private Long id;
 
-		@OneToMany(cascade =  {
-				CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH}
-				, mappedBy = "f")
+		@OneToMany(cascade = { MERGE, PERSIST, REFRESH }, mappedBy = "f")
 		private Set<E> eCollection = new java.util.HashSet<>();
 
 		@ManyToOne(optional = false)
@@ -459,7 +455,7 @@ public class CascadeComplicatedToOnesEagerTest extends BaseReactiveTest {
 	public static class G extends AbstractEntity {
 
 		@Id
-		@GeneratedValue(strategy = GenerationType.SEQUENCE)
+		@GeneratedValue(strategy = SEQUENCE)
 		private Long id;
 
 		@ManyToOne(optional = false)
--- hibernate-reactive-core/src/test/java/org/hibernate/reactive/EagerTest.java
+++ hibernate-reactive-core/src/test/java/org/hibernate/reactive/EagerTest.java
@@ -12,7 +12,6 @@ import java.util.concurrent.CompletionStage;
 
 import org.hibernate.Hibernate;
 import org.hibernate.annotations.Fetch;
-import org.hibernate.annotations.FetchMode;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.reactive.util.impl.CompletionStages;
 
@@ -20,9 +19,7 @@ import org.junit.jupiter.api.Test;
 
 import io.vertx.junit5.Timeout;
 import io.vertx.junit5.VertxTestContext;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
@@ -38,11 +35,17 @@ import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 import jakarta.persistence.Version;
 
+import static jakarta.persistence.CascadeType.MERGE;
+import static jakarta.persistence.CascadeType.PERSIST;
+import static jakarta.persistence.CascadeType.REFRESH;
+import static jakarta.persistence.CascadeType.REMOVE;
+import static jakarta.persistence.FetchType.EAGER;
+import static jakarta.persistence.FetchType.LAZY;
 import static java.util.concurrent.TimeUnit.MINUTES;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hibernate.annotations.FetchMode.SELECT;
 
 @Timeout(value = 10, timeUnit = MINUTES)
-
 public class EagerTest extends BaseReactiveTest {
 
 	@Override
@@ -60,25 +63,26 @@ public class EagerTest extends BaseReactiveTest {
 	@Override
 	protected CompletionStage<Void> cleanDb() {
 		return getSessionFactory()
-				.withTransaction( s -> s.createMutationQuery( "delete from Element" ).executeUpdate()
+				.withTransaction( s -> s
+						.createMutationQuery( "delete from Element" ).executeUpdate()
 						.thenCompose( v -> s.createMutationQuery( "delete from Node" ).executeUpdate() )
-						.thenCompose( CompletionStages::voidFuture ) );
+				)
+				.thenCompose( CompletionStages::voidFuture );
 	}
 
 	@Test
 	public void testEagerCollectionFetch(VertxTestContext context) {
+		Node basik = new Node( "Child" );
+		basik.parent = new Node( "Parent" );
+		basik.elements.add( new Element( basik ) );
+		basik.elements.add( new Element( basik ) );
+		basik.elements.add( new Element( basik ) );
 
-		Node basik = new Node("Child");
-		basik.parent = new Node("Parent");
-		basik.elements.add(new Element(basik));
-		basik.elements.add(new Element(basik));
-		basik.elements.add(new Element(basik));
-
-		test(context,
-				openSession()
-						.thenCompose(s -> s.persist(basik).thenCompose(v -> s.flush()))
+		test(
+				context, openSession()
+						.thenCompose( s -> s.persist( basik ).thenCompose( v -> s.flush() ) )
 						.thenCompose( v -> openSession() )
-						.thenCompose(s -> s.find( Node.class, basik.getId() ))
+						.thenCompose( s -> s.find( Node.class, basik.getId() ) )
 						.thenAccept( node -> {
 							assertThat( Hibernate.isInitialized( node.getElements() ) ).isTrue();
 							assertThat( node.getElements() ).hasSize( 3 );
@@ -91,18 +95,17 @@ public class EagerTest extends BaseReactiveTest {
 
 	@Test
 	public void testEagerParentFetch(VertxTestContext context) {
+		Node basik = new Node( "Child" );
+		basik.parent = new Node( "Parent" );
+		basik.elements.add( new Element( basik ) );
+		basik.elements.add( new Element( basik ) );
+		basik.elements.add( new Element( basik ) );
 
-		Node basik = new Node("Child");
-		basik.parent = new Node("Parent");
-		basik.elements.add(new Element(basik));
-		basik.elements.add(new Element(basik));
-		basik.elements.add(new Element(basik));
-
-		test(context,
-				openSession()
-						.thenCompose(s -> s.persist(basik).thenCompose(v -> s.flush()))
+		test(
+				context, openSession()
+						.thenCompose( s -> s.persist( basik ).thenCompose( v -> s.flush() ) )
 						.thenCompose( v -> openSession() )
-						.thenCompose(s -> s.find( Element.class, basik.getElements().get(0).getId() ))
+						.thenCompose( s -> s.find( Element.class, basik.getElements().get( 0 ).getId() ) )
 						.thenAccept( element -> {
 							assertThat( Hibernate.isInitialized( element.getNode() ) ).isTrue();
 							assertThat( Hibernate.isInitialized( element.getNode().getElements() ) ).isTrue();
@@ -113,20 +116,19 @@ public class EagerTest extends BaseReactiveTest {
 
 	@Test
 	public void testEagerParentFetchQuery(VertxTestContext context) {
+		Node basik = new Node( "Child" );
+		basik.parent = new Node( "Parent" );
+		basik.elements.add( new Element( basik ) );
+		basik.elements.add( new Element( basik ) );
+		basik.elements.add( new Element( basik ) );
 
-		Node basik = new Node("Child");
-		basik.parent = new Node("Parent");
-		basik.elements.add(new Element(basik));
-		basik.elements.add(new Element(basik));
-		basik.elements.add(new Element(basik));
-
-		test(context,
-				openSession()
-						.thenCompose(s -> s.persist(basik).thenCompose(v -> s.flush()))
+		test(
+				context, openSession()
+						.thenCompose( s -> s.persist( basik ).thenCompose( v -> s.flush() ) )
 						.thenCompose( v -> openSession() )
-						.thenCompose(s -> s.createSelectionQuery( "from Element", Element.class ).getResultList())
+						.thenCompose( s -> s.createSelectionQuery( "from Element", Element.class ).getResultList() )
 						.thenAccept( elements -> {
-							for (Element element: elements) {
+							for ( Element element : elements ) {
 								assertThat( Hibernate.isInitialized( element.getNode() ) ).isTrue();
 								assertThat( Hibernate.isInitialized( element.getNode().getElements() ) ).isTrue();
 								assertThat( element.getNode().getElements() ).hasSize( 3 );
@@ -137,32 +139,37 @@ public class EagerTest extends BaseReactiveTest {
 
 	@Test
 	public void testEagerFetchQuery(VertxTestContext context) {
+		Node basik = new Node( "Child" );
+		basik.parent = new Node( "Parent" );
+		basik.elements.add( new Element( basik ) );
+		basik.elements.add( new Element( basik ) );
+		basik.elements.add( new Element( basik ) );
 
-		Node basik = new Node("Child");
-		basik.parent = new Node("Parent");
-		basik.elements.add(new Element(basik));
-		basik.elements.add(new Element(basik));
-		basik.elements.add(new Element(basik));
-
-		test(context,
-				openSession()
-						.thenCompose(s -> s.persist(basik).thenCompose(v -> s.flush()))
+		test(
+				context, openSession()
+						.thenCompose( s -> s.persist( basik ).thenCompose( v -> s.flush() ) )
 						.thenCompose( v -> openSession() )
-						.thenCompose(s -> s.createSelectionQuery("from Node order by id", Node.class).getResultList())
-						.thenAccept(list -> {
+						.thenCompose( s -> s.createSelectionQuery(
+								"from Node order by id",
+								Node.class
+						).getResultList() )
+						.thenAccept( list -> {
 							assertThat( list ).hasSize( 2 );
-							assertThat( Hibernate.isInitialized( list.get(0).getElements() ) ).isTrue();
-							assertThat( list.get(0).getElements() ).hasSize( 3 );
-							assertThat( list.get(1).getElements() ).hasSize( 0 );
-						})
+							assertThat( Hibernate.isInitialized( list.get( 0 ).getElements() ) ).isTrue();
+							assertThat( list.get( 0 ).getElements() ).hasSize( 3 );
+							assertThat( list.get( 1 ).getElements() ).hasSize( 0 );
+						} )
 						.thenCompose( v -> openSession() )
-						.thenCompose(s -> s.createSelectionQuery("select distinct n, e from Node n join n.elements e order by n.id", Object[].class).getResultList())
-						.thenAccept(list -> {
+						.thenCompose( s -> s.createSelectionQuery(
+								"select distinct n, e from Node n join n.elements e order by n.id",
+								Object[].class
+						).getResultList() )
+						.thenAccept( list -> {
 							assertThat( list ).hasSize( 3 );
-							Object[] tup = list.get(0);
-							assertThat( Hibernate.isInitialized( ((Node) tup[0]).getElements() ) ).isTrue();
-							assertThat( ((Node) tup[0]).getElements() ).hasSize( 3 );
-						})
+							Object[] tup = list.get( 0 );
+							assertThat( Hibernate.isInitialized( ( (Node) tup[0] ).getElements() ) ).isTrue();
+							assertThat( ( (Node) tup[0] ).getElements() ).hasSize( 3 );
+						} )
 		);
 	}
 
@@ -174,7 +181,7 @@ public class EagerTest extends BaseReactiveTest {
 		Integer id;
 
 		@ManyToOne
-		@Fetch(FetchMode.SELECT)
+		@Fetch(SELECT)
 		Node node;
 
 		public Element(Node node) {
@@ -212,18 +219,11 @@ public class EagerTest extends BaseReactiveTest {
 		Integer version;
 		String string;
 
-		@ManyToOne(fetch = FetchType.LAZY,
-				cascade = {CascadeType.PERSIST,
-						CascadeType.REFRESH,
-						CascadeType.MERGE,
-						CascadeType.REMOVE})
+		@ManyToOne(fetch = LAZY, cascade = { PERSIST, REFRESH, MERGE, REMOVE })
 		Node parent;
 
-		@OneToMany(fetch = FetchType.EAGER,
-				cascade = {CascadeType.PERSIST,
-						CascadeType.REMOVE},
-				mappedBy = "node")
-		@Fetch(FetchMode.SELECT)
+		@OneToMany(fetch = EAGER, cascade = { PERSIST, REMOVE }, mappedBy = "node")
+		@Fetch(SELECT)
 		List<Element> elements = new ArrayList<>();
 
 		@Transient
@@ -306,19 +306,19 @@ public class EagerTest extends BaseReactiveTest {
 
 		@Override
 		public boolean equals(Object o) {
-			if (this == o) {
+			if ( this == o ) {
 				return true;
 			}
-			if (o == null || getClass() != o.getClass()) {
+			if ( o == null || getClass() != o.getClass() ) {
 				return false;
 			}
 			Node node = (Node) o;
-			return Objects.equals(string, node.string);
+			return Objects.equals( string, node.string );
 		}
 
 		@Override
 		public int hashCode() {
-			return Objects.hash(string);
+			return Objects.hash( string );
 		}
 
 		public Integer getVersion() {
