19d5757e5bf555d7c3655046fc0ab314ff3e2e34
Davide D'Alto <davide@hibernate.org>
2026-03-05 15:35:51 +0100

    [#1060] Use Testcontainers as default for testing
    Test will run using Testcontainers unless specified
    using the `skipTestcontainers` parameter.

--- hibernate-reactive-core/src/test/java/org/hibernate/reactive/containers/MySQLDatabase.java
+++ hibernate-reactive-core/src/test/java/org/hibernate/reactive/containers/MySQLDatabase.java
@@ -80,8 +80,8 @@ class MySQLDatabase implements TestableDatabase {
 	}};
 
 	/**
-	 * Holds configuration for the MySQL database container. If the build is run with <code>-Pdocker</code> then
-	 * Testcontainers+Docker will be used.
+	 * Holds configuration for the MySQL database container. By default, Testcontainers will be used.
+	 * To use a local MySQL instance instead, run the build with <code>-PskipTestcontainers</code>.
 	 * <p>
 	 * TIP: To reuse the same containers across multiple runs, set `testcontainers.reuse.enable=true` in a file located
 	 * at `$HOME/.testcontainers.properties` (create the file if it does not exist).
