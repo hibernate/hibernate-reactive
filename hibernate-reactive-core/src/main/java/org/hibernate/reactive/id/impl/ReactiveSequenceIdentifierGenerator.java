53460078029f4ec3481bd6208b788ee535b577b9
Davide D'Alto <davide@hibernate.org>
2026-03-12 16:42:45 +0100

    [#3367] Clean up identifier generators

--- hibernate-reactive-core/src/main/java/org/hibernate/reactive/id/impl/ReactiveSequenceIdentifierGenerator.java
+++ hibernate-reactive-core/src/main/java/org/hibernate/reactive/id/impl/ReactiveSequenceIdentifierGenerator.java
@@ -3,14 +3,12 @@
  * Copyright Red Hat Inc. and Hibernate Authors
  */
 package org.hibernate.reactive.id.impl;
-import java.util.Properties;
+
 import java.util.concurrent.CompletionStage;
 
-import org.hibernate.HibernateException;
 import org.hibernate.boot.model.relational.QualifiedName;
 import org.hibernate.boot.model.relational.SqlStringGenerationContext;
 import org.hibernate.dialect.Dialect;
-import org.hibernate.engine.spi.SharedSessionContractImplementor;
 import org.hibernate.id.IdentifierGenerator;
 import org.hibernate.id.enhanced.DatabaseStructure;
 import org.hibernate.metamodel.spi.RuntimeModelCreationContext;
@@ -19,20 +17,20 @@ import org.hibernate.reactive.session.ReactiveConnectionSupplier;
 /**
  * Support for JPA's {@link jakarta.persistence.SequenceGenerator}.
  * <p>
+ * Mimic a {@link org.hibernate.id.enhanced.SequenceStyleGenerator} with {@link org.hibernate.id.enhanced.SequenceStructure}.
+ * <p>
  * This implementation supports block allocation, but does not
  * guarantee that generated identifiers are sequential.
  */
 public class ReactiveSequenceIdentifierGenerator extends BlockingIdentifierGenerator implements IdentifierGenerator {
 
 	public static final Object[] NO_PARAMS = new Object[0];
-	private Dialect dialect;
-	private QualifiedName qualifiedName;
+
+	private final Dialect dialect;
+	private final QualifiedName qualifiedName;
+	private final int increment;
 
 	private String sql;
-	private int increment;
-
-	public ReactiveSequenceIdentifierGenerator() {
-	}
 
 	public ReactiveSequenceIdentifierGenerator(DatabaseStructure structure, RuntimeModelCreationContext creationContext) {
 		qualifiedName = structure.getPhysicalName();
@@ -47,24 +45,12 @@ public class ReactiveSequenceIdentifierGenerator extends BlockingIdentifierGener
 
 	@Override
 	protected CompletionStage<Long> nextHiValue(ReactiveConnectionSupplier session) {
-		return session.getReactiveConnection()
-				.selectIdentifier( sql, NO_PARAMS, Long.class );
+		return session.getReactiveConnection().selectIdentifier( sql, NO_PARAMS, Long.class );
 	}
 
 	// Called after configure
 	@Override
 	public void initialize(SqlStringGenerationContext context) {
-		String renderedSequenceName = context.format( qualifiedName );
-		sql = dialect.getSequenceSupport().getSequenceNextValString( renderedSequenceName );
-	}
-
-	@Override
-	public Object generate(SharedSessionContractImplementor session, Object object) throws HibernateException {
-		// TODO: Do we need to implement this?
-		throw new UnsupportedOperationException("Not yet implemented");
-	}
-
-	public QualifiedName getSequenceName() {
-		return qualifiedName;
+		sql = dialect.getSequenceSupport().getSequenceNextValString( context.format( qualifiedName ) );
 	}
 }
