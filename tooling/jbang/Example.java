6e0e15b6f1e7fc901831a7ea8409ede5319e25d7
Davide D'Alto <davide@hibernate.org>
2026-03-20 16:57:36 +0100

    [#3458] Update JBang templates dependencies

--- tooling/jbang/Example.java
+++ tooling/jbang/Example.java
@@ -8,8 +8,8 @@
 //DEPS com.ongres.scram:scram-client:3.2
 //DEPS io.vertx:vertx-pg-client:${vertx.version:4.5.25}
 //DEPS io.vertx:vertx-mysql-client:${vertx.version:4.5.25}
-//DEPS io.vertx:vertx-db2-client:${vertx.version:4.5.25}
-//DEPS org.hibernate.reactive:hibernate-reactive-core:${hibernate-reactive.version:3.3.0.CR1}
+//DEPS io.vertx:vertx-db2-client:${vertx.version:5.0.8}
+//DEPS org.hibernate.reactive:hibernate-reactive-core:${hibernate-reactive.version:4.3.0.Final}
 //DEPS org.slf4j:slf4j-simple:2.0.17
 //DESCRIPTION Allow authentication to PostgreSQL using SCRAM:
 
