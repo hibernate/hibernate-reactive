847acdcb0a744bbe97a0b2bd2355ce22cc076f0c
Davide D'Alto <davide@hibernate.org>
2026-03-26 15:22:07 +0100

    Use a converter instead of a factory
    Reduces the amount of code needed to have a reactive
    StateManagement

--- hibernate-reactive-core/src/main/java/org/hibernate/reactive/persister/entity/impl/ReactiveJoinedSubclassEntityPersister.java
+++ hibernate-reactive-core/src/main/java/org/hibernate/reactive/persister/entity/impl/ReactiveJoinedSubclassEntityPersister.java
@@ -37,7 +37,6 @@ import org.hibernate.metamodel.spi.RuntimeModelCreationContext;
 import org.hibernate.persister.entity.AbstractEntityPersister;
 import org.hibernate.persister.entity.EntityPersister;
 import org.hibernate.persister.entity.JoinedSubclassEntityPersister;
-import org.hibernate.persister.state.spi.StateManagement;
 import org.hibernate.property.access.spi.PropertyAccess;
 import org.hibernate.reactive.bythecode.spi.ReactiveBytecodeEnhancementMetadataPojoImplAdapter;
 import org.hibernate.reactive.loader.ast.internal.ReactiveSingleIdArrayLoadPlan;
@@ -57,7 +56,6 @@ import org.hibernate.type.EntityType;
 
 import static java.lang.invoke.MethodHandles.lookup;
 import static org.hibernate.reactive.logging.impl.LoggerFactory.make;
-import static org.hibernate.reactive.persister.entity.impl.ReactiveAbstractEntityPersister.reactiveStateManagement;
 
 /**
  * An {@link ReactiveEntityPersister} backed by {@link JoinedSubclassEntityPersister}
@@ -80,13 +78,9 @@ public class ReactiveJoinedSubclassEntityPersister extends JoinedSubclassEntityP
 				cacheAccessStrategy,
 				naturalIdRegionAccessStrategy,
 				new ReactiveRuntimeModelCreationContext( creationContext ),
-				ReactiveJoinedSubclassEntityPersister::createReactiveStateManagement
+				ReactiveAbstractEntityPersister::reactiveStateManagement
 		);
 		reactiveDelegate = new ReactiveAbstractPersisterDelegate( this, persistentClass, new ReactiveRuntimeModelCreationContext( creationContext ) );
-	}
-
-	private static StateManagement createReactiveStateManagement(PersistentClass pc) {
-		return reactiveStateManagement( AbstractEntityPersister.createStateManagement( pc ) );
 	}
 
 	@Override
--- hibernate-reactive-core/src/main/java/org/hibernate/reactive/persister/entity/impl/ReactiveSingleTableEntityPersister.java
+++ hibernate-reactive-core/src/main/java/org/hibernate/reactive/persister/entity/impl/ReactiveSingleTableEntityPersister.java
@@ -39,7 +39,6 @@ import org.hibernate.metamodel.spi.RuntimeModelCreationContext;
 import org.hibernate.persister.entity.AbstractEntityPersister;
 import org.hibernate.persister.entity.EntityPersister;
 import org.hibernate.persister.entity.SingleTableEntityPersister;
-import org.hibernate.persister.state.spi.StateManagement;
 import org.hibernate.property.access.spi.PropertyAccess;
 import org.hibernate.reactive.bythecode.spi.ReactiveBytecodeEnhancementMetadataPojoImplAdapter;
 import org.hibernate.reactive.loader.ast.internal.ReactiveSingleIdArrayLoadPlan;
@@ -59,7 +58,6 @@ import org.hibernate.type.EntityType;
 
 import static java.lang.invoke.MethodHandles.lookup;
 import static org.hibernate.reactive.logging.impl.LoggerFactory.make;
-import static org.hibernate.reactive.persister.entity.impl.ReactiveAbstractEntityPersister.reactiveStateManagement;
 
 
 /**
@@ -82,13 +80,9 @@ public class ReactiveSingleTableEntityPersister extends SingleTableEntityPersist
 				cacheAccessStrategy,
 				naturalIdRegionAccessStrategy,
 				new ReactiveRuntimeModelCreationContext( creationContext ),
-				ReactiveSingleTableEntityPersister::createReactiveStateManagement
+				ReactiveAbstractEntityPersister::reactiveStateManagement
 		);
 		reactiveDelegate = new ReactiveAbstractPersisterDelegate( this, persistentClass, new ReactiveRuntimeModelCreationContext( creationContext ) );
-	}
-
-	private static StateManagement createReactiveStateManagement(PersistentClass pc) {
-		return reactiveStateManagement( AbstractEntityPersister.createStateManagement( pc ) );
 	}
 
 	@Override
